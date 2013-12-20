06c4b0c9bf9f97a01955a88792e5fe567aa2783a
Andrei Neculau <andrei.neculau@gmail.com>
2013-12-20 22:48:40 +0200

    add phonies

--- util.mk
+++ util.mk
@@ -11,6 +11,7 @@ env-guard-%:
 
 
 # CHECK EXECUTABLE
+.PHONY: env-has-%
 env-has-%:
 	@command -v "${*}" >/dev/null 2>&1
 
@@ -18,12 +19,14 @@ env-has-%:
 # silent TARGET
 # Usage:
 # my-target: my-optional-target | silent
+.PHONY: silent
 silent:
 	@:
 
 
 # help TARGET (list all available targets)
 # From http://stackoverflow.com/a/15058900
+.PHONY: help
 help:
 #	@sh -c "$(MAKE) -p silent | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | grep -v -e 'env-guard-*' -e 'env-has-*' -e 'make' -e 'Makefile' | sort | uniq"
 	@$(MAKE) -p silent | \
@@ -36,5 +39,6 @@ help:
 
 
 # usage TARGET (echo USAGE information)
+.PHONY: usage
 usage: env-has-USAGE
 	@echo "$$USAGE"
