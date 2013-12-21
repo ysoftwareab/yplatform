79a19996e2678600c48fd562935d402c9e2a230b
Andrei Neculau <andrei.neculau@gmail.com>
2013-12-21 17:27:46 +0200

    namespace all

--- util.mk
+++ util.mk
@@ -2,8 +2,8 @@
 # Usage:
 # my-target: env-guard-HOST
 # means my-target depends on $HOST being set
-.PHONY: env-guard-%
-env-guard-%:
+.PHONY: util-env-guard-%
+util-env-guard-%:
 	@if [[ "$${${*}}" == "" ]]; then \
 		echo "ERROR: Environment variable ${*} is not defined!"; \
 		exit -1; \
@@ -11,8 +11,8 @@ env-guard-%:
 
 
 # CHECK EXECUTABLE
-.PHONY: env-has-%
-env-has-%:
+.PHONY: util-env-has-%
+util-env-has-%:
 	@command -v "${*}" >/dev/null 2>&1 || { \
 		echo "ERROR: Please install ${*}!"; \
 		exit -1; \
@@ -21,27 +21,28 @@ env-has-%:
 
 # silent TARGET
 # Usage:
-# my-target: my-optional-target | silent
-.PHONY: silent
-silent:
+# my-target: my-optional-target | util-silent
+.PHONY: util-silent
+util-silent:
 	@:
 
 
 # help TARGET (list all available targets)
 # From http://stackoverflow.com/a/15058900
-.PHONY: help
-help:
+.PHONY: util-help
+util-help:
 #	@sh -c "$(MAKE) -p silent | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | grep -v -e 'env-guard-*' -e 'env-has-*' -e 'make' -e 'Makefile' | sort | uniq"
-	@$(MAKE) -p silent | \
+	@$(MAKE) -p util-silent | \
 		pcregrep -e '^[a-zA-Z0-9]+:' | \
 		sed 's/:.*//' | \
 		pcregrep -v -e 'make' | \
-		pcregrep -v -e 'silent' | \
+		pcregrep -v -e 'Makefile' | \
+		pcregrep -v -e 'util-*' | \
 		sort | \
 		uniq
 
 
 # usage TARGET (echo USAGE information)
-.PHONY: usage
-usage: env-guard-USAGE
+.PHONY: util-usage
+util-usage: util-env-guard-USAGE
 	@echo "$$USAGE"
