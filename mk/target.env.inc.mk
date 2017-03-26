628771aa6a6aeb927e05c01fb04b63cb5a839a64
Andrei Neculau <andrei.neculau@gmail.com>
2017-03-26 10:34:11 +0200

    [andreineculau/core.inc.mk] canonical guard targets

--- mk/target.env.inc.mk
+++ mk/target.env.inc.mk
@@ -1,13 +1,13 @@
-.PHONY: env-guard-%
-env-guard-%: # Guard on environment variable.
+.PHONY: guard-env-%
+guard-env-%: # Guard on environment variable.
 	@if [ "$($*)" = "" ] && [ "$${$*:-}" = "" ]; then \
 		echo >&2 "ERROR: Environment variable $* is not defined!"; \
 		exit 1; \
 	fi
 
 
-.PHONY: env-has-%
-env-has-%: # Guard on environment executable.
+.PHONY: guard-env-has-%
+guard-env-has-%: # Guard on environment executable.
 	@command -v "${*}" >/dev/null 2>&1 || { \
 		echo >&2 "ERROR: Please install ${*}!"; \
 		exit 1; \
