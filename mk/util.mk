79ac8598dd2962554d02470fdc6324f9edaf5255
Andrei Neculau <andrei.neculau@gmail.com>
2017-04-17 16:40:22 +0200

    [andreineculau/core.inc.mk] no util- target prefixes

--- mk/util.mk
+++ mk/util.mk
@@ -1,8 +1,8 @@
 # https://raw.githubusercontent.com/andreineculau/util.mk/master/util.mk
 
 # PRINT MAKEFILE VARIABLES
-.PHONY: util-printvars
-util-printvars:
+.PHONY: print-all
+print-all:
 	@$(foreach V,$(sort $(.VARIABLES)),
 		$(if $(filter-out environment% default automatic,
 		$(origin $V)),$(warning $V=$($V) ($(value $V)))))
@@ -10,8 +10,8 @@ util-printvars:
 
 # PRINT MAKEFILE VARIABLE
 # From http://blog.jgc.org/2015/04/the-one-line-you-should-add-to-every.html
-.PHONY: util-printvar-%
-util-printvar-%:
+.PHONY: print-%
+print-%:
 	@echo $*=$($*)
 
 
@@ -19,8 +19,8 @@ util-printvar-%:
 # Usage:
 # my-target: env-guard-HOST
 # means my-target depends on $HOST being set
-.PHONY: util-env-guard-%
-util-env-guard-%:
+.PHONY: env-guard-%
+env-guard-%:
 	@if [[ "$${${*}}" == "" ]]; then \
 		echo "ERROR: Environment variable ${*} is not defined!"; \
 		exit -1; \
@@ -28,8 +28,8 @@ util-env-guard-%:
 
 
 # CHECK EXECUTABLE
-.PHONY: util-env-has-%
-util-env-has-%:
+.PHONY: env-has-%
+env-has-%:
 	@command -v "${*}" >/dev/null 2>&1 || { \
 		echo "ERROR: Please install ${*}!"; \
 		exit -1; \
@@ -39,15 +39,15 @@ util-env-has-%:
 # silent TARGET
 # Usage:
 # my-target: my-optional-target | util-silent
-.PHONY: util-silent
-util-silent:
+.PHONY: silent
+silent:
 	@:
 
 
 # help TARGET (list all available targets)
 # From http://stackoverflow.com/a/15058900
-.PHONY: util-help
-util-help:
+.PHONY: help
+help:
 #	@sh -c "$(MAKE) -p silent | awk -F':' '/^[a-zA-Z0-9][^\$$#\/\\t=]*:([^=]|$$)/ {split(\$$1,A,/ /);for(i in A)print A[i]}' | grep -v '__\$$' | grep -v -e 'env-guard-*' -e 'env-has-*' -e 'make' -e 'Makefile' | sort | uniq"
 	@$(MAKE) -p util-silent | \
 		pcregrep -e '^[a-zA-Z0-9][^\$$#\\t=]*:' | \
@@ -58,9 +58,3 @@ util-help:
 		pcregrep -v -e 'util-*' | \
 		sort | \
 		uniq
-
-
-# usage TARGET (echo USAGE information)
-.PHONY: util-usage
-util-usage: util-env-guard-USAGE
-	@echo "$$USAGE"
