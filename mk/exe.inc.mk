f372a3de139b38a8edd35580ad713d2c536723a8
Andrei Neculau <andrei.neculau@gmail.com>
2017-07-11 16:30:13 +0200

    [andreineculau/core.inc.mk] add exe.echo.inc.mk

--- mk/exe.inc.mk
+++ mk/exe.inc.mk
@@ -1,5 +1,6 @@
 WHICH_Q := 2>/dev/null which
 
 include $(CORE_INC_MK_PATH)/exe.gnu.inc.mk
+include $(CORE_INC_MK_PATH)/exe.echo.inc.mk
 include $(CORE_INC_MK_PATH)/exe.node.inc.mk
 include $(CORE_INC_MK_PATH)/exe.misc.inc.mk
