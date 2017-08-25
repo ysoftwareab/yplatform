3d535126dedd18881ccb7dc67e6377df9755bec3
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-08-25 21:29:32 +0200

    add Makefile templates

---
+++ repo/Makefile.common.mk
@@ -0,0 +1,79 @@
+# Usage:
+
+# ifeq (,$(wildcard support-firecloud/Makefile))
+# INSTALL_SUPPORT_FIRECLOUD := $(shell git submodule update --init --recursive support-firecloud)
+# ifneq (,$(filter undefine,$(.FEATURES)))
+# undefine INSTALL_SUPPORT_FIRECLOUD
+# endif
+# endif
+
+# TOP := $(abspath $(shell dirname $(lastword $(MAKEFILE_LIST))))
+# include support-firecloud/repo/Makefile.common.mk
+# include support-firecloud/repo/...
+
+SF_PATH := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/..))
+include $(SF_PATH)/repo/core.inc.mk/Makefile
+
+ECLINT = $(call which,ECLINT,eclint)
+JSONLINT = $(call which,JSONLINT,jsonlint)
+
+EC_FILES_IGNORE := \
+	-e "^package-lock.json$$" \
+	-e "^LICENSE$$" \
+	-e "^UNLICENSE$$" \
+
+EC_FILES = $(shell $(GIT_LS) | $(GREP) -v $(EC_FILES_IGNORE) | $(SED) "s/^/'/g" | $(SED) "s/$$/'/g")
+JSON_FILES = $(shell $(GIT_LS) | $(GREP) -e ".json$$" | $(SED) "s/^/'/g" | $(SED) "s/$$/'/g")
+
+SF_CLEAN_FILES :=
+
+SF_BUILD_TARGETS :=
+
+SF_CHECK_TARGETS := \
+	lint-ec \
+
+ECLINT_ARGS ?=
+
+# ------------------------------------------------------------------------------
+
+.PHONY: clean
+clean: ## Clean.
+	@$(ECHO_DO) "Cleaning..."
+	$(RM) $(SF_CLEAN_FILES)
+	@$(ECHO_DONE)
+
+
+.PHONY: nuke
+nuke: ## Nuke (Stash actually) all files/changes not checked in.
+	@$(ECHO_DO) "Nuking..."
+	$(GIT) reset
+	$(GIT) stash --all
+	@$(ECHO_DONE)
+
+
+.PHONY: deps-git
+deps-git:
+	$(GIT) submodule sync
+	$(GIT) submodule update --init --recursive
+
+
+.PHONY: build
+build: ## Build.
+	@$(ECHO_DO) "Building..."
+	$(MAKE) $(SF_BUILD_TARGETS)
+	@$(ECHO_DONE)
+
+
+.PHONY: lint-ec
+lint-ec:
+	$(ECLINT) check $(ECLINT_ARGS) $(EC_FILES) || { \
+		$(ECLINT) fix $(ECLINT_ARGS) $(EC_FILES) 2>/dev/null >&2; \
+		exit 1; \
+	}
+
+
+.PHONY: check
+check: ## Check.
+	@$(ECHO_DO) "Checking..."
+	$(MAKE) $(SF_CHECK_TARGETS)
+	@$(ECHO_DONE)
---
+++ repo/Makefile.pkg.node.mk
@@ -0,0 +1,92 @@
+PATH := $(MAKE_PATH)/node_modules/.bin:$(GIT_ROOT)/node_modules/.bin:$(PATH)
+export PATH
+
+ESLINT = $(call which,ESLINT,eslint)
+NPM_PUBLISH_GIT = $(call which,NPM_PUBLISH_GIT,npm-publish-git)
+
+ESLINT_ARGS ?= --ignore-pattern '!.eslintrc.js' --config $(MAKE_PATH)/node_modules/eslint-config-firecloud/no-ide.js
+
+JS_FILES = $(shell $(GIT_LS) | $(GREP) -e ".js$$" | $(SED) "s/^/'/g" | $(SED) "s/$$/'/g")
+
+SRC_JS_FILES := $(shell $(FIND_Q) src -type f -name "*.js" -print)
+LIB_JS_FILES := $(patsubst src/%.js,lib/%.js,$(SRC_JS_FILES))
+
+SF_CLEAN_FILES := \
+	lib \
+	node_modules \
+
+SF_BUILD_TARGETS := \
+	$(SF_BUILD_TARGETS) \
+	build-js \
+
+SF_CHECK_TARGETS := \
+	$(SF_CHECK_TARGETS) \
+	lint-js \
+
+# ------------------------------------------------------------------------------
+
+.PHONY: all
+all: deps build check ## Fetch dependencies, build and check.
+
+
+.PHONY: deps-npm
+deps-npm:
+	$(NPM) install --no-package-lock
+	node_modules/babel-preset-firecloud/npm-install-peer-dependencies
+	node_modules/eslint-config-firecloud/npm-install-peer-dependencies
+
+
+.PHONY: deps
+deps: ## Fetch dependencies.
+	@$(ECHO_DO) "Fetching dependencies..."
+	$(MAKE) deps-git deps-npm
+	@$(ECHO_DONE)
+
+
+$(LIB_JS_FILES): lib/%.js: src/%.js $(SRC_JS_FILES)
+	$(MKDIR) $(shell dirname $@)
+	$(BABEL) $< --source-maps --out-file $@
+
+
+.PHONY: build-js
+build-js: $(LIB_JS_FILES)
+
+
+.PHONY: lint-js
+lint-js:
+	$(ESLINT) $(ESLINT_ARGS) $(JS_FILES) || { \
+		$(ESLINT) $(ESLINT_ARGS) --fix $(JS_FILES) 2>/dev/null >&2; \
+		exit 1; \
+	}
+
+
+.PHONY: version
+version: version/patch ## Bump version (patch level).
+
+
+.PHONY: version/%
+version/%: ## Bump version to given level (major/minor/patch).
+	$(NPM) version ${*}
+
+
+.PHONY: publish
+publish: ## Publish as a git version tag.
+	@$(ECHO_DO) "Publishing version..."
+	$(NPM_PUBLISH_GIT)
+	@$(ECHO_DONE)
+
+
+.PHONY: publish/%
+publish/%: ## Publish as given git tag.
+	@$(ECHO_DO) "Publishing tag ${*}..."
+	$(NPM_PUBLISH_GIT) --tag ${*}
+	@$(ECHO_DONE)
+
+
+.PHONY: package-json-prepare
+ifneq (node_modules,$(shell basename $(abspath ..))) # let Makefile build, or else build runs twice
+package-json-prepare:
+	:
+else # installing as dependency
+package-json-prepare: build
+endif
