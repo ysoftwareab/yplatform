f962d3ab73e7f15a1957f5996da004107deaf34b
Andrei Neculau <andrei.neculau@gmail.com>
2017-08-25 21:42:54 +0200

    simplify which

--- exe.inc.mk
+++ exe.inc.mk
@@ -1,6 +1,13 @@
 WHICH_Q := 2>/dev/null which
 
+define global-which
+$(shell export RESULT="$$(for CMD in $(2); do $(WHICH_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
+endef
+
+define which
+$(shell PATH="$(PATH)" export RESULT="$$(for CMD in $(2); do $(WHICH_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
+endef
+
 include $(CORE_INC_MK_PATH)/exe.gnu.inc.mk
 include $(CORE_INC_MK_PATH)/exe.echo.inc.mk
-include $(CORE_INC_MK_PATH)/exe.node.inc.mk
 include $(CORE_INC_MK_PATH)/exe.misc.inc.mk
