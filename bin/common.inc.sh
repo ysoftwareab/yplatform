d9be27f6488055f2f1d41e24990b797f961f9b06
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-08-30 19:41:19 +0200

    revamp travis

--- bin/common.inc.sh
+++ bin/common.inc.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[ -z "${INSTALL_SUPPORT_FIRECLOUD_SH:-}" ] || {
-  export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+[ -n "${INSTALL_SUPPORT_FIRECLOUD_SH:-}" ] || {
+    export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+    export SUPPORT_FIRECLOUD_CACHE_DIR=/tmp/support-firecloud.cache
 
-  source ${SUPPORT_FIRECLOUD_DIR}/bin/core.inc.sh
-  source ${SUPPORT_FIRECLOUD_DIR}/bin/exe.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/bin/core.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/bin/exe.inc.sh
 
-  export INSTALL_SUPPORT_FIRECLOUD_SH=true
+    # export INSTALL_SUPPORT_FIRECLOUD_SH=true
 }
