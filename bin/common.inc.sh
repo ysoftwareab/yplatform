2fc31e8aacd161e570c31d6a1eef06ec4d98bb17
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2017-09-05 12:55:05 +0200

    make sure the cache dir exists

--- bin/common.inc.sh
+++ bin/common.inc.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 [ -n "${INSTALL_SUPPORT_FIRECLOUD_SH:-}" ] || {
     export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
     export SUPPORT_FIRECLOUD_CACHE_DIR=/tmp/support-firecloud.cache
+    mkdir -p "${SUPPORT_FIRECLOUD_CACHE_DIR}"
 
     source ${SUPPORT_FIRECLOUD_DIR}/bin/core.inc.sh
     source ${SUPPORT_FIRECLOUD_DIR}/bin/exe.inc.sh
