26d7dd285e1c7ef8ce5537d29809a69e87842c58
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-02-16 13:32:38 +0100

    publish target should not version under the hood

--- repo/mk/js.publish.tag.mk
+++ repo/mk/js.publish.tag.mk
@@ -1,12 +1,10 @@
 publish:
 	@$(ECHO_DO) "Publishing version..."
-	$(MAKE) version
 	$(GIT) push origin v`$(CAT) "package.json" | $(JSON) "version"`
 	@$(ECHO_DONE)
 
 
 publish/%:
 	@$(ECHO_DO) "Publishing tag ${*}..."
-	$(MAKE) version/${*}
 	$(GIT) push origin ${*}
 	@$(ECHO_DONE)
