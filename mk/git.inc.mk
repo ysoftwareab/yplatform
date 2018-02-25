458d035b823926fe7501568430c1ed7577121b94
Andrei Neculau <andrei.neculau@gmail.com>
2018-02-25 23:07:39 +0100

    [andreineculau/core.inc.mk] add GIT_REMOTE and GIT_TAG

--- mk/git.inc.mk
+++ mk/git.inc.mk
@@ -3,4 +3,7 @@ GIT_HASH = $(shell cd $(TOP) && git rev-parse HEAD 2>/dev/null)
 GIT_HASH_SHORT = $(shell cd $(TOP) && git rev-parse --short HEAD 2>/dev/null)
 GIT_BRANCH = $(shell cd $(TOP) && git rev-parse --abbrev-ref HEAD 2>/dev/null)
 GIT_BRANCH_SHORT = $(shell basename $(GIT_BRANCH))
-$(foreach VAR,GIT_ROOT GIT_HASH GIT_HASH_SHORT GIT_BRANCH GIT_BRANCH_SHORT,$(call make-lazy,$(VAR)))
+GIT_REMOTE = $(git config branch.$(GIT_BRANCH).remote 2>/dev/null || true)
+GIT_TAG = $(git --describe --exact-match --tags HEAD 2>/dev/null || true)
+
+$(foreach VAR,GIT_ROOT GIT_HASH GIT_HASH_SHORT GIT_BRANCH GIT_BRANCH_SHORT GIT_REMOTE GIT_TAG,$(call make-lazy,$(VAR)))
