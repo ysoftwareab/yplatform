ddfbe722d1e9f77cafadd0fbe55076b9ca4477b2
Andrei Neculau <andrei.neculau@gmail.com>
2018-02-26 12:18:12 +0100

    [andreineculau/core.inc.mk] rename GIT_TAG -> GIT_TAGS

--- mk/git.inc.mk
+++ mk/git.inc.mk
@@ -4,10 +4,10 @@ GIT_HASH = $(shell cd $(TOP) && git rev-parse HEAD 2>/dev/null)
 GIT_HASH_SHORT = $(shell cd $(TOP) && git rev-parse --short HEAD 2>/dev/null)
 GIT_REMOTE = $(shell git config branch.$(GIT_BRANCH).remote 2>/dev/null)
 GIT_ROOT = $(shell cd $(TOP) && git rev-parse --show-toplevel 2>/dev/null)
-GIT_TAG = $(shell git describe --exact-match --tags HEAD 2>/dev/null)
+GIT_TAGS = $(shell git describe --exact-match --tags HEAD 2>/dev/null)
 
 ifeq (true,$(TRAVIS))
 GIT_BRANCH = $(TRAVIS_BRANCH)
 endif
 
-$(foreach VAR,GIT_BRANCH GIT_BRANCH_SHORT GIT_HASH GIT_HASH_SHORT GIT_REMOTE GIT_ROOT GIT_TAG,$(call make-lazy,$(VAR)))
+$(foreach VAR,GIT_BRANCH GIT_BRANCH_SHORT GIT_HASH GIT_HASH_SHORT GIT_REMOTE GIT_ROOT GIT_TAGS,$(call make-lazy,$(VAR)))
