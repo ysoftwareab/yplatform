ec7c9fa0ad8fcaf4ed10ac108fd60e3dcac3753e
Andrei Neculau <andrei.neculau@gmail.com>
2018-02-26 12:18:04 +0100

    do not rely on TRAVIS_TAG

--- git.inc.mk
+++ git.inc.mk
@@ -8,8 +8,6 @@ GIT_TAG = $(shell git describe --exact-match --tags HEAD 2>/dev/null)
 
 ifeq (true,$(TRAVIS))
 GIT_BRANCH = $(TRAVIS_BRANCH)
-export TRAVIS_TAG := $(GIT_TAG) # FIXME https://github.com/travis-ci/travis-ci/issues/9268
-GIT_TAG = $(TRAVIS_TAG)
 endif
 
 $(foreach VAR,GIT_BRANCH GIT_BRANCH_SHORT GIT_HASH GIT_HASH_SHORT GIT_REMOTE GIT_ROOT GIT_TAG,$(call make-lazy,$(VAR)))
