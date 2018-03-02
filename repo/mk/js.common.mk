d93d054c690395a2b66ee735a401fc9c1d32efad
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-02 11:11:06 +0100

    add echo-section for 'make version'

--- repo/mk/js.common.mk
+++ repo/mk/js.common.mk
@@ -56,4 +56,6 @@ version: version/patch ## Bump version (patch level).
 
 .PHONY: version/%
 version/%: ## Bump version to given level (major/minor/patch).
+	@$(ECHO_DO) "Bumping $* version..."
 	$(NPM) version $*
+	@$(ECHO_DONE)
