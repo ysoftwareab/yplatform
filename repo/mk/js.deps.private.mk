647737ad0274dd8a5c9e627d7ebce5b5ab24dc0f
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-16 11:29:52 +0100

    add node_modules_private targets

---
+++ repo/mk/js.deps.private.mk
@@ -0,0 +1,30 @@
+SF_DEPS_TARGETS := \
+	$(SF_DEPS_TARGETS) \
+	deps-node_modules_private \
+
+# ------------------------------------------------------------------------------
+
+.PHONY: deps--node_modules_private
+deps-node_modules_private:
+	DEPS=""; \
+	for DEP_NAME in `$(CAT) package.json | $(JSON) "privateDependencies" | $(JSON) -a -k`; do \
+		DEP_VSN=`$(CAT) "package.json" | $(JSON) "privateDependencies $${DEP_NAME}"`; \
+		BUNDLED_DEP_VSN=`$(CAT) "node_modules_private/lib/node_modules/$${DEP_NAME}/package.json" | $(JSON) "_from"`; \
+		[[ "$${DEP_VSN}" = "$${BUNDLED_DEP_VSN}" ]] || { \
+			$(ECHO_ERR) "node_modules_private/lib/node_modules/$${DEP_NAME} is outdated."; \
+			$(ECHO_ERR) "Found    $${DEP_VSN}."; \
+			$(ECHO_ERR) "Expected $${BUNDLED_DEP_VSN}."; \
+			$(ECHO_ERR) "Run <make node_modules_private> to update it."; \
+			exit 1; \
+		}; \
+	done; \
+
+
+.PHONY: node_modules_private
+node_modules_private: ## Refresh node_modules_private folder.
+	DEPS=""; \
+	for DEP_NAME in `$(CAT) package.json | $(JSON) "privateDependencies" | $(JSON) -a -k`; do \
+		DEP_VSN=`$(CAT) "package.json" | $(JSON) "privateDependencies $${DEP_NAME}"`; \
+		DEPS="$${DEPS} $${DEP_NAME}@$${DEP_VSN}"; \
+	done; \
+	$(NPM) install --prefix node_modules_private --global $${DEPS}
