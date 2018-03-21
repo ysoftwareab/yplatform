36528ec2a83058f6ca92ff2cec7ae2700f77d07c
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-21 22:51:50 +0100

    [andreineculau/core.inc.mk] add link to printvar techniques

--- mk/target.printvar.inc.mk
+++ mk/target.printvar.inc.mk
@@ -1,3 +1,5 @@
+# see https://blog.melski.net/2010/11/30/makefile-hacks-print-the-value-of-any-variable/
+
 .PHONY: printvars
 printvars: ## Print all Makefile variables.
 	@$(foreach V,$(sort $(.VARIABLES)), $(if $(filter-out environment% default automatic, $(origin $V)),$(warning $V=$($V) ($(value $V)))))
