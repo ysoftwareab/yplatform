3efa2f1f3bc74c6778c84922544a1763c09a164f
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-04-02 10:48:00 +0200

    breaking: JS_FILES -> SF_ESLINT_FILES

--- repo/mk/js.check.eslint.mk
+++ repo/mk/js.check.eslint.mk
@@ -5,6 +5,15 @@ ESLINT_ARGS := \
 	--ignore-pattern '!.babelrc.js' \
 	--ignore-pattern '!.eslintrc.js' \
 
+SF_ESLINT_FILES_IGNORE := \
+	-e "^$$"
+
+SF_ESLINT_FILES = $(shell $(GIT_LS) | \
+	$(GREP) -v $(SF_ESLINT_FILES_IGNORE) | \
+	$(GREP) -e ".js$$" | \
+	$(SED) "s/^/'/g" | \
+	$(SED) "s/$$/'/g")
+
 SF_CHECK_TARGETS := \
 	$(SF_CHECK_TARGETS) \
 	lint-eslint \
@@ -13,9 +22,9 @@ SF_CHECK_TARGETS := \
 
 .PHONY: lint-eslint
 lint-eslint:
-	[[ "$(words $(JS_FILES))" = "0" ]] || { \
-		$(ESLINT) $(ESLINT_ARGS) $(JS_FILES) || { \
-			$(ESLINT) $(ESLINT_ARGS) --fix $(JS_FILES) 2>/dev/null >&2; \
+	[[ "$(words $(SF_ESLINT_FILES))" = "0" ]] || { \
+		$(ESLINT) $(ESLINT_ARGS) $(SF_ESLINT_FILES) || { \
+			$(ESLINT) $(ESLINT_ARGS) --fix $(SF_ESLINT_FILES) 2>/dev/null >&2; \
 			exit 1; \
 		}; \
 	}
--- repo/mk/js.common.mk
+++ repo/mk/js.common.mk
@@ -6,15 +6,6 @@ include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/js.misc.version.mk
 PATH := $(MAKE_PATH)/node_modules/.bin:$(GIT_ROOT)/node_modules/.bin:$(PATH)
 export PATH
 
-JS_FILES_IGNORE := \
-	-e "^$$"
-
-JS_FILES = $(shell $(GIT_LS) | \
-	$(GREP) -v $(JS_FILES_IGNORE) | \
-	$(GREP) -e ".js$$" | \
-	$(SED) "s/^/'/g" | \
-	$(SED) "s/$$/'/g")
-
 SRC_JS_FILES := $(shell $(FIND_Q) src -type f -name "*.js" -print)
 
 # ------------------------------------------------------------------------------
