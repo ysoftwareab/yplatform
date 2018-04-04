4ac435696c7a5ddea63cb556d1f636687a095541
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-04-04 10:48:04 +0200

    promote target should also push a tag with a timestamp of the promotion

--- repo/mk/generic.misc.promote-tag-to-env-branch.mk
+++ repo/mk/generic.misc.promote-tag-to-env-branch.mk
@@ -18,4 +18,5 @@ promote/%: ## promote/<env>/<tag> Promote tag to env branch.
 		exit 1; \
 	}
 	$(GIT) push -f $(GIT_REMOTE) $(shell $(GIT) rev-list -n1 $(TAG)):refs/heads/env/$(ENV_NAME)
+	$(GIT) push -f $(GIT_REMOTE) $(shell $(GIT) rev-list -n1 $(TAG)):refs/tags/env/$(ENV_NAME)/$(MAKE_DATE)-$(MAKE_TIME)-$(TAG)
 	$(GIT) fetch
