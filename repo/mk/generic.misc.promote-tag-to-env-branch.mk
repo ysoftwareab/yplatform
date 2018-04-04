40394ee9d24630526675c849e09807a3556d26ab
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-04-04 08:49:43 +0200

    updated repo/mk/generic.misc.promote-tag-to-env-branch.mk

---
+++ repo/mk/generic.misc.promote-tag-to-env-branch.mk
@@ -0,0 +1,21 @@
+SF_PROMOTE_ENVS := \
+	dev \
+	prod-staging \
+	prod \
+
+# ------------------------------------------------------------------------------
+
+.PHONY: promote/%
+promote/%: ## promote/<env>/<tag> Promote tag to env branch.
+	$(eval ENV_NAME := $(shell dirname "$*"))
+	$(eval TAG := $(shell basename "$*"))
+	$(ECHO) "$(SF_PROMOTE_ENVS)" | $(GREP) -q -e "$(ENV_NAME)" || { \
+		$(ECHO_ERR) "$(ENV_NAME) is not a known env."; \
+		exit 1; \
+	}
+	$(GIT) tag --list | $(GREP) -q "^$(TAG)$$" || { \
+		$(ECHO_ERR) "$(TAG) is not a tag."; \
+		exit 1; \
+	}
+	$(GIT) push -f $(GIT_REMOTE) $(shell $(GIT) rev-list -n1 $(TAG)):refs/heads/env/$(ENV_NAME)
+	$(GIT) fetch
