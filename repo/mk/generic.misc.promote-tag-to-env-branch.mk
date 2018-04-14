47261c5dda1c85da413ba14a367b795c41ec94f3
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-04-15 00:07:02 +0200

    simplify

--- repo/mk/generic.misc.promote-tag-to-env-branch.mk
+++ repo/mk/generic.misc.promote-tag-to-env-branch.mk
@@ -18,6 +18,6 @@ promote/%: ## promote/<env>/<tag> Promote tag to env branch.
 		exit 1; \
 	}
 	$(GIT) push -f $(GIT_REMOTE) \
-		$(shell $(GIT) rev-list -n1 $(TAG)):refs/heads/env/$(ENV_NAME) \
-		$(shell $(GIT) rev-list -n1 $(TAG)):refs/tags/env/$(ENV_NAME)/$(MAKE_DATE)-$(MAKE_TIME)-$(TAG)
+		$(TAG):refs/heads/env/$(ENV_NAME) \
+		$(TAG):refs/tags/env/$(ENV_NAME)/$(MAKE_DATE)-$(MAKE_TIME)-$(TAG)
 	$(GIT) fetch
