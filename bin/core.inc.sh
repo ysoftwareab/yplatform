e8c9629f0a0e629f6aec67e9ae97084fd3a8dee2
Andrei Neculau <andrei.neculau@gmail.com>
2018-04-17 21:48:30 +0200

    sort

--- bin/core.inc.sh
+++ bin/core.inc.sh
@@ -14,19 +14,19 @@ VERBOSE=${V}
 
 [[ ${VERBOSE} != true ]] || set -x
 
+ARCH=$(uname -m)
+ARCH_BIT=$(uname -m | grep -q "x86_64" && echo "64" || echo "32")
+ARCH_SHORT=$(uname -m | grep -q "x86_64" && echo "x64" || echo "x86")
 OS=$(uname | tr "[A-Z]" "[a-z]")
 OS_SHORT=$(echo ${OS} | sed "s/^\([a-z]\+\).*/\1/g")
-ARCH=$(uname -m)
-ARCH_SHORT=$(uname -m | grep -q "x86_64" && echo "x64" || echo "x86")
-ARCH_BIT=$(uname -m | grep -q "x86_64" && echo "64" || echo "32")
 
-GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || true)
-GIT_HASH=$(git rev-parse HEAD 2>/dev/null || true)
-GIT_HASH_SHORT=$(git rev-parse --short HEAD 2>/dev/null || true)
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || true)
 GIT_BRANCH_SHORT=$(basename ${GIT_BRANCH} 2>/dev/null || true)
-GIT_TAGS=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
+GIT_HASH=$(git rev-parse HEAD 2>/dev/null || true)
+GIT_HASH_SHORT=$(git rev-parse --short HEAD 2>/dev/null || true)
 GIT_REMOTE=$(git config branch.${GIT_BRANCH}.remote 2>/dev/null || true)
+GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || true)
+GIT_TAGS=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
 
 [[ -z "${TRAVIS_BRANCH:-}" ]] || {
     GIT_BRANCH=${TRAVIS_BRANCH}
