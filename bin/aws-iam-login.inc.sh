92307e7657ec1e089fdf9d47d394070039d748d7
Andrei Neculau <andrei.neculau@gmail.com>
2018-11-16 13:12:43 +0100

    add aws-iam-login-ns to store aws credentials in namespaced env vars

--- bin/aws-iam-login.inc.sh
+++ bin/aws-iam-login.inc.sh
@@ -44,13 +44,13 @@ function aws-iam-login() {
 
     rm -f ${CREDENTIALS_TEMP}
 
-    [ -n "${AWS_SECRET_ACCESS_KEY}" ] || {
-        echo >&2 "No AWS_SECRET_ACCESS_KEY in the environment. Something went wrong."
+    [ -n "${AWS_ACCESS_KEY_ID}" ] || {
+        echo >&2 "No AWS_ACCESS_KEY_ID in the environment. Something went wrong."
         return 1
     }
 
-    [ -n "${AWS_ACCESS_KEY_ID}" ] || {
-        echo >&2 "No AWS_ACCESS_KEY_ID in the environment. Something went wrong."
+    [ -n "${AWS_SECRET_ACCESS_KEY}" ] || {
+        echo >&2 "No AWS_SECRET_ACCESS_KEY in the environment. Something went wrong."
         return 1
     }
 
@@ -58,6 +58,33 @@ function aws-iam-login() {
         echo >&2 "No AWS_SESSION_TOKEN in the environment. Something went wrong."
         return 1
     }
+}
+
+function aws-iam-login-ns() {
+    [ $# -eq 2 ] || {
+        echo >&2 "Usage: aws-iam-login-ns <profilename> <namespace>"
+        return 1
+    }
+
+    local LOCAL_AWS_PROFILE="${AWS_PROFILE:-}"
+    local NS_AWS_PROFILE=$1
+    local NS=$2
+
+    aws-iam-login ${NS_AWS_PROFILE}
+
+    declare -x ${NS}_AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+    declare -x ${NS}_AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+    declare -x ${NS}_AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
+
+    if [ -n "${LOCAL_AWS_PROFILE:-}" ]; then
+        aws-iam-login ${LOCAL_AWS_PROFILE}
+    else
+        echo >&2
+        echo >&2 "[WARN] You are logged in as ${NS_AWS_PROFILE} in your CURRENT environment as well."
+    fi
+
+    echo >&2
+    echo >&2 "[INFO] Credentials for ${NS_AWS_PROFILE} are now stored in ${NS}_ environment variables."
 }
 
 function _aws_profile_completer() {
@@ -74,3 +101,4 @@ if [ -n "$ZSH_VERSION" ]; then
     }
 fi
 complete -F _aws_profile_completer aws-iam-login
+complete -F _aws_profile_completer aws-iam-login-ns
