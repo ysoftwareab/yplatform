7106cbec074c80bcdaa0d7a8302285ae01371d14
Andrei Neculau <andrei.neculau@gmail.com>
2018-11-26 10:30:38 +0100

    support python

---
+++ repo/mk/py.common.mk
@@ -0,0 +1,12 @@
+SUPPORT_FIRECLOUD_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/../..))
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/generic.common.mk
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/py.deps.pipenv.mk
+
+# .py filename should not contain dots or hyphens, but it can have underscores
+SF_PATH_LINT_RE := ^\([a-z0-9/.-]\+\|[a-z0-9/_]\+\.py\)$$
+
+SF_PATH_FILES_IGNORE := \
+	$(SF_PATH_FILES_IGNORE) \
+	-e "^Pipfile$$" \
+
+# ------------------------------------------------------------------------------
