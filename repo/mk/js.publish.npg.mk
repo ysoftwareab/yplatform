10b7f5299904d7f09e7931a9ebdd94e631a3abb4
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-12-02 21:25:08 +0100

    create a merge commit when upstream diverges before/during release. fix #26

--- repo/mk/js.publish.npg.mk
+++ repo/mk/js.publish.npg.mk
@@ -25,6 +25,17 @@ release/%: ## Release a new version with given level (major/minor/patch).
 	@$(ECHO_DO) "Release new $* version..."
 	$(MAKE) nuke all test version/$* publish
 	sleep 15 # allow CI to pick the new tag first
+	$(GIT) fetch
+#	if upstream diverged, create merge commit or else `git push` fails
+	[[ `$(GIT) rev-parse HEAD~` = `$(GIT) rev-parse @{u}` ]] || { \
+		$(ECHO_INFO) "Upstream has new commits..."; \
+		$(GIT) log --oneline --no-color --no-decorate `$(GIT) rev-parse HEAD~`..`$(GIT) rev-parse @{u}`; \
+		GIT_TAG=`$(GIT) tag -l --points-at -n1 HEAD`; \
+		$(ECHO_INFO) "Merging in tag ${GIT_TAG} instead of fast-forwarding..."; \
+		$(GIT) commit-tree -p @{u} -p HEAD \
+			-m "Merge tag ${GIT_TAG}" "HEAD^{tree}" | read GIT_MERGE_COMMIT; \
+		$(GIT) reset $${GIT_MERGE_COMMIT}; \
+	}
 	$(GIT) push
 	@$(ECHO_DONE)
 
