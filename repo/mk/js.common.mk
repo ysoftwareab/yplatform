3dbb2251809ced8ff9c289271fb417542ac81f25
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-02 23:35:25 +0100

    style

--- repo/mk/js.common.mk
+++ repo/mk/js.common.mk
@@ -3,6 +3,8 @@ include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/generic.common.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/js.deps.npm.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/js.misc.version.mk
 
+# ------------------------------------------------------------------------------
+
 PATH := $(MAKE_PATH)/node_modules/.bin:$(GIT_ROOT)/node_modules/.bin:$(PATH)
 export PATH
 
--- repo/mk/py.common.mk
+++ repo/mk/py.common.mk
@@ -2,6 +2,8 @@ SUPPORT_FIRECLOUD_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/generic.common.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/py.deps.pipenv.mk
 
+# ------------------------------------------------------------------------------
+
 # .py filename should not contain dots or hyphens, but it can have underscores
 SF_PATH_LINT_RE := ^\([a-z0-9/.-]\+\|[a-z0-9/_]\+\.py\)$$
 
