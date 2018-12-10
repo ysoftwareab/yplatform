2172eab26f2d5470872fa0a4979c4355ab012688
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-10 14:00:13 +0100

    report not explicit babel-jest transformer

--- repo/jest.config.sf.js
+++ repo/jest.config.sf.js
@@ -1,3 +1,33 @@
+let path = require('path');
+
+let transform = {};
+
+// only add babel-jest transformer if babel-jest is a top-level dependency
+try {
+  require.resolve('babel-jest');
+
+  let jestConfigModule = module;
+  while (jestConfigModule.parent) {
+    if (path.basename(jestConfigModule.filename) !== 'jest.config.js') {
+      jestConfigModule = jestConfigModule.parent;
+      continue;
+    }
+    break;
+  }
+
+  let topPackageJson = path.join(path.dirname(jestConfigModule.filename), 'package.json');
+  // eslint-disable-next-line global-require
+  let topPackage = require(topPackageJson);
+
+  if (!topPackage.devDependencies['babel-jest']) {
+    throw new Error(`babel-jest is not a dev dependency in ${topPackageJson}`);
+  }
+
+  transform['^.+\\.js$'] = 'babel-jest';
+} catch (_err) {
+  // console.log(_err);
+}
+
 module.exports = {
   // FIXME coverage is disabled due to https://github.com/facebook/jest/issues/3959
   collectCoverage: false,
@@ -15,7 +45,5 @@ module.exports = {
   testMatch: [
     '**/test/**/*.test.js'
   ],
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  }
+  transform
 };
