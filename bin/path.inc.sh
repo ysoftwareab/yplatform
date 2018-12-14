e8701af602ae541398dbf1176b7a0ea4625b3bad
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-14 23:50:07 +0100

    make sure we don't add to PATH more than once

--- bin/path.inc.sh
+++ bin/path.inc.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
+function path_prepend() {
+    [[ ":${PATH}:" =~ *"$1"* ]] || export PATH=$1:${PATH}
+}
+
+function path_append() {
+    [[ ":${PATH}:" =~ *"$1"* ]] || export PATH=${PATH}:$1
+}
+
 if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
-    export PATH=/home/linuxbrew/.linuxbrew/sbin:${PATH}
-    export PATH=/home/linuxbrew/.linuxbrew/bin:${PATH}
+    path_prepend /home/linuxbrew/.linuxbrew/sbin
+    path_prepend /home/linuxbrew/.linuxbrew/sbin
+    path_prepend /home/linuxbrew/.linuxbrew/bin
 elif [[ -x ~/.linuxbrew/bin/brew ]]; then
-    export PATH=~/.linuxbrew/sbin:${PATH}
-    export PATH=~/.linuxbrew/bin:${PATH}
+    path_prepend ~/.linuxbrew/sbin
+    path_prepend ~/.linuxbrew/bin
 fi
-export PATH=/usr/local/sbin:${PATH}
-export PATH=/usr/local/bin:${PATH}
-export PATH=${HOME}/.local/sbin:${PATH}
-export PATH=${HOME}/.local/bin:${PATH}
+path_prepend /usr/local/sbin
+path_prepend /usr/local/bin
+path_prepend ${HOME}/.local/sbin
+path_prepend ${HOME}/.local/bin
 
 if which brew >/dev/null 2>&1; then
     HOMEBREW_PREFIX=$(brew --prefix)
-    export PATH=${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin:${PATH}
+    path_prepend ${HOMEBREW_PREFIX}/sbin
+    path_prepend ${HOMEBREW_PREFIX}/bin
 
     for f in coreutils findutils gnu-sed gnu-tar gnu-time gnu-which grep gzip make; do
-        export PATH=${HOMEBREW_PREFIX}/opt/${f}/libexec/gnubin:${PATH}
+        path_prepend ${HOMEBREW_PREFIX}/opt/${f}/libexec/gnubin
     done
-    export PATH=${HOMEBREW_PREFIX}/opt/curl/bin:${PATH}
-    export PATH=${HOMEBREW_PREFIX}/opt/unzip/bin:${PATH}
+    path_prepend ${HOMEBREW_PREFIX}/opt/curl/bin
+    path_prepend ${HOMEBREW_PREFIX}/opt/unzip/bin
     unset HOMEBREW_PREFIX
 fi
