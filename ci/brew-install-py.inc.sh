ecc00b80e4807837e8d782c1834dc349cca78622
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-07 22:54:14 +0100

    linuxbrew: use pipenv 2018.10.13 (virtualenv 16.0.0)
    See https://github.com/pypa/pipenv/issues/3395
    See https://github.com/pypa/virtualenv/issues/1270

--- ci/brew-install-py.inc.sh
+++ ci/brew-install-py.inc.sh
@@ -21,3 +21,16 @@ exe_and_grep_q "pip2 --version | head -1" "^pip "
 exe_and_grep_q "pip3 --version | head -1" "^pip "
 exe_and_grep_q "pyenv --version | head -1" "^pyenv "
 exe_and_grep_q "pipenv --version | head -1" "^pipenv, version "
+
+# FIXME temporary fix
+# See https://github.com/pypa/pipenv/issues/3395
+# See https://github.com/pypa/virtualenv/issues/1270
+echo_do "brew: Installing 'pipenv 2018.10.13'..."
+HOMEBREW_CORE_GUC=https://raw.githubusercontent.com/Linuxbrew/homebrew-core
+PIPENV_2018_10_13=${HOMEBREW_CORE_GUC}/494b0638a632244d25aaf2bd2292ae54c99ffa94/Formula/pipenv.rb
+brew uninstall --force pipenv
+brew install ${PIPENV_2018_10_13}
+brew pin pipenv
+echo_done
+
+exe_and_grep_q "pipenv --version | head -1" "^pipenv, version 2018.10.13"
