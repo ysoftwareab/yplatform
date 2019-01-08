034ad28809e0757dcea5df98595d9c1740996910
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-08 15:26:25 +0100

    fix check for CI

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # when not in CI, use dev install
-[[ "${CI:-}" != "true" ]] || SF_CI_BREW_INSTALL=${SF_CI_BREW_INSTALL:-dev}
+[[ "${CI:-}" = "true" ]] || SF_CI_BREW_INSTALL=${SF_CI_BREW_INSTALL:-dev}
 
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-${SF_CI_BREW_INSTALL}.inc.sh
