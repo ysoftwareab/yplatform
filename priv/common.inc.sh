94fe3926ab3e9b6173f4fd48b7304cd25b1493c2
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-08 22:03:31 +0100

    use preferred priv/*.inc.sh

--- priv/common.inc.sh
+++ priv/common.inc.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 [[ -n ${INSTALL_SUPPORT_FIRECLOUD_SH:-} ]] || {
     export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-    source ${SUPPORT_FIRECLOUD_DIR}/bin/core.inc.sh
-    source ${SUPPORT_FIRECLOUD_DIR}/bin/exe.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/priv/core.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/priv/exe.inc.sh
 
     # export INSTALL_SUPPORT_FIRECLOUD_SH=true
 }
