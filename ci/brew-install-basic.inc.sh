163e26710e413b913ba50aec6f20a3548099fabc
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-09 01:36:51 +0100

    temp fix. silence error on rsync v2

--- ci/brew-install-basic.inc.sh
+++ ci/brew-install-basic.inc.sh
@@ -15,4 +15,10 @@ echo_done
 # test
 exe_and_grep_q "curl --version | head -1" "^curl 7\\."
 exe_and_grep_q "git --version | head -1" "^git version 2\\."
-exe_and_grep_q "rsync --version | head -1" "^rsync  version 3\\."
+# IDK rsync v3 seems to be installed by homebrew, PATH is correct
+# yet `rsync --version` which should be equivalent to `/usr/local/bin/rsync --version`
+# says it's v2...
+# https://travis-ci.com/tobiipro/support-firecloud/jobs/169054509#L1571
+# https://travis-ci.com/tobiipro/support-firecloud/jobs/169054509#L1587
+#exe_and_grep_q "rsync --version | head -1" "^rsync  version 3\\."
+exe_and_grep_q "rsync --version | head -1" "^rsync  version "
