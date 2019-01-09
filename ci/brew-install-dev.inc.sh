43b96c467c4022d2656422672711f8c7a9f0c393
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-09 01:08:36 +0100

    add instructions for devs in bootstrap script

--- ci/brew-install-dev.inc.sh
+++ ci/brew-install-dev.inc.sh
@@ -4,3 +4,10 @@ set -euo pipefail
 echo_do "brew: Installing dev packages..."
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-common.inc.sh
 echo_done
+
+cat <<-EOF
+Append 'source ˙~/${SUPPORT_FIRECLOUD_DIR#${HOME}/}/priv/dev.inc.sh'
+to your '~/.bashrc' or '~/.zshrc' or similar.
+
+Restart your shell, and you're good to go.
+EOF
