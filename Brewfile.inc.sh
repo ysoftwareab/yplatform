cac60877e07d973ce87883e88ee8e28dbb5bfca1
Andrei Neculau <andrei.neculau@gmail.com>
2019-01-27 16:23:10 +0100

    support-firecloud needs node

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -4,3 +4,4 @@
 [[ "${CI:-}" = "true" ]] || SF_CI_BREW_INSTALL=${SF_CI_BREW_INSTALL:-dev}
 
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-${SF_CI_BREW_INSTALL}.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-node.inc.sh
