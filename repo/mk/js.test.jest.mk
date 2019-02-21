ef2ab92910eafe5d52f357a07b5633b42233a538
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-21 14:03:59 +0100

    add target to run jest on single test files

--- repo/mk/js.test.jest.mk
+++ repo/mk/js.test.jest.mk
@@ -3,6 +3,7 @@ $(foreach VAR,JEST,$(call make-lazy,$(VAR)))
 
 JEST_ARGS ?=
 
+JEST_TEST_FILES := $(shell $(FIND_Q_NOSYM) test -type f -name "*.test.js" -print)
 
 SF_CLEAN_FILES := \
 	$(SF_CLEAN_FILES) \
@@ -17,3 +18,8 @@ SF_TEST_TARGETS := \
 .PHONY: test-jest
 test-jest:
 	$(JEST) $(JEST_ARGS)
+
+
+.PHONY: $(JEST_TEST_FILES)
+$(JEST_TEST_FILES):
+	$(JEST) $(JEST_ARGS) $@
