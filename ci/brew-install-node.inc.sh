1629882eac197e3e5c27897e1ca27357db368a4f
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-01 22:27:36 +0200

    try to compile nodejs with clang

--- ci/brew-install-node.inc.sh
+++ ci/brew-install-node.inc.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo_do "brew: Installing NodeJS packages..."
-BREW_FORMULAE="$(cat <<-EOF
+(
+    [[ "${OS}" != "linux" ]] || {
+        # if a node bottle doesn't exist, we need to compile node with clang instead of gcc
+        # to get around "out of memory" issues
+        brew_install llvm
+        export PATH=$(brew --prefix)/opt/llvm/bin:${PATH}
+        export C=clang
+        export CXX=clang++
+        export LDFLAGS="-L$(brew --prefix)/opt/llvm/lib -Wl,-rpath,$(brew --prefix)/opt/llvm/lib"
+    }
+
+    echo_do "brew: Installing NodeJS packages..."
+    BREW_FORMULAE="$(cat <<-EOF
 node
 EOF
 )"
-brew_install "${BREW_FORMULAE}"
-unset BREW_FORMULAE
-npm install --global npm@6
-npm install --global json@9
-echo_done
+    brew_install "${BREW_FORMULAE}"
+    unset BREW_FORMULAE
+    npm install --global npm@6
+    npm install --global json@9
+    echo_done
+)
 
 echo_do "brew: Testing NodeJS packages..."
 exe_and_grep_q "node --version | head -1" "^v"
