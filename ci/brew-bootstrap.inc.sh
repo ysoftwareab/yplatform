919042cfd1ef006ed1eef8dfbd421374e94cb805
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-02 20:51:08 +0200

    Revert "brew install/upgrade with --force-bottle"
    This reverts commit e310be8ef91638e5af8cefb35cd9948d82f41d63.

--- ci/brew-bootstrap.inc.sh
+++ ci/brew-bootstrap.inc.sh
@@ -68,7 +68,7 @@ brew_upgrade() {
         # is it already up-to-date?
         brew outdated ${NAME} >/dev/null 2>&1 || {
             echo_do "brew: Upgrading ${NAME}..."
-            brew upgrade --force-bottle ${NAME}
+            brew upgrade ${NAME}
             echo_done
         }
     done 3< <(echo "$@")
@@ -112,7 +112,7 @@ brew_install() {
             fi
         fi
         echo_do "brew: Installing ${FORMULA}..."
-        brew install --force-bottle ${FORMULA}
+        brew install ${FORMULA}
         echo_done
     done 3< <(echo "$@")
     # see https://github.com/Homebrew/brew/issues/5013
