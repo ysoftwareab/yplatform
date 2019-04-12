2170c1974c7991d4230095cf900f13c64daeeded
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-12 09:04:07 +0200

    breaking: stop using typescript-formatter

--- repo/mk/js.check.tslint.mk
+++ repo/mk/js.check.tslint.mk
@@ -1,6 +1,5 @@
-TSFMT = $(call which,TSFMT,tsfmt)
 TSLINT = $(call which,TSLINT,tslint)
-$(foreach VAR,TSFMT TSLINT,$(call make-lazy,$(VAR)))
+$(foreach VAR,TSLINT,$(call make-lazy,$(VAR)))
 
 TSLINT_ARGS ?=
 TSLINT_ARGS := \
@@ -19,10 +18,6 @@ check-tslint:
 	[[ "$(words $(TS_FILES))" = "0" ]] || { \
 		$(TSLINT) $(TSLINT_ARGS) $(TS_FILES) || { \
 			$(TSLINT) $(TSLINT_ARGS) --fix $(TS_FILES) 2>/dev/null >&2; \
-			exit 1; \
-		}; \
-		$(TSFMT) --verify -- $(TS_FILES) || { \
-			$(TSFMT) --replace -- $(TS_FILES) 2>/dev/null >&2; \
 			exit 1; \
 		}; \
 	}
