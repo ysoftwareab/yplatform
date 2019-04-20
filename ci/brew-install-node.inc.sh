749cab7f7ba6e96369e5c473965e7840366f440a
Andrei Neculau <andrei.neculau@gmail.com>
2019-04-20 11:14:01 +0200

    try to force node linuxbrew bottle on travis

--- ci/brew-install-node.inc.sh
+++ ci/brew-install-node.inc.sh
@@ -2,8 +2,18 @@
 set -euo pipefail
 
 echo_do "brew: Installing NodeJS packages..."
+
+# force node bottle on Travis, compiling node fails or takes forever
+NODE_FORMULA=node
+[[ "${TRAVIS:-}" != "true" ]] || {
+    cd $(brew --repo homebrew/core)
+    git fetch --depth 1000
+    NODE_BOTTLE_COMMIT=$(git log -1 --pretty=format:"%H" --grep update --grep bottle Formula/node.rb)
+    NODE_FORMULA="https://raw.githubusercontent.com/Homebrew/linuxbrew-core/${NODE_BOTTLE_COMMIT}/Formula/node.rb"
+}
+
 BREW_FORMULAE="$(cat <<-EOF
-node
+${NODE_FORMULA}
 EOF
 )"
 brew_install "${BREW_FORMULAE}"
