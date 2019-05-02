54f6e72910d83082c5f42e1470cff609950665f9
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-02 11:18:57 +0200

    add help-all target

--- target.help.inc.mk
+++ target.help.inc.mk
@@ -3,4 +3,16 @@ help: ## Show this help message.
 	@echo "usage: `basename $(MAKE)` [targets]"
 	@echo
 	@echo "Available targets:"
-	@$(SED) "s/^\(.\+\):.*\s##\s\+\(.\+\)\$$/  \1##\2/;tx;d;:x" $(MAKEFILE_LIST) | sort -u | column -t -s "##"
+	@for Makefile in $(MAKEFILE_LIST); do \
+		$(SED) "s/^\([^#.$$\t].\+\):.*\s##\s\+\(.\+\)\$$/  \1##\2/;tx;d;:x" $${Makefile}; \
+	done | sort -u | column -t -s "##"
+
+
+.PHONY: help-all
+help-all: ## Show this help message, including all intermediary targets and source Makefiles.
+	@echo "usage: `basename $(MAKE)` [targets]"
+	@echo
+	@echo "Available targets:"
+	@for Makefile in $(MAKEFILE_LIST); do \
+		$(SED) "s|^\([^#.$$\t].\+\):\(.*\s##\s\+\(.\+\)\)\?\$$|  \1##$${Makefile#$(SUPPORT_FIRECLOUD_DIR)/}##\3|;tx;d;:x" $${Makefile}; \
+	done | sort -u | column -t -s "##"
