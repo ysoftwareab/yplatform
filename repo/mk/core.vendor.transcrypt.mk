ed93660a2b3798e3ad5de1e3acb9dca7546694ac
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-06 11:04:10 +0200

    fix SF_IS_TRANSCRYPTED

--- repo/mk/core.vendor.transcrypt.mk
+++ repo/mk/core.vendor.transcrypt.mk
@@ -1,4 +1,4 @@
-SF_IS_TRANSCRYPTED = $(shell $(GIT) config --local transcrypted.version >/dev/null && echo true || echo false)
+SF_IS_TRANSCRYPTED = $(shell $(GIT) config --local transcrypt.version >/dev/null && echo true || echo false)
 
 SF_VENDOR_FILES_IGNORE := \
 	$(SF_VENDOR_FILES_IGNORE) \
