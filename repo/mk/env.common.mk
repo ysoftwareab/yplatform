56cbd0f454cdbd1ce7b72e5b7622e1df26213dad
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-15 14:40:55 +0200

    add env.common.mk

---
+++ repo/mk/env.common.mk
@@ -0,0 +1,5 @@
+SUPPORT_FIRECLOUD_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/../..))
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/env.promote-tag-to-env-branch.mk
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/env.teardown-env.mk
+
+# ------------------------------------------------------------------------------
