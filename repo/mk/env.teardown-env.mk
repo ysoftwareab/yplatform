f668cf213394f24225a64c70045c916d4fbda95d
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-15 14:40:40 +0200

    add teardown-env target

---
+++ repo/mk/env.teardown-env.mk
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+
+.PHONY: teardown-env
+teardown-env: ## Teardown env
+	$(GIT) diff --cached --exit-code --quiet || { \
+		$(ECHO_ERR) "Unstage your changes before calling 'make teardown-env'."; \
+		exit 1; \
+	}
+	$(GIT) commit --allow-empty -m "[TEARDOWN-ENV $(ENV_NAME)]"
+	$(GIT) push -f
