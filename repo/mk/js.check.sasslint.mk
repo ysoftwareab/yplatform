b487261e49c174d6c677da3327dc295eb6f1f747
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-24 10:13:57 +0200

    add check-sasslint target

---
+++ repo/mk/js.check.sasslint.mk
@@ -0,0 +1,26 @@
+SASSLINT = $(call npm-which,SASSLINT,sass-lint)
+$(foreach VAR,SASSLINT,$(call make-lazy,$(VAR)))
+
+SASSLINT_ARGS ?=
+
+SF_SASSLINT_FILES_IGNORE := \
+	-e "^$$"
+
+SF_SASSLINT_FILES = $(shell $(GIT_LS) . | \
+	$(GREP) -v $(SF_ESLINT_FILES_IGNORE) | \
+	$(GREP) -e "\\.\\(sass\\|scss\\)$$" | \
+	$(SED) "s/^/'/g" | \
+	$(SED) "s/$$/'/g") \
+
+
+SF_CHECK_TARGETS := \
+	$(SF_CHECK_TARGETS) \
+	check-sasslint \
+
+# ------------------------------------------------------------------------------
+
+.PHONY: check-sasslint
+check-sasslint:
+	[[ "$(words $(SF_SASSLINT_FILES))" = "0" ]] || { \
+		$(SASSLINT) $(SASSLINT_ARGS) $(SF_SASSLINT_FILES); \
+	}
