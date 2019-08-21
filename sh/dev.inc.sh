fb1d7b9b127d7143b40816309abe0370da2a421e
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-21 13:04:03 +0200

    try to remove old node-esm symlink

--- sh/dev.inc.sh
+++ sh/dev.inc.sh
@@ -17,5 +17,7 @@ source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/exe-env.inc.sh
 source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/aws-iam-login.inc.sh
 
 path_prepend ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/dev/bin
+# remove old symlink if possible
+rm /usr/local/bin/node-esm 2>/dev/null || true
 
 export SF_DEV_INC_SH=true
