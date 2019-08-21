6ec29210d371728798adf3b49e304ad9971ac8a8
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-21 13:01:14 +0200

    ln -sf fails in /usr/local for ubuntu 18.04. change method for sf-bins

--- sh/dev.inc.sh
+++ sh/dev.inc.sh
@@ -7,28 +7,15 @@ elif [ -n "${ZSH_VERSION}" ]; then
     autoload -U compaudit compinit bashcompinit
     bashcompinit || {
         echo >&2 "Initialization of zsh completion features has failed in"
-        echo >&2 "${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/aws-iam-login.inc.sh."
+        echo >&2 "${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/dev.inc.sh."
     }
 else
-    echo >&2 "Unsupported shell in aws-iam-login.inc.sh, or \$BASH_VERSION or \$ZSH_VERSION undefined."
+    echo >&2 "Unsupported shell or \$BASH_VERSION and \$ZSH_VERSION are undefined."
 fi
 
 source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/exe-env.inc.sh
 source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/aws-iam-login.inc.sh
 
-function sf-bin() {
-    local GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || true)
-    local SF_BIN=$1
-    shift 1
-    if [[ -x ${GIT_ROOT}/support-firecloud/bin/${SF_BIN} ]]; then
-        ${GIT_ROOT}/support-firecloud/bin/${SF_BIN} "$@"
-        return 0
-    fi
-    ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/bin/${SF_BIN} "$@"
-}
-
-alias sake="sf-bin sake"
-# alias node-esm="sf-bin node-esm"
-ln -sf ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/bin/node-esm /usr/local/bin/node-esm
+path_prepend ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/dev/bin
 
 export SF_DEV_INC_SH=true
