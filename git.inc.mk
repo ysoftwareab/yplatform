f6dd0b8abd81150dc4dd1ffea0dd806490c8c8d1
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-30 15:36:53 +0200

    add GIT_DESCRIBE

--- git.inc.mk
+++ git.inc.mk
@@ -1,8 +1,11 @@
 GIT_BRANCH = $(shell cd $(TOP) && git rev-parse --abbrev-ref HEAD 2>/dev/null)
 GIT_BRANCH_SHORT = $(shell basename $(GIT_BRANCH))
+GIT_DESCRIBE = $(shell cd $(TOP) && git describe --always --dirty)
 GIT_HASH = $(shell cd $(TOP) && git rev-parse HEAD 2>/dev/null)
 GIT_HASH_SHORT = $(shell cd $(TOP) && git rev-parse --short HEAD 2>/dev/null)
+GIT_TAGS = $(shell git describe --exact-match --tags HEAD 2>/dev/null)
+$(foreach VAR,GIT_BRANCH GIT_BRANCH_SHORT GIT_DESCRIBE GIT_HASH GIT_HASH_SHORT GIT_TAGS,$(call make-lazy,$(VAR)))
+
 GIT_REMOTE = $(shell git config branch.$(GIT_BRANCH).remote 2>/dev/null)
 GIT_ROOT = $(shell cd $(TOP) && git rev-parse --show-toplevel 2>/dev/null)
-GIT_TAGS = $(shell git describe --exact-match --tags HEAD 2>/dev/null)
-$(foreach VAR,GIT_BRANCH GIT_BRANCH_SHORT GIT_HASH GIT_HASH_SHORT GIT_REMOTE GIT_ROOT GIT_TAGS,$(call make-lazy,$(VAR)))
+$(foreach VAR,GIT_REMOTE GIT_ROOT,$(call make-lazy,$(VAR)))
