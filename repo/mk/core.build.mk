487efdd6f173a93258c5bc8ef1b11c2718b5c83f
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-31 22:32:20 +0200

    refactor BUILD and VERSION target

--- repo/mk/core.build.mk
+++ repo/mk/core.build.mk
@@ -7,15 +7,43 @@ SF_BUILD_TARGETS := \
 	$(SF_BUILD_TARGETS) \
 	VERSION \
 
+BUILD_DATE ?= $(MAKE_DATE)
+BUILD_TIME ?= $(MAKE_TIME)
+
+BUILD_HOSTNAME ?= $(shell hostname)
+BUILD_USER ?= $(shell whoami)
+$(foreach VAR,BUILD_HOSTNAME BUILD_USER,$(call make-lazy,$(VAR)))
+
 BUILD_GIT_BRANCH ?= $(GIT_BRANCH_SHORT)
 BUILD_GIT_DESCRIBE ?= $(GIT_DESCRIBE)
 BUILD_GIT_HASH ?= $(GIT_HASH_SHORT)
-BUILD_HOSTNAME ?= $(shell hostname)
+BUILD_GIT_TAGS ?= $(subst $( ),$(,),$(GIT_TAGS))
 BUILD_OS ?= $(OS)
 BUILD_OS_ARCH ?= $(ARCH)
-BUILD_USER ?= $(shell whoami)
 BUILD_VSN ?= $(PKG_VSN)_$(GIT_HASH)
-$(foreach VAR,BUILD_HOSTNAME BUILD_USER,$(call make-lazy,$(VAR)))
+
+
+SF_BUILD_VARS := \
+	BUILD_VSN \
+	BUILD_DATE \
+	BUILD_TIME \
+	BUILD_GIT_BRANCH \
+	BUILD_GIT_DESCRIBE \
+	BUILD_GIT_HASH \
+	BUILD_GIT_TAGS \
+	BUILD_OS \
+	BUILD_OS_ARCH \
+	BUILD_USER \
+	BUILD_HOSTNAME \
+
+SF_VERSION_VARS := \
+	PKG_NAME \
+	PKG_VSN \
+
+define sf-write-var-to-file
+	$(ECHO)  | $(TEE) -a $2
+endef
+
 
 # ------------------------------------------------------------------------------
 
@@ -23,23 +51,13 @@ $(foreach VAR,BUILD_HOSTNAME BUILD_USER,$(call make-lazy,$(VAR)))
 BUILD:
 	$(ECHO_DO) "Generating $@..."
 	$(RM) $@
-	$(ECHO) "BUILD_VSN=$(BUILD_VSN)" >> $@
-	$(ECHO) "BUILD_DATE=$(MAKE_DATE)" >> $@
-	$(ECHO) "BUILD_TIME=$(MAKE_TIME)" >> $@
-	$(ECHO) "BUILD_GIT_HASH=$(BUILD_GIT_HASH)" >> $@
-	$(ECHO) "BUILD_GIT_BRANCH=$(BUILD_GIT_BRANCH)" >> $@
-	$(ECHO) "BUILD_GIT_DESCRIBE=$(BUILD_GIT_DESCRIBE)" >> $@
-	$(ECHO) "BUILD_OS=$(BUILD_OS)" >> $@
-	$(ECHO) "BUILD_OS_ARCH=$(BUILD_OS_ARCH)" >> $@
-	$(ECHO) "BUILD_USER=$(BUILD_USER)" >> $@
-	$(ECHO) "BUILD_HOSTNAME=$(BUILD_HOSTNAME)" >> $@
+	$(ECHO) $(foreach VAR,$(SF_BUILD_VARS),"$(VAR)=$($(VAR))") | $(TR) ' ' '\n' > $@
 	$(ECHO_DONE)
 
 
 VERSION: BUILD
 	$(ECHO_DO) "Generating $@..."
 	$(RM) $@
-	$(ECHO) "PKG_NAME=$(PKG_NAME)" >> $@
-	$(ECHO) "PKG_VSN=$(PKG_VSN)" >> $@
+	$(ECHO) $(foreach VAR,$(SF_VERSION_VARS),"$(VAR)=$($(VAR))") | $(TR) ' ' '\n' > $@
 	$(CAT) BUILD >> $@
 	$(ECHO_DONE)
