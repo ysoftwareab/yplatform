fd36c9270f37e5d8f421ba49df4011eff60a6610
Andrei Neculau <andrei.neculau@gmail.com>
2019-09-03 00:34:19 +0200

    order

--- repo/mk/generic.common.mk
+++ repo/mk/generic.common.mk
@@ -2,10 +2,10 @@ SUPPORT_FIRECLOUD_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.common.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.vendor.transcrypt.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.deps.git-hook-pre-push.mk
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.build-version-file.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.path.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.eclint.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.jsonlint.mk
-include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.build-version-file.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.misc.snapshot.mk
 
 # ------------------------------------------------------------------------------
