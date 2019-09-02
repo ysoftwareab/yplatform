8fd18e60c05e1c8716b3f0f4cc186a0f2c0c710d
Andrei Neculau <andrei.neculau@gmail.com>
2019-09-02 23:13:11 +0200

    clarify naming

--- repo/mk/generic.common.mk
+++ repo/mk/generic.common.mk
@@ -5,7 +5,7 @@ include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.deps.git-hook-pre-push.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.path.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.eclint.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.check.jsonlint.mk
-include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.build.mk
+include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.build-version-file.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/core.misc.snapshot.mk
 
 # ------------------------------------------------------------------------------
