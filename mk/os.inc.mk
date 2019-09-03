b6fc9379d519e6551258bedfdd5326b170c3c317
Andrei Neculau <andrei.neculau@gmail.com>
2019-09-03 17:31:05 +0200

    [andreineculau/core.inc.mk] use exe vars

--- mk/os.inc.mk
+++ mk/os.inc.mk
@@ -1,8 +1,8 @@
-ARCH = $(shell uname -m)
-ARCH_SHORT = $(shell uname -m | grep -q "x86_64" && echo "x64" || echo "x86")
-ARCH_BIT = $(shell uname -m | grep -q "x86_64" && echo "64" || echo "32")
+ARCH = $(shell $(UNAME) -m)
+ARCH_SHORT = $(shell $(UNAME) -m | grep -q "x86_64" && echo "x64" || echo "x86")
+ARCH_BIT = $(shell $(UNAME) -m | grep -q "x86_64" && echo "64" || echo "32")
 $(foreach VAR,ARCH ARCH_SHORT ARCH_BIT,$(call make-lazy,$(VAR)))
 
-OS = $(shell uname | tr "A-Z" "a-z")
-OS_SHORT = $(shell echo $(OS) | sed "s/^\([a-z]\+\).*/\1/g")
+OS = $(shell $(UNAME) | $(TR) "A-Z" "a-z")
+OS_SHORT = $(shell $(ECHO) $(OS) | $(SED) "s/^\([a-z]\+\).*/\1/g")
 $(foreach VAR,OS OS_SHORT,$(call make-lazy,$(VAR)))
