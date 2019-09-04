e359bdf5c223ef4e3fb07f35dcbd609f3d61d960
Andrei Neculau <andrei.neculau@gmail.com>
2019-09-04 11:13:30 +0200

    reuse SF_IS_TRANSCRYPTED

--- repo/mk/core.misc.source-const-inc.mk
+++ repo/mk/core.misc.source-const-inc.mk
@@ -1,20 +1,16 @@
 # CONST.inc
-ifneq ($(wildcard $(GIT_ROOT)/CONST.inc),)
+ifneq (,$(wildcard $(GIT_ROOT)/CONST.inc))
 include $(GIT_ROOT)/CONST.inc
 export $(shell $(SED) 's/=.*//' $(GIT_ROOT)/CONST.inc)
 endif
 
 # CONST.inc.secret
-ifneq ($(wildcard $(GIT_ROOT)/CONST.inc.secret),)
-SF_TMP_IS_TRANSCRYPTED := $(shell $(GIT) config --local transcrypt.version >/dev/null && echo -n true || echo -n false)
-ifeq ($(SF_TMP_IS_TRANSCRYPTED),true)
+ifneq (,$(wildcard $(GIT_ROOT)/CONST.inc.secret))
+SF_IS_TRANSCRYPTED ?=
+ifeq (true,$(SF_IS_TRANSCRYPTED))
 
 include $(GIT_ROOT)/CONST.inc.secret
 export $(shell $(SED) 's/=.*//' $(GIT_ROOT)/CONST.inc.secret)
 
-ifneq (,$(filter undefine,$(.FEATURES)))
-undefine SF_TMP_IS_TRANSCRYPTED
-
-endif
 endif
 endif
