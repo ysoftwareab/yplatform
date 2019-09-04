d3c2f212ee88cbee92ea343f6135341e249c64e7
Andrei Neculau <andrei.neculau@gmail.com>
2019-09-04 18:33:33 +0200

    docs

--- repo/mk/env.common.mk
+++ repo/mk/env.common.mk
@@ -1,5 +1,11 @@
+# This is a collection of "must have" targets for environment repositories.
+#
+# ------------------------------------------------------------------------------
+
 SUPPORT_FIRECLOUD_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))/../..))
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/env.promote-tag-to-env-branch.mk
 include $(SUPPORT_FIRECLOUD_DIR)/repo/mk/env.teardown-env.mk
+
+# ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
