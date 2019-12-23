79e0e9496016f6eece2498dc1d46ed95c046da83
Andrei Neculau <andrei.neculau@gmail.com>
2019-12-23 11:09:36 +0100

    introduce flag to trigger docker deployment

--- .ci.sh
+++ .ci.sh
@@ -8,18 +8,15 @@ function ci_run_before_deploy() {
     true
 }
 
-function ci_run_deploy() {
+function ci_run_deploy_docker_image() {
     DOCKER_ORG=${DOCKER_ORG:-tobiipro}
     [[ -n "${DOCKER_USERNAME:-}" ]] || return
     [[ -n "${DOCKER_PASSWORD:-}" ]] || return
     [[ -e "/etc/os-release" ]] || return
 
-    # temporary; should check instead if a docker repository exists for ${DOCKER_ORG}/${DOCKER_IMAGE_TAG}
-    [[ "${SF_CI_BREW_INSTALL}" = "common" ]] || return
-
     RELEASE_ID="$(source /etc/os-release && echo ${ID})"
     RELEASE_VERSION_CODENAME="$(source /etc/os-release && echo ${VERSION_CODENAME})"
-    DOCKER_IMAGE_NAME=sf-${RELEASE_ID}-${RELEASE_VERSION_CODENAME}
+    DOCKER_IMAGE_NAME=sf-${RELEASE_ID}-${RELEASE_VERSION_CODENAME}-${SF_CI_BREW_INSTALL}
     DOCKER_IMAGE_TAG=${GIT_HASH_SHORT}
     DOCKERFILE=${SUPPORT_FIRECLOUD_DIR}/ci/${OS_SHORT}/Dockerfile.${RELEASE_ID}.${RELEASE_VERSION_CODENAME}
     [[ -f "${DOCKERFILE}" ]] || return
@@ -27,6 +24,10 @@ function ci_run_deploy() {
     echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
     docker build . --file ${DOCKERFILE} --tag ${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
     docker push ${DOCKER_ORG}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+}
+
+function ci_run_deploy() {
+    [[ -z "${SF_DEPLOY_DOCKER_IMAGE:-}" ]] || ci_run_deploy_docker_image
 }
 
 source "${SUPPORT_FIRECLOUD_DIR}/repo/dot.ci.sh.sf"
