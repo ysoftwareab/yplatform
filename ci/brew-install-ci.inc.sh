48d54deba0af01a243fea919afa9f5e147608d1c
Andrei Neculau <andrei.neculau@gmail.com>
2019-12-30 12:23:03 +0100

    use jq after installing

--- ci/brew-install-ci.inc.sh
+++ ci/brew-install-ci.inc.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo_do "brew: Unlink keg-only packages..."
-BREW_FORMULAE="$(brew info --json=v1 --installed | \
-    jq -r 'map(select(.keg_only == true and .linked_keg != null)) | map(.name) | .[]')"
-echo "${BREW_FORMULAE}"
-while read -u3 FORMULA; do
-    brew unlink ${FORMULA} || true
-done 3< <(echo -n "${BREW_FORMULAE}")
-unset FORMULA
-unset BREW_FORMULAE
-echo_done
-
 echo_do "brew: Installing CI packages..."
 BREW_FORMULAE="$(cat <<-EOF
 git
@@ -27,4 +16,15 @@ echo_do "brew: Testing CI packages..."
 exe_and_grep_q "git --version | head -1" "^git version 2\\."
 exe_and_grep_q "jq --version | head -1" "^jq\\-1\\."
 exe_and_grep_q "rsync --version | head -1" "^rsync  version 3\\."
+echo_done
+
+echo_do "brew: Unlink keg-only packages..."
+BREW_FORMULAE="$(brew info --json=v1 --installed | \
+    jq -r 'map(select(.keg_only == true and .linked_keg != null)) | map(.name) | .[]')"
+echo "${BREW_FORMULAE}"
+while read -u3 FORMULA; do
+    brew unlink ${FORMULA} || true
+done 3< <(echo -n "${BREW_FORMULAE}")
+unset FORMULA
+unset BREW_FORMULAE
 echo_done
