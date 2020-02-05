50b2daaf7bffe56ab19bdbbe91ea7337ac997839
Andrei Neculau <andrei.neculau@gmail.com>
2020-02-05 23:26:38 +0100

    remove redundant INSTALL_SUPPORT_FIRECLOUD_SH

--- sh/common.inc.sh
+++ sh/common.inc.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[[ -n ${INSTALL_SUPPORT_FIRECLOUD_SH:-} ]] || {
-    export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-    source ${SUPPORT_FIRECLOUD_DIR}/sh/core.inc.sh
-    source ${SUPPORT_FIRECLOUD_DIR}/sh/exe.inc.sh
-
-    # export INSTALL_SUPPORT_FIRECLOUD_SH=true
-}
+source ${SUPPORT_FIRECLOUD_DIR}/sh/core.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/sh/exe.inc.sh
