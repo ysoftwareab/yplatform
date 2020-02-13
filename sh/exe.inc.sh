3e66e85b9e6fe893f6c887b8828fab62f28c9776
Tobias Lindgren <tobias.lindgren@tobii.com>
2020-02-13 15:59:59 +0100

    Improved log output from exe_and_grep_q

--- sh/exe.inc.sh
+++ sh/exe.inc.sh
@@ -65,7 +65,7 @@ function printenv_with_name() {
 function exe_and_grep_q() {
     local OUTPUT=$(eval "$1")
     local EXECUTABLE=$(echo "$1" | cut -d" " -f1)
-    echo_info "Testing if '${OUTPUT}' matches '$2'..."
+    echo_info "Testing if command '$1' with output '${OUTPUT}' matches '$2'..."
     # using a for loop because 'xargs -r' is not part of the BSD version (MacOS)
     # which -a ${EXECUTABLE} | xargs -r -L1 ls -l || true
     for EXECUTABLE_PATH in $(which -a ${EXECUTABLE}); do
