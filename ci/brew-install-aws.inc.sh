add692bb7f864f8ac904a994d084b381388d8cd1
Andrei Neculau <andrei.neculau@gmail.com>
2020-02-13 21:27:58 +0100

    allow awscli v2, in addition to v1. ref #150

--- ci/brew-install-aws.inc.sh
+++ ci/brew-install-aws.inc.sh
@@ -13,6 +13,8 @@ aws configure set s3.signature_version s3v4
 echo_done
 
 echo_do "brew: Testing AWS utils..."
-exe_and_grep_q "aws --version | head -1" "^aws-cli/1\\."
+# allow for a smooth transition to v2, but lock to version 2 by end of 2020
+# exe_and_grep_q "aws --version | head -1" "^aws-cli/2\\."
+exe_and_grep_q "aws --version | head -1" "^aws-cli/[12]\\."
 exe_and_grep_q "awslogs --version | head -1" "^awslogs 0\\."
 echo_done
