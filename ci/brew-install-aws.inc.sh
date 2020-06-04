fc96af76c983fc335adfeebb22bcecc2d6ff8ee2
Andrei Neculau <andrei.neculau@gmail.com>
2020-06-05 00:23:09 +0200

    stop installing awslogs as part of aws bootstrapping
    The bottle is broken (error while loading shared libraries: libpython3.8.so.1.0: cannot open shared object file: No such file or directory),
    and awslogs is not used anymore.
    
    See https://github.com/tobiipro/support-firecloud/pull/166
    and https://github.com/Homebrew/linuxbrew-core/issues/20174

--- ci/brew-install-aws.inc.sh
+++ ci/brew-install-aws.inc.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 echo_do "brew: Installing AWS utils..."
 BREW_FORMULAE="$(cat <<-EOF
 awscli
-awslogs
 EOF
 )"
 brew_install "${BREW_FORMULAE}"
@@ -16,5 +15,4 @@ echo_do "brew: Testing AWS utils..."
 # allow for a smooth transition to v2, but lock to version 2 by end of 2020
 # exe_and_grep_q "aws --version | head -1" "^aws-cli/2\\."
 exe_and_grep_q "aws --version | head -1" "^aws-cli/[12]\\."
-exe_and_grep_q "awslogs --version | head -1" "^awslogs 0\\."
 echo_done
