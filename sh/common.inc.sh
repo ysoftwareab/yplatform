9877d0b1a92745f394d51df1e95a888c45c3d717
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-12 07:26:54 +0200

    make minimum bash version 4 explicit. see #143

--- sh/common.inc.sh
+++ sh/common.inc.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+if [ -z "${BASH_VERSINFO}" ] || [ -z "${BASH_VERSINFO[0]}" ] || [ ${BASH_VERSINFO[0]} -lt 4 ]; then
+    echo >&2 "Your Bash version is ${BASH_VERSINFO[0]}. ${0} requires >= 4.";
+    exit 1;
+fi
+
 export SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source ${SUPPORT_FIRECLOUD_DIR}/sh/core.inc.sh
