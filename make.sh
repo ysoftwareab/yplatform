d5e0de6f1cc5a7e5eb7c042ae3d9664c1d5316cc
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-13 10:49:50 +0200

    ability to overload 'make' in a shell session that has exe-env.inc.sh sourced. fix #179

---
+++ make.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script is a dummy script, just to test the functionality of sh/exe-env.inc.sh:make.
+
+[[ -n "${SF_MAKE_SH_PASS:-}" ]] || {
+    echo >&2 "[ERR ] Expected SF_MAKE_SH_PASS=true but got SF_MAKE_SH_PASS=${SF_MAKE_SH_PASS:-}."
+    exit 1;
+}
+
+$(command -v make) $@
