74ae39088dd6797631bed5a05e5503b4060d484e
Andrei Neculau <andrei@normative.io>
2020-07-15 09:10:29 +0200

    fixup! add core.misc.tpl.mk

--- repo/mk/core.misc.tpl.mk
+++ repo/mk/core.misc.tpl.mk
@@ -2,20 +2,27 @@
 # E.g. .vscode/settings.json from .vscode/settings.json.tpl
 # where the latter is an executable that outputs the content of the former.
 #
-# In order to generate a file from its template, add it SF_TPL_FILES.
-# SF_TPL_FILES += \
-#	some.file.tpl \
+# In order to skip some .tpl executables from being called, remove them from SF_TPL_FILES
+# SF_TPL_FILES := $(filter-out some/file.json.tpl,$(SF_TPL_FILES)).
 #
-# You can now add 'some.file' as a target's dependency.
+# You can now add 'some/file.json' or $(SF_TPL_FILES_GEN)
+# as a target's dependency or to SF_DEPS_TARGETS.
 #
 # ------------------------------------------------------------------------------
 
-SF_TPL_FILES += \
+SF_TPL_FILES := $(shell $(FIND_Q_NOSYM) . -type f -name "*.tpl" -print)
 
 SF_TPL_FILES_GEN = $(patsubst %.tpl,%,$(SF_TPL_FILES))
 
 # ------------------------------------------------------------------------------
 
 .PHONY: $(SF_TPL_FILES_GEN)
-$(SF_TPL_FILES_GEN): $(SF_TPL_FILES): %: %.tpl:
-	$< > $@
+$(SF_TPL_FILES_GEN): %: %.tpl
+	if $$($(ECHO) "$(SF_TPL_FILES)" | $(GREP) -q "$<"); then \
+		$(ECHO_INFO) "$< was removed from SF_TPL_FILES."; \
+		$(ECHO_SKIP) "Generating $@ from template $<..."; \
+	else \
+		$(ECHO_DO) "Generating $@ from template $<..."; \
+		$< > $@; \
+		$(ECHO_DONE); \
+	fi
