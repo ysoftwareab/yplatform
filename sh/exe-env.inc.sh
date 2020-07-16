e7698635c61125db8616c30062679d0793003a63
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-16 17:22:42 +0200

    uncertain how 'brew --prefix nvm' can return a path which does NOT exist
    https://travis-ci.com/github/rokmoln/support-firecloud/jobs/361634100\n/home/travis/build/rokmoln/support-firecloud/sh/exe-env.inc.sh: line 46: /home/linuxbrew/.linuxbrew/Cellar/nvm/0.35.3/nvm.sh: No such file or directory

--- sh/exe-env.inc.sh
+++ sh/exe-env.inc.sh
@@ -44,7 +44,7 @@ if which brew >/dev/null 2>&1; then
     [ -n "${NVM_DIR:-}" ] || export NVM_DIR=${HOME}/.nvm
     type nvm >/dev/null 2>&1 || {
         NVM_INSTALLATION_DIR=$(brew --prefix nvm 2>/dev/null || true)
-        [ -z "${NVM_INSTALLATION_DIR}" ] || source ${NVM_INSTALLATION_DIR}/nvm.sh --no-use
+        [ ! -r ${NVM_INSTALLATION_DIR}/nvm.sh ] || source ${NVM_INSTALLATION_DIR}/nvm.sh --no-use
         unset NVM_INSTALLATION_DIR
     }
 
