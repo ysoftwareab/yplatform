397fba50d2cad8a6cb19a08778a411573e8b5457
Andrei Neculau <andrei@normative.io>
2020-07-20 14:03:14 +0200

    clearer error message

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 # This script is a dummy script, just to test the functionality of sh/exe-env.inc.sh:make.
 
 [[ -n "${SF_MAKE_SH_PASS:-}" ]] || {
-    echo >&2 "[ERR ] Expected SF_MAKE_SH_PASS=true but got SF_MAKE_SH_PASS=${SF_MAKE_SH_PASS:-}."
+    echo >&2 "[ERR ] Expected SF_MAKE_SH_PASS to be defined."
     exit 1;
 }
 
