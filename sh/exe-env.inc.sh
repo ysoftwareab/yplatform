61b03ca0e82baa98034670730d52547fe0fdaa94
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-23 22:36:33 +0200

    less verbose output from overloaded make

--- sh/exe-env.inc.sh
+++ sh/exe-env.inc.sh
@@ -55,9 +55,14 @@ fi
 function make() {
     local MAKE_COMMAND=$(which -a make | grep "^/" | head -1)
     if [[ -z "${SF_MAKE_COMMAND:-}" ]] && [[ -x make.sh ]]; then
-        echo >&2 "[INFO] Running    ${PWD}/make.sh $@"
-        echo >&2 "       instead of ${MAKE_COMMAND} $@"
+        [[ ! -f make.sh.successful ]] || {
+            echo >&2 "[INFO] Running    ${PWD}/make.sh $@"
+            echo >&2 "       instead of ${MAKE_COMMAND} $@"
+        }
         SF_MAKE_COMMAND=${MAKE_COMMAND} ./make.sh $@
+        local EXIT_CODE=$?
+        # á la Ubuntu's ~/.sudo_as_admin_successful
+        [[ ${EXIT_CODE} -ne 0 ]] || touch make.sh.successful
         return $?
     fi
     ${MAKE_COMMAND} $@
