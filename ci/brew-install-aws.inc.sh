3cff42102c0fb1580b24da41e8902579c0d83f56
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-27 19:29:30 +0200

    try to use an older bottle for awscli that used to work on ubuntu-focal

--- ci/brew-install-aws.inc.sh
+++ ci/brew-install-aws.inc.sh
@@ -3,10 +3,11 @@ set -euo pipefail
 
 echo_do "brew: Installing AWS utils..."
 BREW_FORMULAE="$(cat <<-EOF
-awscli
+https://github.com/Homebrew/linuxbrew-core/blob/dcdc755deab824ed59d40614ff9255555d7ef32b/Formula/awscli.rb
 EOF
 )"
 brew_install "${BREW_FORMULAE}"
+brew pin awscli
 unset BREW_FORMULAE
 aws configure set s3.signature_version s3v4
 echo_done
