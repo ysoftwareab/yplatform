8efe8defe052f647301577545fa93df4f3cb26ad
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-27 11:16:19 +0200

    implement a 'yarn ci' mode

--- repo/mk/js.deps.yarn.mk
+++ repo/mk/js.deps.yarn.mk
@@ -6,6 +6,21 @@
 YARN = $(call which,YARN,yarn)
 $(foreach VAR,YARN,$(call make-lazy,$(VAR)))
 
+# see https://github.com/yarnpkg/yarn/issues/5869
+YARN_CI_OR_INSTALL := install \
+	--non-interactive \
+	--skip-integrity-check \
+
+ifeq (true,$(CI))
+YARN_CI_OR_INSTALL := install \
+	--check-files \
+	--frozen-lockfile \
+	--no-progress \
+	--non-interactive \
+	--skip-integrity-check \
+
+endif
+
 SF_CLEAN_FILES += \
 	node_modules \
 
@@ -24,16 +39,11 @@ endif
 deps-yarn:
 #	'yarn install' will also remove extraneous dependencies
 #	See https://classic.yarnpkg.com/en/docs/cli/prune/
-	$(YARN) install \
-		--frozen-lockfile \
-		--non-interactive
+	$(YARN) $(YARN_CI_OR_INSTALL)
 
 
 .PHONY: deps-yarn-prod
 deps-yarn-prod:
 #	'yarn install' will also remove extraneous dependencies
 #	See https://classic.yarnpkg.com/en/docs/cli/prune/
-	$(YARN) install \
-		--frozen-lockfile \
-		--non-interactive \
-		--production
+	$(YARN)  $(YARN_CI_OR_INSTALL) --production
