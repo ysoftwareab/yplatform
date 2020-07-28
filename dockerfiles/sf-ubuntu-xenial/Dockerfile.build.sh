21617a072af3f5cf4a0013c63d1dc978c04f8e67
Andrei Neculau <andrei@normative.io>
2020-07-29 00:30:22 +0200

    sync 'apt_update' functions

--- dockerfiles/sf-ubuntu-xenial/Dockerfile.build.sh
+++ dockerfiles/sf-ubuntu-xenial/Dockerfile.build.sh
@@ -3,10 +3,14 @@ set -euo pipefail
 
 function apt_update() {
     apt-get update -y --fix-missing 2>&1 || {
+        set -x
         # try to handle "Hash Sum mismatch" error
         apt-get clean
         rm -rf /var/lib/apt/lists/*
+        # see https://bugs.launchpad.net/ubuntu/+source/apt/+bug/1785778
+        apt-get update -o Acquire::CompressionTypes::Order::=gz
         apt-get update -y --fix-missing
+        set +x
     }
 }
 
