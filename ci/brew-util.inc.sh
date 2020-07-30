22d84435431ed82be152e8c5a9244778e4ec65c4
Andrei Neculau <andrei.neculau@gmail.com>
2020-07-30 09:10:59 +0200

    partial printenv even without SF_PRINTENV_BOOTSTRAP=true, list pwd and HOME contents

--- ci/brew-util.inc.sh
+++ ci/brew-util.inc.sh
@@ -5,6 +5,51 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 
+# brew_ namespace chosen for consistency, even if less relevant for printenv
+function brew_printenv() {
+    echo_do "Printenv..."
+    if [[ "${SF_PRINTENV_BOOTSTRAP:-}" = "true" ]]; then
+        printenv
+    else
+        echo_info "Printing only excerpts of printenv."
+        printenv | grep -f <(
+                cat <<EOF
+^CI=
+^CONTINUOUS_INTEGRATION=
+^EDITOR=
+^GIT_
+^HOME=
+^HOMEBREW_NO_
+^LANG=
+^LANGUAGE=
+^LC_ALL=
+^LC_CTYPE=
+^MANPATH=
+^PAGER=
+^PATH=
+^PWD=
+^SHELL=
+^SF_LOG_BOOTSTRAP=
+^SF_PRINTENV_BOOTSTRAP=
+^SUDO=
+^TZ=
+^USER=
+EOF
+            )
+    fi
+    echo_done
+
+    echo_do "Listing current working directory..."
+    pwd
+    ls -la $(pwd)
+    echo_done
+
+    echo_do "Listing HOME directory..."
+    echo ${HOME}
+    ls -la ${HOME}
+    echo_done
+}
+
 function brew_update() {
     echo_do "brew: Updating..."
     brew update >/dev/null
