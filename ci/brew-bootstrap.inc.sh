710e830d6c64b3fd8c84b705f6ee35596aef4063
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-02 13:12:27 +0200

    split into bootstrap_has_brew

--- ci/brew-bootstrap.inc.sh
+++ ci/brew-bootstrap.inc.sh
@@ -4,51 +4,51 @@ set -euo pipefail
 SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source ${SUPPORT_FIRECLOUD_DIR}/sh/common.inc.sh
 
-function bootstrap_brew() {
-    local HAS_BREW_2=true
-
+function bootstrap_has_brew() {
     if which brew >/dev/null 2>&1; then
         # using tail or else broken pipe. see https://github.com/Homebrew/homebrew-cask/issues/36218
-        # exe_and_grep_q "brew --version | head -1" "^Homebrew 2." || HAS_BREW_2=false
-        exe_and_grep_q "brew --version | tail -n+1 | head -1" "^Homebrew 2\." || HAS_BREW_2=false
+        # exe_and_grep_q "brew --version | head -1" "^Homebrew 2." || return 1
+        exe_and_grep_q "brew --version | tail -n+1 | head -1" "^Homebrew 2\." || return 1
     else
         echo_info "brew: Executable brew not found."
-        HAS_BREW_2=false
+        return 1
     fi
+}
 
+function bootstrap_brew() {
+    local HAS_BREW_2=true
+    bootstrap_has_brew || HAS_BREW_2=false
     local RAW_GUC_URL="https://raw.githubusercontent.com"
 
-    case $(uname -s) in
-        Darwin)
-            if [[ "${HAS_BREW_2}" = "true" ]]; then
-                echo_do "brew: Updating homebrew..."
-                brew update >/dev/null
-                echo_done
-            else
-                echo_do "brew: Installing homebrew..."
-                </dev/null /bin/bash -c "$(curl -fqsS -L ${RAW_GUC_URL}/Homebrew/install/master/install.sh)"
-                echo_done
-            fi
+    case ${HAS_BREW_2}-$(uname -s) in
+        false-Darwin)
+            echo_do "brew: Installing homebrew..."
+            </dev/null /bin/bash -c "$(curl -fqsS -L ${RAW_GUC_URL}/Homebrew/install/master/install.sh)"
+            echo_done
             ;;
-        Linux)
-            if [[ "${HAS_BREW_2}" = "true" ]]; then
-                echo_do "brew: Updating linuxbrew..."
-                brew update >/dev/null
+        true-Darwin)
+            echo_do "brew: Updating homebrew..."
+            brew update >/dev/null
+            echo_done
+            ;;
+        false-Linux)
+            echo_do "brew: Installing linuxbrew..."
+            if [[ "${SUDO}" = "" ]] || [[ "${SUDO}" = "sf_nosudo" ]]; then
+                HOMEBREW_PREFIX=~/.linuxbrew
+                echo_do "brew: Installing without sudo into ${HOMEBREW_PREFIX}..."
+                mkdir -p ${HOMEBREW_PREFIX}
+                curl -fqsS -L https://github.com/Homebrew/brew/tarball/master | \
+                    tar xz --strip 1 -C ${HOMEBREW_PREFIX}
                 echo_done
             else
-                echo_do "brew: Installing linuxbrew..."
-                if [[ "${SUDO}" = "" ]] || [[ "${SUDO}" = "sf_nosudo" ]]; then
-                    HOMEBREW_PREFIX=~/.linuxbrew
-                    echo_do "brew: Installing without sudo into ${HOMEBREW_PREFIX}..."
-                    mkdir -p ${HOMEBREW_PREFIX}
-                    curl -fqsS -L https://github.com/Homebrew/brew/tarball/master | \
-                        tar xz --strip 1 -C ${HOMEBREW_PREFIX}
-                    echo_done
-                else
-                    </dev/null /bin/bash -c "$(curl -fqsS -L ${RAW_GUC_URL}/Homebrew/install/master/install.sh)"
-                fi
-                echo_done
+                </dev/null /bin/bash -c "$(curl -fqsS -L ${RAW_GUC_URL}/Homebrew/install/master/install.sh)"
             fi
+            echo_done
+            ;;
+        true-Linux)
+            echo_do "brew: Updating linuxbrew..."
+            brew update >/dev/null
+            echo_done
             ;;
         *)
             echo_err "brew: $(uname -s) is an unsupported OS."
