ea8692196248530ea42a1b45a112a3d6dcaa7ecd
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-07 16:36:38 +0200

    break into singular functions

--- ci/brew-util.inc.sh
+++ ci/brew-util.inc.sh
@@ -57,50 +57,58 @@ function brew_update() {
     echo_done
 }
 
+function brew_upgrade_one() {
+    local FORMULA="$@"
+
+    local FULLNAME=$(echo "${FORMULA}" | cut -d " " -f 1)
+    local NAME=$(basename "${FULLNAME}" | sed "s/\.rb\$//")
+
+    # install any missing dependencies
+    local MISSING="$(brew missing ${NAME})"
+    [[ -z "${MISSING}" ]] || {
+        echo_info "brew: Found missing dependencies for ${NAME}: ${MISSING}."
+        echo_do "brew: Installing missing dependencies for ${NAME}..."
+        brew install ${MISSING}
+        echo_done
+    }
+
+    # link, if not already
+    echo_do "brew: Linking ${NAME}..."
+    if [[ "${CI:-}" != "true" ]]; then
+        brew link ${NAME} || true
+    else
+        brew link --force --overwrite ${NAME} || true
+    fi
+    echo_done
+
+    # is it pinned?
+    if brew list ${NAME} --pinned | grep -q "^${NAME}$"; then
+        echo_info "brew: ${NAME} is pinned."
+        echo_skip "brew: Upgrading ${NAME}..."
+        return 0
+    fi
+
+    # is it already up-to-date?
+    if ! brew outdated ${NAME} >/dev/null 2>&1; then
+        echo_info "brew: ${NAME} is up-to-date."
+        echo_skip "brew: Upgrading ${NAME}..."
+        return 0
+    fi
+
+    echo_do "brew: Upgrading ${NAME}..."
+    brew upgrade ${NAME}
+    echo_done
+}
+
 function brew_upgrade() {
-    while read -u3 NAME; do
-        [[ -n "${NAME}" ]] || continue
-
-        # install any missing dependencies
-        local MISSING="$(brew missing ${NAME})"
-        [[ -z "${MISSING}" ]] || {
-            echo_info "brew: Found missing dependencies for ${NAME}: ${MISSING}."
-            echo_do "brew: Installing missing dependencies for ${NAME}..."
-            brew install ${MISSING}
-            echo_done
-        }
-
-        # link, if not already
-        echo_do "brew: Linking ${NAME}..."
-        if [[ "${CI:-}" != "true" ]]; then
-            brew link ${NAME} || true
-        else
-            brew link --force --overwrite ${NAME} || true
-        fi
-        echo_done
-
-        # is it pinned?
-        if brew list ${NAME} --pinned | grep -q "^${NAME}$"; then
-            echo_info "brew: ${NAME} is pinned."
-            echo_skip "brew: Upgrading ${NAME}..."
-            continue
-        fi
-
-        # is it already up-to-date?
-        if ! brew outdated ${NAME} >/dev/null 2>&1; then
-            echo_info "brew: ${NAME} is up-to-date."
-            echo_skip "brew: Upgrading ${NAME}..."
-            continue
-        fi
-
-        echo_do "brew: Upgrading ${NAME}..."
-        brew upgrade ${NAME}
-        echo_done
+    while read -u3 FORMULA; do
+        [[ -n "${FORMULA}" ]] || continue
+        brew_upgrade_one "${FORMULA}"
     done 3< <(echo "$@")
 }
 
 # install erlang without wxmac bloat
-function brew_install_erlang() {
+function brew_install_one_erlang() {
     echo_do "brew: Installing erlang, without wxmac..."
     brew tap linuxbrew/xorg
     # using a for loop because 'xargs -r' is not part of the BSD version (MacOS)
@@ -112,75 +120,80 @@ function brew_install_erlang() {
     echo_done
 }
 
+function brew_install_one() {
+    local FORMULA="$@"
+
+    local FULLNAME=$(echo "${FORMULA}" | cut -d " " -f 1)
+    local NAME=$(basename "${FULLNAME}" | sed "s/\.rb\$//")
+    local OPTIONS=$(echo "${FORMULA} " | cut -d " " -f 2- | xargs -n 1 | sort -u)
+
+    case ${NAME} in
+        erlang)
+            brew_install_one_erlang
+            return 0
+            ;;
+        *)
+            true
+            ;;
+    esac
+
+    # is it already installed ?
+    if brew list "${NAME}" >/dev/null 2>&1; then
+        # is it a url/path to a formula.rb file
+        [[ "${FULLNAME}" = "${NAME}" ]] || {
+            brew uninstall ${NAME}
+
+            echo_do "brew: Installing ${FORMULA}..."
+            if [[ "${CI:-}" != "true" ]]; then
+                brew install ${FORMULA}
+            else
+                brew install --force ${FORMULA} || brew link --force --overwrite ${FORMULA}
+            fi
+            echo_done
+
+            return 0
+        }
+
+        # install without specific options ?
+        [[ -n "${OPTIONS}" ]] || {
+            echo_skip "brew: Installing ${FORMULA}..."
+            brew_upgrade ${NAME}
+            return 0
+        }
+
+        # is it already installed with the required options ?
+        local USED_OPTIONS="$(brew info --json=v1 ${NAME} | \
+            /usr/bin/python \
+                -c 'import sys,json;print "".join(json.load(sys.stdin)[0]["installed"][0]["used_options"])' | \
+            xargs -n 1 | \
+            sort -u || true)"
+        local NOT_FOUND_OPTIONS="$(comm -23 <(echo "${OPTIONS}") <(echo "${USED_OPTIONS}"))"
+        [[ -n "${NOT_FOUND_OPTIONS}" ]] || {
+            echo_skip "brew: Installing ${FORMULA}..."
+            brew_upgrade ${NAME}
+            return 0
+        }
+
+        echo_err "${NAME} is already installed with options '${USED_OPTIONS}',"
+        echo_err "but not the required '${NOT_FOUND_OPTIONS}'."
+
+        if [[ "${TRAVIS:-}" = "true" ]]; then
+            brew uninstall ${NAME}
+        else
+            echo_err "Consider uninstalling ${NAME} with 'brew uninstall ${NAME}' and rerun the bootstrap!"
+            return 1
+        fi
+    fi
+
+    echo_do "brew: Installing ${FORMULA}..."
+    brew install ${FORMULA}
+    echo_done
+}
+
 function brew_install() {
     while read -u3 FORMULA; do
         [[ -n "${FORMULA}" ]] || continue
-
-        local FULLNAME=$(echo "${FORMULA}" | cut -d " " -f 1)
-        local NAME=$(basename "${FULLNAME}" | sed "s/\.rb\$//")
-        local OPTIONS=$(echo "${FORMULA} " | cut -d " " -f 2- | xargs -n 1 | sort -u)
-
-        case ${NAME} in
-            erlang)
-                brew_install_erlang
-                continue
-                ;;
-            *)
-                true
-                ;;
-        esac
-
-        # is it already installed ?
-        if brew list "${NAME}" >/dev/null 2>&1; then
-            # is it a url/path to a formula.rb file
-            [[ "${FULLNAME}" = "${NAME}" ]] || {
-                brew uninstall ${NAME}
-
-                echo_do "brew: Installing ${FORMULA}..."
-                if [[ "${CI:-}" != "true" ]]; then
-                    brew install ${FORMULA}
-                else
-                    brew install --force ${FORMULA} || brew link --force --overwrite ${FORMULA}
-                fi
-                echo_done
-
-                continue
-            }
-
-            # install without specific options ?
-            [[ -n "${OPTIONS}" ]] || {
-                echo_skip "brew: Installing ${FORMULA}..."
-                brew_upgrade ${NAME}
-                continue
-            }
-
-            # is it already installed with the required options ?
-            local USED_OPTIONS="$(brew info --json=v1 ${NAME} | \
-                /usr/bin/python \
-                    -c 'import sys,json;print "".join(json.load(sys.stdin)[0]["installed"][0]["used_options"])' | \
-                xargs -n 1 | \
-                sort -u || true)"
-            local NOT_FOUND_OPTIONS="$(comm -23 <(echo "${OPTIONS}") <(echo "${USED_OPTIONS}"))"
-            [[ -n "${NOT_FOUND_OPTIONS}" ]] || {
-                echo_skip "brew: Installing ${FORMULA}..."
-                brew_upgrade ${NAME}
-                continue
-            }
-
-            echo_err "${NAME} is already installed with options '${USED_OPTIONS}',"
-            echo_err "but not the required '${NOT_FOUND_OPTIONS}'."
-
-            if [[ "${TRAVIS:-}" = "true" ]]; then
-                brew uninstall ${NAME}
-            else
-                echo_err "Consider uninstalling ${NAME} with 'brew uninstall ${NAME}' and rerun the bootstrap!"
-                return 1
-            fi
-        fi
-
-        echo_do "brew: Installing ${FORMULA}..."
-        brew install ${FORMULA}
-        echo_done
+        brew_install_one ${FORMULA}
     done 3< <(echo "$@")
     # see https://github.com/Homebrew/brew/issues/5013
     hash -r
@@ -218,14 +231,19 @@ function apt_update() {
     }
 }
 
+function apt_install_one() {
+    local DPKG="$@"
+
+    echo_do "aptitude: Installing ${DPKG}..."
+    # ${SUDO} apt-get install -y --force-yes ${DPKG}
+    ${SUDO} apt-get install -y ${FORCE_YES} ${DPKG}
+    echo_done
+}
+
 function apt_install() {
     local FORCE_YES="--allow-downgrades --allow-remove-essential --allow-change-held-packages"
     while read -u3 DPKG; do
         [[ -n "${DPKG}" ]] || continue
-
-        echo_do "aptitude: Installing ${DPKG}..."
-        # ${SUDO} apt-get install -y --force-yes ${DPKG}
-        ${SUDO} apt-get install -y ${FORCE_YES} ${DPKG}
-        echo_done
+        apt_install_one ${DPKG}
     done 3< <(echo "$@")
 }
