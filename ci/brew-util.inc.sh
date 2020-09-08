ad6098f3832894f3996b0da3e39cbbc7007bade8
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-08 13:50:28 +0200

    fixup! split brew-util.inc.sh

--- ci/brew-util.inc.sh
+++ ci/brew-util.inc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-util/apt.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-util/env.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-util/install.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-util/print.inc.sh
