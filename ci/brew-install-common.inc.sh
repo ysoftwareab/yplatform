b5fe908cb052473a48739873b214fb75f1030b0b
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-08 11:47:42 +0200

    move perl to minimal

--- ci/brew-install-common.inc.sh
+++ ci/brew-install-common.inc.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-function brew_install_perl() {
-    # workaround for failing to install `perl` (pyenv > autoconf > perl)
-    # fatal error: xlocale.h: No such file or directory
-    # see https://github.com/Homebrew/linuxbrew-core/issues/20914
-    if [[ "${OS_SHORT}" = "linux" ]] && [[ ! -f /usr/include/xlocale.h ]]; then
-        ${SUDO} ln -s /usr/include/locale.h /usr/include/xlocale.h
-    fi
-
-    brew_install perl
-}
-
 if [[ "${SF_SKIP_COMMON_BOOTSTRAP:-}" = "true" ]]; then
     echo_info "brew: SF_SKIP_COMMON_BOOTSTRAP=${SF_SKIP_COMMON_BOOTSTRAP}"
     echo_skip "brew: Installing common packages..."
@@ -20,11 +9,5 @@ else
     source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-minimal.inc.sh
     source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-node.inc.sh
     source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-docker.inc.sh
-
-    # installing perl for performance reasons, since it takes a very long time to install via homebrew,
-    # and quite a few formulas require it
-    # NOTE: many formulas are optimized to use system's perl on Darwin, but not Linux
-    brew_install_perl
-
     echo_done
 fi
--- ci/brew-install-minimal.inc.sh
+++ ci/brew-install-minimal.inc.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+function brew_install_perl() {
+    # workaround for failing to install `perl` (pyenv > autoconf > perl)
+    # fatal error: xlocale.h: No such file or directory
+    # see https://github.com/Homebrew/linuxbrew-core/issues/20914
+    if [[ "${OS_SHORT}" = "linux" ]] && [[ ! -f /usr/include/xlocale.h ]]; then
+        ${SUDO} ln -s /usr/include/locale.h /usr/include/xlocale.h
+    fi
+
+    brew_install perl
+}
+
 if [[ "${SF_SKIP_COMMON_BOOTSTRAP:-}" = "true" ]]; then
     echo_info "brew: SF_SKIP_COMMON_BOOTSTRAP=${SF_SKIP_COMMON_BOOTSTRAP}"
     echo_skip "brew: Installing minimal packages..."
@@ -8,5 +19,11 @@ else
     echo_do "brew: Installing minimal packages..."
     source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-core.inc.sh
     source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-gnu.inc.sh
+
+    # installing perl for performance reasons, since it takes a very long time to install via homebrew,
+    # and quite a few formulas require it
+    # NOTE: many formulas are optimized to use system's perl on Darwin, but not Linux
+    brew_install_perl
+
     echo_done
 fi
