a65d56cc2f838caa5986d70ced3046e642596a1c
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-24 14:52:58 +0200

    add replace-functions for apt_update and brew_update, instead of unsetting them, to provide help info

--- ci/brew-util.inc.sh
+++ ci/brew-util.inc.sh
@@ -11,12 +11,20 @@ function brew_brewfile_inc_sh() {
     echo_warn "Hiding 'apt_update' as 'force_apt_update'."
     echo_info "Running 'force_apt_update' will make the system unpredictable."
     eval "force_$(declare -f apt_update)"
-    unset apt_update
+    eval "apt_update() { \
+        echo '[ERR ] Updating Aptitude will make the system unpredictable.'; \
+        echo '[INFO] Call \'force_apt_update\' if you really need to, instead of \'apt_update\'.'; \
+        exit 1; \
+    }"
 
     echo_warn "Hiding 'brew_update' as 'force_brew_update'."
     echo_info "Running 'force_brew_update' will make the system unpredictable."
     eval "force_$(declare -f brew_update)"
-    unset brew_update
+    eval "brew_update() { \
+        echo '[ERR ] Updating HomeBrew will make the system unpredictable.'; \
+        echo '[INFO] Call \'force_brew_update\' if you really need to, instead of \'brew_update\'.'; \
+        exit 1; \
+    }"
 
     local BREWFILE_INC_SH=${GIT_ROOT}/Brewfile.inc.sh
     [[ -f "${BREWFILE_INC_SH}" ]] || {
