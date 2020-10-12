65410ce29e3a2baaf412bca6d7e9dc6a0a5970a7
Andrei Neculau <andrei.neculau@gmail.com>
2020-10-13 00:26:40 +0200

    retry 'brew update'

--- ci/brew-util/update.inc.sh
+++ ci/brew-util/update.inc.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 
 function brew_update() {
     echo_do "brew: Updating..."
-    brew update >/dev/null
+    # 'brew update' is currently flaky, resulting in 'transfer closed with outstanding read data remaining'
+    # see https://github.com/Homebrew/homebrew-core/issues/61772
+    brew update >/dev/null || brew update
     brew outdated
     echo_done
 }
