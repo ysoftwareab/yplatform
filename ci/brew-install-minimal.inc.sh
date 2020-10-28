02215660a5cd90e402356db3ec5c9f8f4204a9a1
Andrei Neculau <andrei.neculau@gmail.com>
2020-10-28 16:41:08 +0100

    install perl via apt on linux, if 'brew install perl' fails

--- ci/brew-install-minimal.inc.sh
+++ ci/brew-install-minimal.inc.sh
@@ -12,7 +12,18 @@ else
     # installing perl for performance reasons, since it takes a very long time to install via homebrew,
     # and quite a few formulas require it
     # NOTE: many formulas are optimized to use system's perl on Darwin, but not Linux
-    brew_install perl
+    case ${OS} in
+        Darwin)
+            brew_install perl
+            ;;
+        Linux)
+            # TODO installing via apt as a workaround for Travis Ubuntu 20.04 running gcc 9.2.1
+            # /usr/lib/gcc/x86_64-linux-gnu/9/include-fixed/bits/statx.h:38:25: error: missing binary operator before token "("
+            # #if __glibc_has_include ("__linux__/stat.h")
+            #                         ^
+            brew_install perl || apt_install perl
+            ;;
+    esac
 
     echo_done
 fi
