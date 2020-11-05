441645dbf01bba4c0134fff6dd2c9e97ce4d92c8
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-06 00:03:25 +0100

    handle missing jq during before-install.pre

--- repo/ci.sh/before-install.pre.inc.sh
+++ repo/ci.sh/before-install.pre.inc.sh
@@ -107,14 +107,26 @@ function sf_get_docker_ci_image() {
     # set the tag to the version of SF
     if [[ ${SF_DOCKER_CI_IMAGE} =~ ^rokmoln/sf- ]] && \
         [[ ! "${SF_DOCKER_CI_IMAGE}" =~ /:/ ]]; then
-        local DOCKER_IMAGE_TAG=$(cat ${SUPPORT_FIRECLOUD_DIR}/package.json | jq -r ".version")
+        if which jq >/dev/null 2>&1; then
+            local DOCKER_IMAGE_TAG=$(cat ${SUPPORT_FIRECLOUD_DIR}/package.json | jq -r ".version")
+        else
+            local DOCKER_IMAGE_TAG=$(
+                cat ${SUPPORT_FIRECLOUD_DIR}/package.json | \
+                    /usr/bin/python -c 'import sys,json;print("".join(json.load(sys.stdin)["version"]))')
+        fi
         SF_DOCKER_CI_IMAGE="${SF_DOCKER_CI_IMAGE}:${DOCKER_IMAGE_TAG}"
     fi
     # if given a docker.pkg.github.com/rokmoln/support-firecloud/sf- image, but without a tag
     # set the tag to the version of SF
     if [[ ${SF_DOCKER_CI_IMAGE} =~ ^docker.pkg.github.com/rokmoln/support-firecloud/sf- ]] && \
         [[ ! "${SF_DOCKER_CI_IMAGE}" =~ /:/ ]]; then
-        local DOCKER_IMAGE_TAG=$(cat ${SUPPORT_FIRECLOUD_DIR}/package.json | jq -r ".version")
+        if which jq >/dev/null 2>&1; then
+            local DOCKER_IMAGE_TAG=$(cat ${SUPPORT_FIRECLOUD_DIR}/package.json | jq -r ".version")
+        else
+            local DOCKER_IMAGE_TAG=$(
+                cat ${SUPPORT_FIRECLOUD_DIR}/package.json | \
+                    /usr/bin/python -c 'import sys,json;print("".join(json.load(sys.stdin)["version"]))')
+        fi
         SF_DOCKER_CI_IMAGE="${SF_DOCKER_CI_IMAGE}:${DOCKER_IMAGE_TAG}"
     fi
 
