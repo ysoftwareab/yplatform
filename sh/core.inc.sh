d2f26a7c6774f69c0d75cd36f8c330bec3310e99
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-12 12:38:04 +0100

    fixup! try redirect stderr to stdout in CI to keep streams in sync

--- sh/core.inc.sh
+++ sh/core.inc.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 CI=${CI:-}
 [[ "${CI}" != "1" ]] || CI=true
 
-# CIs have issues keeping stdout and stderr in sync because they parse the streams
-# e.g. to mask secret values
-[[ "${CI}" != "true" ]] || exec 2>&1
-
 V=${V:-${VERBOSE:-}}
 VERBOSE=${V}
 [[ "${VERBOSE}" != "1" ]] || VERBOSE=true
