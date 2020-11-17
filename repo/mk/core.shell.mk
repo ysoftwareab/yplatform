23e2f7fabceeacf6e7793e72fcd575fbbba622c9
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-17 17:59:18 +0100

    placeholder for tmate needing ~/.ssh/id_rsa

--- repo/mk/core.shell.mk
+++ repo/mk/core.shell.mk
@@ -5,6 +5,10 @@
 #
 # ------------------------------------------------------------------------------
 
+#	TODO Uncertain if tmate works without a pre-existing ~/.ssh/id_rsa
+# SSH_KEYHEN = $(call which,SSH_KEYGEN,ssh-keygen)
+# $(foreach VAR,SSH_KEYGEN,$(call make-lazy,$(VAR)))
+
 TMATE = $(call which,TMATE,tmate)
 $(foreach VAR,TMATE,$(call make-lazy,$(VAR)))
 
@@ -22,6 +26,9 @@ shell/tmate:
 	$(eval TMATE_SOCKET := $(shell $(MKTEMP)))
 	$(ECHO_INFO) "Install 'tmate' via 'brew install tmate'."
 	$(ECHO_INFO) "tmate socket: $(TMATE_SOCKET)"
+#	TODO Uncertain if tmate works without a pre-existing ~/.ssh/id_rsa
+#	$(MKDIR) ${HOME}/.ssh
+#	[[ -e $${HOME}/.ssh/id_rsa ]] || $(SSH_KEYGEN) -q -t rsa -b 4096 -N "" -f $${HOME}/.ssh/id_rsa
 	$(RM) $(TMATE_SOCKET)
 	$(TMATE) -S $(TMATE_SOCKET) new-session -d "$(SHELL) -l"
 	$(TMATE) -S $(TMATE_SOCKET) wait tmate-ready
