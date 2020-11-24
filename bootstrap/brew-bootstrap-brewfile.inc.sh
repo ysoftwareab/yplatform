4708654ab8f2dd1ed67cc659cee5e6854f43acf7
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-24 14:32:33 +0100

    move brew_brewfile_inc_sh to standalone brew-bootstrap-brewfile.inc.sh for consistency

---
+++ bootstrap/brew-bootstrap-brewfile.inc.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo_warn "Hiding 'apt_update' as 'force_apt_update'."
+echo_info "Running 'force_apt_update' will make the system unpredictable."
+eval "force_$(declare -f apt_update)"
+eval "apt_update() { \
+    echo \"[ERR ] Updating Aptitude will make the system unpredictable.\"; \
+    echo \"[INFO] Call 'force_apt_update' if you really need to, instead of 'apt_update'.\"; \
+    exit 1; \
+}"
+
+echo_warn "Hiding 'brew_update' as 'force_brew_update'."
+echo_info "Running 'force_brew_update' will make the system unpredictable."
+eval "force_$(declare -f brew_update)"
+eval "brew_update() { \
+    echo \"[ERR ] Updating Homebrew will make the system unpredictable.\"; \
+    echo \"[INFO] Call 'force_brew_update' if you really need to, instead of 'brew_update'.\"; \
+    exit 1; \
+}"
+
+BREWFILE_INC_SH=${GIT_ROOT}/Brewfile.inc.sh
+[[ -f "${BREWFILE_INC_SH}" ]] || {
+    echo_err "No ${BREWFILE_INC_SH} file present."
+    return 1
+}
+echo_do "Sourcing ${BREWFILE_INC_SH}..."
+source ${BREWFILE_INC_SH}
+echo_done
+unset BREWFILE_INC_SH
--- bootstrap/brew-util.inc.sh
+++ bootstrap/brew-util.inc.sh
@@ -6,31 +6,3 @@ source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/env.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/install.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/print.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/update.inc.sh
-
-function brew_brewfile_inc_sh() {
-    echo_warn "Hiding 'apt_update' as 'force_apt_update'."
-    echo_info "Running 'force_apt_update' will make the system unpredictable."
-    eval "force_$(declare -f apt_update)"
-    eval "apt_update() { \
-        echo \"[ERR ] Updating Aptitude will make the system unpredictable.\"; \
-        echo \"[INFO] Call 'force_apt_update' if you really need to, instead of 'apt_update'.\"; \
-        exit 1; \
-    }"
-
-    echo_warn "Hiding 'brew_update' as 'force_brew_update'."
-    echo_info "Running 'force_brew_update' will make the system unpredictable."
-    eval "force_$(declare -f brew_update)"
-    eval "brew_update() { \
-        echo \"[ERR ] Updating Homebrew will make the system unpredictable.\"; \
-        echo \"[INFO] Call 'force_brew_update' if you really need to, instead of 'brew_update'.\"; \
-        exit 1; \
-    }"
-
-    local BREWFILE_INC_SH=${GIT_ROOT}/Brewfile.inc.sh
-    [[ -f "${BREWFILE_INC_SH}" ]] || {
-        echo_err "No ${BREWFILE_INC_SH} file present."
-        return 1
-    }
-    echo_info "Sourcing ${BREWFILE_INC_SH}..."
-    source ${BREWFILE_INC_SH}
-}
