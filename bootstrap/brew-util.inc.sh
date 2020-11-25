4d57d4f875366214bfd7a23b419c22505c964ec2
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-25 16:53:35 +0100

    split brew_lockfile to separate file

--- bootstrap/brew-util.inc.sh
+++ bootstrap/brew-util.inc.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/apt.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/env.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/lockfile.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/install.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/print.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/update.inc.sh
