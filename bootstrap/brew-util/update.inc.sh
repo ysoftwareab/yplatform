058af43e92627bc6d77b405fc84cfd5585390cff
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-26 11:42:51 +0100

    run verbose brew update on second try

--- bootstrap/brew-util/update.inc.sh
+++ bootstrap/brew-util/update.inc.sh
@@ -5,7 +5,7 @@ function brew_update() {
     echo_do "brew: Updating..."
     # 'brew update' is currently flaky, resulting in 'transfer closed with outstanding read data remaining'
     # see https://github.com/Homebrew/homebrew-core/issues/61772
-    brew update >/dev/null || brew update
+    brew update >/dev/null || brew update --verbose
     brew outdated
     echo_done
 }
