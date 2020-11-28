853d11d3f75e72569658d74fd2a019b251000d14
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-28 01:14:03 +0100

    fixup! deprecate travis in favour of github actions as primary ci

--- .ci.sh
+++ .ci.sh
@@ -4,6 +4,5 @@
 SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SUPPORT_FIRECLOUD_DIR}/sh/common.inc.sh
 
-TRAVIS_BUILD_STAGE_NAME=${TRAVIS_BUILD_STAGE_NAME:-test}
-TRAVIS_BUILD_STAGE_NAME_LOWER=$(echo "${TRAVIS_BUILD_STAGE_NAME}" | tr "[:upper:]" "[:lower:]")
-source .ci.${TRAVIS_BUILD_STAGE_NAME_LOWER}.sh
+GITHUB_JOB_LOWER=$(echo "${GITHUB_JOB}" | tr "[:upper:]" "[:lower:]")
+source .ci.${GITHUB_JOB_LOWER}.sh
