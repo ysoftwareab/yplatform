058c287481624ba6cb1af090cf75970978f99e78
Andrei Neculau <andrei.neculau@gmail.com>
2020-11-29 12:11:37 +0100

    fixup! fixup! fixup! fixup! deprecate travis in favour of github actions as primary ci

--- .ci.sh
+++ .ci.sh
@@ -5,4 +5,4 @@ SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SUPPORT_FIRECLOUD_DIR}/sh/common.inc.sh
 
 GITHUB_JOB=${GITHUB_JOB:-main}
-source .ci.${GITHUB_JOB_LOWER}.sh
+source .ci.${GITHUB_JOB}.sh
