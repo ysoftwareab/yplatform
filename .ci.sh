76299e763015b6882b966508e07b6117e025bc55
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-12 11:51:50 +0100

    set SF_CI_ECHO_BENCHMARK

--- .ci.sh
+++ .ci.sh
@@ -4,5 +4,7 @@
 SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SUPPORT_FIRECLOUD_DIR}/sh/common.inc.sh
 
+export SF_CI_ECHO_BENCHMARK=${GIT_ROOT}/sf-ci-echo-benchmark
+
 GITHUB_JOB=${GITHUB_JOB:-main}
 source .ci.${GITHUB_JOB}.sh
