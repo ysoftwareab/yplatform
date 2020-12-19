eb9a74f19913a1f1b2ccc92f77613425d6a90ded
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-19 11:55:07 +0100

    fixup! fixup! setup support-firecloud's git attributes/excludes locally per repo, instead of assuming user has bootstrapped ~/.gitconfig

--- build.mk/core.deps.git-info-attributes.mk
+++ build.mk/core.deps.git-info-attributes.mk
@@ -12,8 +12,8 @@ endif
 
 # ------------------------------------------------------------------------------
 
-.PHONY: .git/info/exclude
-.git/info/exclude: $(SUPPORT_FIRECLOUD_DIR)/gitconfig/dot.gitignore_global
+.PHONY: .git/info/attributes
+.git/info/attributes: $(SUPPORT_FIRECLOUD_DIR)/gitconfig/dot.gitignore_global
 	$(MKDIR) $$(dirname $@)
 	[[ -f $@ ]] && $(CAT) $@ 2>/dev/null | $(GREP) -v -e "^#" -e "^\s\+$$"| $(GREP) -q "^." || \
 			$(LN) -sf $< $@
