96429ccb05981afee3aa91462329c6559fbb0610
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-20 00:45:38 +0100

    use posix sed for increased compatibility gnu/bsd

--- build.mk/core.misc.source-const-inc.mk
+++ build.mk/core.misc.source-const-inc.mk
@@ -1,7 +1,7 @@
 # CONST.inc
 ifneq (,$(wildcard $(GIT_ROOT)/CONST.inc))
 include $(GIT_ROOT)/CONST.inc
-export $(shell $(SED) 's/=.*//' $(GIT_ROOT)/CONST.inc)
+export $(shell $(SED) 's/=.\{0,\}//' $(GIT_ROOT)/CONST.inc)
 endif
 
 # CONST.inc.secret
@@ -10,7 +10,7 @@ SF_IS_TRANSCRYPTED ?=
 ifeq (true,$(SF_IS_TRANSCRYPTED))
 
 include $(GIT_ROOT)/CONST.inc.secret
-export $(shell $(SED) 's/=.*//' $(GIT_ROOT)/CONST.inc.secret)
+export $(shell $(SED) 's/=.\{0,\}//' $(GIT_ROOT)/CONST.inc.secret)
 
 endif
 endif
