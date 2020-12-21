77b7bab90b952bbecdfdb7d5b3dfa808f5444fc3
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-21 11:30:28 +0100

    fixup! use posix sed for increased compatibility gnu/bsd

--- build.mk/core.misc.codeship-ci.mk
+++ build.mk/core.misc.codeship-ci.mk
@@ -17,7 +17,7 @@ jet-steps:
 		--ci-committer-email "$$($(GIT) log -1 --format=%ce $(GIT_HASH))" \
 		--ci-committer-name "$$($(GIT) log -1 --format=%cn $(GIT_HASH))" \
 		--ci-repo-name "$$($(GIT) remote -v 2>/dev/null | \
-		$(GREP) -oP "(?<=github.com.).+" | \
-		$(GREP) -oP ".+(?= \(fetch\))" | \
+		$(GREP) -oP "(?<=github.com.).\{1,\}" | \
+		$(GREP) -oP ".\{1,\}(?= \(fetch\))" | \
 		$(HEAD) -n1 | \
 		$(SED) "s/\.git$$//")" \
