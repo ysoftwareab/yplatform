06a00fdfd510ead400cd07e56dc375f3fd98ecea
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-21 19:26:12 +0100

    fixup! use posix sed and no labels for increased compatibility gnu/bsd

--- os.inc.mk
+++ os.inc.mk
@@ -4,5 +4,5 @@ ARCH_BIT = $(shell $(UNAME) -m | grep -q "x86_64" && echo "64" || echo "32")
 $(foreach VAR,ARCH ARCH_SHORT ARCH_BIT,$(call make-lazy,$(VAR)))
 
 OS = $(shell $(UNAME) | $(TR) "[:upper:]" "[:lower:]")
-OS_SHORT = $(shell $(ECHO) $(OS) | $(SED) "s/^\([[:alpha:]]\{1,\}\).*\$/\1/g")
+OS_SHORT = $(shell $(ECHO) $(OS) | $(SED) "s/^\([[:alpha:]]\{1,\}\).*\$$/\1/g")
 $(foreach VAR,OS OS_SHORT,$(call make-lazy,$(VAR)))
