408f45710020ee92988f9b2986832fb3676611ec
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-23 17:12:11 +0100

    clarify node is installed only in SF_CI_BREW_INSTALL=minimal

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -4,4 +4,4 @@
 [[ "${CI:-}" = "true" ]] || SF_CI_BREW_INSTALL=${SF_CI_BREW_INSTALL:-dev}
 
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-${SF_CI_BREW_INSTALL}.inc.sh
-source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
+[[ "${${SF_CI_BREW_INSTALL}}" != "minimal" ]] || source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
