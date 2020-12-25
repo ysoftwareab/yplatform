c6541abca992fec2e7d676e25bdca84623a39885
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-25 11:52:49 +0100

    optimize installation of node packages

--- bootstrap/brew-install-node.inc.sh
+++ bootstrap/brew-install-node.inc.sh
@@ -6,17 +6,19 @@ if [[ "${SF_SKIP_COMMON_BOOTSTRAP:-}" = "true" ]]; then
     echo_skip "brew: Installing NodeJS packages..."
 else
     echo_do "brew: Installing NodeJS packages..."
+    # NOTE node can be installed in such a way that
+    # - "npm i -g" executables are not available in PATH
+    # - "npm i -g" is missing the right permissions
+    # therefore we always install it via homebrew
+    # unless_exe_and_grep_q_then "node --version | head -1" "^v" brew_install_one node
     brew_install_one node
+    exe_and_grep_q "node --version | head -1" "^v"
 
     # allow npm upgrade to fail on WSL; fails with EACCESS
-    npm install --global --force npm@6 || ${SUPPORT_FIRECLOUD_DIR}/bin/is-wsl
-    npm install --global json@9
-    hash -r
-    echo_done
+    unless_exe_and_grep_q_then "npm --version | head -1" "^6\." \
+        npm install --global --force npm@6 || ${SUPPORT_FIRECLOUD_DIR}/bin/is-wsl
 
-    echo_do "brew: Testing NodeJS packages..."
-    exe_and_grep_q "node --version | head -1" "^v"
-    exe_and_grep_q "npm --version | head -1" "^6\."
-    exe_and_grep_q "json --version | head -1" "^json 9\."
+    unless_exe_and_grep_q_then "json --version | head -1" "^json 9\." \
+        npm install --global json@9
     echo_done
 fi
