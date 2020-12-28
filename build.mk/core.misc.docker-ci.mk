e0c048114884eaee1ddbb667fe7d1d7c7bca6296
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-28 16:46:31 +0100

    fixup! clarify that before-install.pre has nothing to do with a phase

--- build.mk/core.misc.docker-ci.mk
+++ build.mk/core.misc.docker-ci.mk
@@ -14,9 +14,8 @@ SF_DOCKER_CI_IMAGE ?= $(shell source $(GIT_ROOT)/.ci.sh && sf_get_docker_ci_imag
 docker-ci:
 	$(eval CONTAINER_NAME := $(shell echo "sf-docker-ci-$$(basename $(PWD))"))
 	source $(SUPPORT_FIRECLOUD_DIR)/sh/common.inc.sh && \
-		source $(SUPPORT_FIRECLOUD_DIR)/repo/ci.sh/before-install.pre.inc.sh && \
+		source $(SUPPORT_FIRECLOUD_DIR)/ci/run.docker-ci.inc.sh && \
 		sf_run_docker_ci_image $(SF_DOCKER_CI_IMAGE) $(PWD) $(CONTAINER_NAME)
 	$(ECHO) "[WARN] Make sure to export relevant environment variables!"
-	$(ECHO) "       e.g. secrets in the web UI of Travis CI."
 	docker exec -it -w $(PWD) -u $$(id -u):$$(id -g) $(CONTAINER_NAME) ./.ci.sh debug || true
 	docker kill $(CONTAINER_NAME)
