8e556386fb493e88d6b08a83c38e2852016ce7d6
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-30 00:35:08 +0100

    fix missing changelog in git tags (github releases) after 02745be3

--- build.mk/core.misc.version.mk
+++ build.mk/core.misc.version.mk
@@ -104,5 +104,5 @@ _version:
 	$(GIT) commit -m "$(PKG_VSN_NEW) release notes"
 	$(NPM) version $(PKG_VSN_NEW)
 	$(GIT) tag $(VSN_TAG) $(VSN_TAG)^{} -f \
-		-F <($(ECHO) ""; $$($(CAT) release-notes/$(VSN_TAG).txt))
+		-F <($(ECHO) ""; $(CAT) release-notes/$(VSN_TAG).txt)
 	@$(ECHO_DONE)
