85b9245134759c714a2911ae7bdbaf03e32cef43
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-01 14:31:35 +0100

    handle no branch

--- sh/core.inc.sh
+++ sh/core.inc.sh
@@ -53,10 +53,12 @@ OS=$(uname | tr "[:upper:]" "[:lower:]")
 OS_SHORT=$(echo "${OS}" | sed "s/^\([a-z]\+\).*/\1/g")
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || true)
-GIT_BRANCH_SHORT=$(basename ${GIT_BRANCH} 2>/dev/null || true)
+GIT_BRANCH_SHORT=
+[[ -z "${GIT_BRANCH}" ]] || GIT_BRANCH_SHORT=$(basename ${GIT_BRANCH} 2>/dev/null || true)
 GIT_HASH=$(git rev-parse HEAD 2>/dev/null || true)
 GIT_HASH_SHORT=$(git rev-parse --short HEAD 2>/dev/null || true)
-GIT_REMOTE=$(git config branch.${GIT_BRANCH}.remote 2>/dev/null || true)
+GIT_REMOTE=origin
+[[ -z "${GIT_BRANCH}" ]] || GIT_REMOTE=$(git config branch.${GIT_BRANCH}.remote 2>/dev/null || true)
 GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || true)
 GIT_TAGS=$(git describe --exact-match --tags HEAD 2>/dev/null || true)
 
