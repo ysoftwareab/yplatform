ad7954fb9427f9082035cec6a53fcfa4d9826968
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-07 22:52:24 +0100

    docker: allow git config to be customized

--- dockerfiles/util/gitconfig.inc.sh
+++ dockerfiles/util/gitconfig.inc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git config --global user.email "${SF_DOCKER_CI_IMAGE_NAME}.${SF_DOCKER_CI_IMAGE_TAG}@docker"
-git config --global user.name "${SF_DOCKER_CI_IMAGE_NAME} ${SF_DOCKER_CI_IMAGE_TAG}"
+git config --global user.email "${GIT_USER_EMAIL}"
+git config --global user.name "${GIT_USER_NAME}"
