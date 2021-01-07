e85b05f5bf33d16315b1f7be794089acfde04a00
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-07 22:51:53 +0100

    refactor codeship. also fix bionic to actually use ubuntu:18.04 not 16.04

---
+++ .codeship/ubuntu-16.04/Dockerfile.build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SUPPORT_FIRECLOUD_DIR=/support-firecloud
+source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/env.inc.sh
+# shellcheck disable=SC2034
+UNAME=codeship
+# shellcheck disable=SC2034
+GNAME=codeship
+# shellcheck disable=SC2034
+GIT_USER_EMAIL="bot@codeship.com"
+# shellcheck disable=SC2034
+GIT_USER_NAME="Codeship"
+
+# DEPS
+source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/apt.inc.sh
+[[ -f /support-firecloud.bootstrapped ]] || {
+    apt_update
+    apt_install_one apt-transport-https
+    apt_install_one ca-certificates
+    apt_install_one software-properties-common
+    apt_install_one gnupg-agent
+    apt_install_one git
+    apt_install_one openssl
+    apt_install_one ssh-client
+    apt_install_one sudo
+
+    source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/gitconfig.inc.sh
+    source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/user.inc.sh
+}
+
+# source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/bootstrap.inc.sh
+# source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/clean.inc.sh
