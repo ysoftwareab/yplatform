b37298485ff71a154f2fd238b0154dfa5a5d0222
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-11 17:32:22 +0100

    use github event's pull request url, instead of re-creating it manually

--- ci/env.github-actions.inc.sh
+++ ci/env.github-actions.inc.sh
@@ -13,16 +13,12 @@ CI_DEBUG_MODE=${CI_DEBUG_MODE:-}
 CI_JOB_ID=$(${SUPPORT_FIRECLOUD_DIR}/bin/github-get-job-id || true)
 # CI_JOB_URL="https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA}/checks?check_suite_id=FIXME"
 CI_JOB_URL="https://github.com/${GITHUB_REPOSITORY}/runs/${CI_JOB_ID}?check_suite_focus=true"
-CI_PR_SLUG=
-if [[ "${GITHUB_EVENT_NAME}" = "pull_request" ]]; then
-    CI_PR_NUMBER=$(${SUPPORT_FIRECLOUD_DIR}/bin/jq -r .github.event.number ${GITHUB_EVENT_PATH})
-    CI_PR_SLUG=https://github.com/${GITHUB_REPOSITORY}/pull/${CI_PR_NUMBER}
-    unset CI_PR_NUMBER
-fi
 CI_REPO_SLUG=${GITHUB_REPOSITORY}
 CI_IS_PR=false
+CI_PR_SLUG=
 if [[ "${GITHUB_EVENT_NAME}" = "pull_request" ]]; then
     CI_IS_PR=true
+    CI_PR_SLUG=$(${SUPPORT_FIRECLOUD_DIR}/bin/jq -r .github.event.pull_request.url ${GITHUB_EVENT_PATH})
 fi
 CI_IS_CRON=false
 if [[ -z "${GITHUB_EVENT_NAME}" ]]; then
