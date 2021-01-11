abb4f04a59879cb0acde320176f48e8206547834
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-11 20:45:25 +0100

    better sudo compat with ubuntu:12.04

--- dockerfiles/util/bootstrap.inc.sh
+++ dockerfiles/util/bootstrap.inc.sh
@@ -12,7 +12,9 @@ git config --replace-all url."https://github.com/".insteadOf "git@github.com:"
 git config --add url."https://github.com/".insteadOf "git://github.com/"
 
 export SF_DOCKER=true
-sudo --preserve-env -H -u ${UNAME} ./bootstrap/linux/bootstrap
+# TODO --preserve-env is not present in the version of ubuntu:12.04 sudo
+# sudo --preserve-env -H -u ${UNAME} ./bootstrap/linux/bootstrap
+sudo -E -H -u ${UNAME} ./bootstrap/linux/bootstrap
 unset SF_DOCKER
 
 source ${SUPPORT_FIRECLOUD_DIR}/sh/env.inc.sh
