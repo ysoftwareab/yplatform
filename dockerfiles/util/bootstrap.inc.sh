d15e22fd72d2acb12ddfece8807855d53598871b
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-11 21:45:43 +0100

    Revert "better sudo compat with ubuntu:12.04"
    This reverts commit 77e799906b203ee6e863ced8115656154783e86f.

--- dockerfiles/util/bootstrap.inc.sh
+++ dockerfiles/util/bootstrap.inc.sh
@@ -12,9 +12,7 @@ git config --replace-all url."https://github.com/".insteadOf "git@github.com:"
 git config --add url."https://github.com/".insteadOf "git://github.com/"
 
 export SF_DOCKER=true
-# TODO --preserve-env is not present in the version of ubuntu:12.04 sudo
-# sudo --preserve-env -H -u ${UNAME} ./bootstrap/linux/bootstrap
-sudo -E -H -u ${UNAME} ./bootstrap/linux/bootstrap
+sudo --preserve-env -H -u ${UNAME} ./bootstrap/linux/bootstrap
 unset SF_DOCKER
 
 source ${SUPPORT_FIRECLOUD_DIR}/sh/env.inc.sh
