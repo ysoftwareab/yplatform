0b162379254a0dc038015ac69121357b21fbeb24
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-14 23:17:33 +0100

    add fixme for arch_short

--- sh/core.inc.sh
+++ sh/core.inc.sh
@@ -48,6 +48,7 @@ fi
 
 ARCH=$(uname -m)
 ARCH_BIT=$(echo "${ARCH}" | grep -q "x86_64" && echo "64" || echo "32")
+# FIXME should handle arm64 aarch64 others?! Same in build.mk/core.inc.mk/os.inc.mk
 ARCH_SHORT=$(echo "${ARCH}" | grep -q "x86_64" && echo "x64" || echo "x86")
 OS=$(uname | tr "[:upper:]" "[:lower:]")
 OS_SHORT=$(echo "${OS}" | sed "s/^\([[:alpha:]]\{1,\}\).*/\1/g")
