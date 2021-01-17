b20d9325fa2f08eb3878d8be7e5eec7812d549b9
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-17 18:49:16 +0100

    fixup! enable newer git in github actions by actions/checkout

--- dockerfiles/util/bootstrap.inc.sh
+++ dockerfiles/util/bootstrap.inc.sh
@@ -24,4 +24,4 @@ make VERSION
 git rev-parse HEAD > /support-firecloud.bootstrapped
 
 # enable newer git in github actions by actions/checkout, or else no history/submodules/etc with REST API checkouts
-ln -s /home/linuxbrew/.linuxbrew/bin/git /usr/local/bin/git
+[[ -e /usr/local/bin/git ]] || ln -s /home/linuxbrew/.linuxbrew/bin/git /usr/local/bin/git
