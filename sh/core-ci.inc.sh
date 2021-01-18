5e2d4c784a85bc14f150d66807cbbe339b2e3ad7
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-18 22:47:30 +0100

    fixup! try to fix HOME for containers on github actions

--- sh/core-ci.inc.sh
+++ sh/core-ci.inc.sh
@@ -8,6 +8,12 @@ HOME_REAL=$(eval echo "~$(id -u -n)")
 [[ "${HOME}" = "${HOME_REAL}" ]] || {
     >&2 echo "[WARN] \$HOME was ${HOME}. It is now reset to ${HOME_REAL}."
     export HOME="${HOME_REAL}"
+
+    # NOTE ideally we would unset all current variables,
+    # but we need to detect and retain those set in the CI configuration
+    # e.g. not only GITHUB_*, but also any given in "env:" as part of a workflow
+    # so instead we only update current variables
+    eval "$(env -i HOME="${HOME}" bash -l -c "printenv" | sed "s/^/export /")"
 }
 unset HOME_REAL
 
