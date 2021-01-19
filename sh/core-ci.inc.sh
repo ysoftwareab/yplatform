aa4c01986d40412ccc79b423eb51ae0f528bd013
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-19 01:25:14 +0100

    fixup! fixup! try to fix HOME for containers on github actions

--- sh/core-ci.inc.sh
+++ sh/core-ci.inc.sh
@@ -6,14 +6,20 @@ true
 # see https://github.com/actions/runner/issues/863
 HOME_REAL=$(eval echo "~$(id -u -n)")
 [[ "${HOME}" = "${HOME_REAL}" ]] || {
-    >&2 echo "[WARN] \$HOME was ${HOME}. It is now reset to ${HOME_REAL}."
+    >&2 echo "$(date +"%H:%M:%S") [WARN] \$HOME was ${HOME}."
+    >&2 echo "$(date +"%H:%M:%S") [DO  ] Resetting HOME to ${HOME_REAL}..."
     export HOME="${HOME_REAL}"
 
     # NOTE ideally we would unset all current variables,
     # but we need to detect and retain those set in the CI configuration
     # e.g. not only GITHUB_*, but also any given in "env:" as part of a workflow
     # so instead we only update current variables
-    eval "$(env -i HOME="${HOME}" bash -l -c "printenv" | sed "s/^/export /")"
+    XTRACE_STATE="$(shopt -po xtrace || true)" # shopt exits with non zero?
+    set -x
+    eval "$(env -i HOME="${HOME}" bash -l -i -c "printenv" | sed "s/^/export /")"
+    eval "${XTRACE_STATE}"
+    unset XTRACE_STATE
+    >&2 echo "$(date +"%H:%M:%S") [DONE]"
 }
 unset HOME_REAL
 
