a79228d008ea82a82e94f32edd623602dc77226c
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-22 14:11:08 +0100

    fix GITHUB_ACTION_PATH for the script to be standalone, even if it should be already ok from action.yml

--- action.sh
+++ action.sh
@@ -3,6 +3,10 @@
 # FIXME https://github.com/actions/runner/issues/863
 source "${GITHUB_ACTION_PATH}/sh/core-ci-home.inc.sh"
 
+# FIXME https://github.com/actions/runner/issues/716
+[[ -d "${GITHUB_ACTION_PATH}" ]] || \
+    GITHUB_ACTION_PATH=/__w/_actions/${GITHUB_ACTION_PATH#/home/runner/work/_actions/}
+
 [[ "${INPUT_DEBUG}" != "true" ]] || {
     echo ::group::github event
     cat "${GITHUB_EVENT_PATH}"
