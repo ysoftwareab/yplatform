318e885b0b24f1fe520e55526828eceb7192f96c
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-22 22:21:52 +0100

    Revert "add group for HOME_REAL"
    This reverts commit ee62f3e327d9457f2776514479079514d5625ff5.

--- sh/core-ci.inc.sh
+++ sh/core-ci.inc.sh
@@ -2,9 +2,7 @@
 # shellcheck disable=SC2034
 true
 
-[[ -z "${GITHUB_ACTIONS:-}" ]] || echo ::group::HOME
 source ${SUPPORT_FIRECLOUD_DIR}/sh/core-ci-home.inc.sh
-[[ -z "${GITHUB_ACTIONS:-}" ]] || echo ::envgroup::
 
 [[ -z "${TRAVIS_BRANCH:-}" ]] || {
     GIT_BRANCH=${TRAVIS_BRANCH}
