ee62f3e327d9457f2776514479079514d5625ff5
Andrei Neculau <andrei.neculau@gmail.com>
2021-01-22 17:40:26 +0100

    add group for HOME_REAL

--- sh/core-ci.inc.sh
+++ sh/core-ci.inc.sh
@@ -2,7 +2,9 @@
 # shellcheck disable=SC2034
 true
 
+[[ -z "${GITHUB_ACTIONS:-}" ]] || echo ::group::HOME
 source ${SUPPORT_FIRECLOUD_DIR}/sh/core-ci-home.inc.sh
+[[ -z "${GITHUB_ACTIONS:-}" ]] || echo ::envgroup::
 
 [[ -z "${TRAVIS_BRANCH:-}" ]] || {
     GIT_BRANCH=${TRAVIS_BRANCH}
