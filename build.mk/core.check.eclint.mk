9def094ba8e54fb4213e30968e1a91b0c071aa53
Andrei Neculau <andrei.neculau@gmail.com>
2021-03-06 22:24:01 +0100

    breaking: remove support for node_modules/.bin/eclint, rename make target 'check-eclint' to 'check-editorconfig'

--- build.mk/core.check.eclint.mk
+++ build.mk/core.check.eclint.mk
@@ -1,16 +1,13 @@
-# Adds a 'check-eclint' internal target to run 'editorconfig-checker', or fallback to deprecated 'eclint',
-# over SF_ECLINT_FILES (defaults to all committed and staged files).
-# The 'check-eclint' target is automatically added to the 'check' target via SF_CHECK_TARGETS.
+# Adds a 'check-editoconfig' internal target to run 'editorconfig-checker',
+# over SF_ECCHECKER_FILES (defaults to all committed and staged files).
+# The 'check-editorconfig' target is automatically added to the 'check' target via SF_CHECK_TARGETS.
 #
 # The editorconfig-checker executable is lazy-found inside $PATH.
 # The arguments to the editorconfig-checker executable can be changed via ECCHECKER_ARGS.
 #
-# The eclint executable is lazy-found inside node_modules/.bin and $PATH.
-# The arguments to the eclint executable can be changed via ECLINT_ARGS.
-#
 # For convenience, specific files can be ignored
-# via grep arguments given to SF_ECLINT_FILES_IGNORE:
-# SF_ECLINT_FILES_IGNORE += \
+# via grep arguments given to SF_ECCHECKER_FILES_IGNORE:
+# SF_ECCHECKER_FILES_IGNORE += \
 #	-e "^path/to/dir/" \
 #	-e "^path/to/file$" \
 #
@@ -21,40 +18,34 @@
 SF_IS_TRANSCRYPTED ?= false
 
 ECCHECKER = $(call which,ECCHECKER,editorconfig-checker) --ignore-defaults
-ECLINT = $(call npm-which,ECLINT,eclint)
-$(foreach VAR,ECCHECKER ECLINT,$(call make-lazy,$(VAR)))
+$(foreach VAR,ECCHECKER,$(call make-lazy,$(VAR)))
 
 ECCHECKER_ARGS += \
 
-ECLINT_ARGS += \
-
+# backwards compat
 SF_ECLINT_FILES_IGNORE += \
+
+SF_ECCHECKER_FILES_IGNORE += \
 	-e "^$$" \
+	$(SF_ECLINT_FILES_IGNORE) \
 	$(SF_VENDOR_FILES_IGNORE) \
 
-SF_ECLINT_FILES += $(shell $(GIT_LS) . | \
+SF_ECCHECKER_FILES += $(shell $(GIT_LS) . | \
 	$(GREP) -Fvxf <($(FIND) $(GIT_ROOT) -type l -printf "%P\n") | \
 	$(GREP) -Fvxf <($(SF_IS_TRANSCRYPTED) || [[ ! -x $(GIT_ROOT)/transcrypt ]] || $(GIT_ROOT)/transcrypt -l) | \
 	$(GREP) -Fvxf <($(GIT) config --file .gitmodules --get-regexp path | $(CUT) -d' ' -f2 || true) | \
-	$(GREP) -v $(SF_ECLINT_FILES_IGNORE) | \
+	$(GREP) -v $(SF_ECCHECKER_FILES_IGNORE) | \
 	$(SED) "s/^/'/g" | \
 	$(SED) "s/$$/'/g")
 
 SF_CHECK_TARGETS += \
-	check-eclint \
+	check-editorconfig \
 
 # ------------------------------------------------------------------------------
 
-.PHONY: check-eclint
-check-eclint:
-	SF_ECLINT_FILES_TMP=($(SF_ECLINT_FILES)); \
-	[[ "$${#SF_ECLINT_FILES_TMP[@]}" = "0" ]] || { \
-		if [[ "$(ECCHECKER)" = "ECCHECKER_NOT_FOUND" ]]; then \
-			$(ECLINT) check $(ECLINT_ARGS) $${SF_ECLINT_FILES_TMP[@]} || { \
-				$(ECLINT) fix $(ECLINT_ARGS) $${SF_ECLINT_FILES_TMP[@]} 2>/dev/null >&2; \
-				exit 1; \
-			}; \
-		else \
-			$(ECCHECKER) $(ECCHECKER_ARGS) $${SF_ECLINT_FILES_TMP[@]}; \
-		fi; \
+.PHONY: check-editorconfig
+check-editorconfig:
+	SF_ECCHECKER_FILES_TMP=($(SF_ECCHECKER_FILES)); \
+	[[ "$${#SF_ECCHECKER_FILES_TMP[@]}" = "0" ]] || { \
+		$(ECCHECKER) $(ECCHECKER_ARGS) $${SF_ECCHECKER_FILES_TMP[@]}; \
 	}
