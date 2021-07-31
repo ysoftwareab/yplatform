11f559b00a1736f769b9d199c5e2b901a2e8e5b3
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-01 01:16:38 +0200

    fixup! bootstrap: add git-diff helpers to common

--- bootstrap/brew-install-git-diff.inc.sh
+++ bootstrap/brew-install-git-diff.inc.sh
@@ -8,7 +8,12 @@ brew_install_one_if exiftool "exiftool -ver | head -1" "^12\."
 brew_install_one_if odt2txt "odt2txt --version | head -1" "^odt2txt 0\." || {
     # no bottle on linuxbrew
     brew install --build-from-source odt2txt
+    exe_and_grep_q "odt2txt --version | head -1" "^odt2txt 0\."
 }
-brew_install_one_if poppler "pdftotext -v | head -1" "^pdftotext version 2[0-9]\."
+brew_install_one_if poppler "pdftotext -v | head -1" "^pdftotext version 2[0-9]\." || {
+    # no bottle on linuxbrew
+    brew install --build-from-source poppler
+    exe_and_grep_q "pdftotext -v | head -1" "^pdftotext version 2[0-9]\."
+}
 brew_install_one_if xz "xz --version | head -1" "^xz (XZ Utils) 5\."
 echo_done
