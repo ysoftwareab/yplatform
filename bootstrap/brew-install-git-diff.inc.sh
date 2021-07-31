9acc9f5d34781eadc1b8eca72557a9970558ed5c
Andrei Neculau <andrei.neculau@gmail.com>
2021-07-31 22:57:25 +0200

    fixup! bootstrap: add git-diff helpers to common

--- bootstrap/brew-install-git-diff.inc.sh
+++ bootstrap/brew-install-git-diff.inc.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo_do "brew: Installing git-diff helpers..."
-brew_install_one_if bzip2 "bzip2 --version | head -1" "^bzip2, a block-sorting file compressor.  Version 1\."
+brew_install_one_if bzip2 "bzip2 --help 2>&1 | head -1" "^bzip2, a block-sorting file compressor.  Version 1\."
 brew_install_one_if docx2txt "docx2txt.sh | head -2 | tail -1" "^Usage :"
 brew_install_one_if exiftool "exiftool -ver | head -1" "^12\."
 brew_install_one_if odt2txt "odt2txt --version | head -1" "^odt2txt 0\."
