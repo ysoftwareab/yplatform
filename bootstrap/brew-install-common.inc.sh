dc226888632f0f41ed805a5f264604f34102bca2
Andrei Neculau <andrei.neculau@gmail.com>
2021-07-31 21:49:20 +0200

    bootstrap: add git-diff helpers to common

--- bootstrap/brew-install-common.inc.sh
+++ bootstrap/brew-install-common.inc.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo_do "brew: Installing common packages..."
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-minimal.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-git-diff.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-docker.inc.sh
 echo_done
