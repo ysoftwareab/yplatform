fd7efe35f664056a83dc72d791708e80071b4aec
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-02 18:39:59 +0200

    sync debian/ubuntu dockerfile's verbosity with others

--- dockerfiles/sf-debian/Dockerfile.build.sh
+++ dockerfiles/sf-debian/Dockerfile.build.sh
@@ -7,6 +7,7 @@ source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/env.inc.sh
 # DEPS
 source ${SUPPORT_FIRECLOUD_DIR}/dockerfiles/util/apt.inc.sh
 [[ -f /support-firecloud.bootstrapped ]] || {
+    set -x
     apt_update
 
     apt_install_one apt-transport-https
