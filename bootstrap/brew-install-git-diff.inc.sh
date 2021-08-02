d6859517efb12c226b4683d5f77a5081279a967e
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-02 13:19:16 +0200

    use magic_install for odt2txt and poppler on linux

--- bootstrap/brew-install-git-diff.inc.sh
+++ bootstrap/brew-install-git-diff.inc.sh
@@ -9,26 +9,26 @@ brew_install_one_if docx2txt "docx2txt.sh | head -2 | tail -1" "^Usage :"
     brew_install_one_if exiftool "exiftool -ver | head -1" "^12\."
 }
 brew_install_one_if odt2txt "odt2txt --version | head -1" "^odt2txt 0\." || {
-    # no bottle on linuxbrew
-    brew install --build-from-source odt2txt
-    exe_and_grep_q "odt2txt --version | head -1" "^odt2txt 0\."
+    # handle no bottle on linuxbrew
+    EXIT_CODE=$?
+    [[ "${OS_SHORT}" = "linux" ]] || exit ${EXIT_CODE}
+    echo_warn "Falling back to installing odt2txt outside Homebrew..."
+
+    # ODT2TXT_VSN="$(brew info --json=v1 odt2txt | jq -r ".[0].versions.stable")"
+    # magic_install_one_if odt2txt@${ODT2TXT_VSN} "odt2txt --version | head -1" "^odt2txt 0\."
+    magic_install_one_if odt2txt "odt2txt --version | head -1" "^odt2txt 0\."
 }
 # NOTE alternatively one can also install xpdf instead of poppler. Uncertain about the diffs.
 # brew_install_one_if xpdf "pdftotext -v 2>&1 | head -1" "^pdftotext version 4\."
 brew_install_one_if poppler "pdftotext -v 2>&1 | head -1" "^pdftotext version 2[0-9]\." || {
-    # no bottle on linuxbrew
-    # brew ls | grep -q "qt@5" || brew install --build-from-source qt@5
-    # brew install --build-from-source poppler
-
+    # handle no bottle on linuxbrew
     EXIT_CODE=$?
     [[ "${OS_SHORT}" = "linux" ]] || exit ${EXIT_CODE}
     echo_warn "Falling back to installing Poppler outside Homebrew..."
 
     # POPPLER_VSN="$(brew info --json=v1 poppler | jq -r ".[0].versions.stable")"
-    # magic_install_one poppler@${POPPLER_VSN}
-    # exe_and_grep_q "pdftotext -v 2>&1 | head -1" "^pdftotext version 2[0-9]\."
-    magic_install_one poppler
-    exe_and_grep_q "pdftotext -v 2>&1 | head -1" "^pdftotext version "
+    # magic_install_one_if poppler@${POPPLER_VSN} "pdftotext -v 2>&1 | head -1" "^pdftotext version 2[0-9]\."
+    magic_install_one_if poppler "pdftotext -v 2>&1 | head -1" "^pdftotext version "
 }
 brew_install_one_if xz "xz --version | head -1" "^xz (XZ Utils) 5\."
 echo_done
