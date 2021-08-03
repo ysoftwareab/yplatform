14050002aaf1e8a6f250009d3a9d1581df531977
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-03 08:16:52 +0200

    [andreineculau/core.inc.mk] use 'command -v' instead of which

--- mk/exe.inc.mk
+++ mk/exe.inc.mk
@@ -1,11 +1,13 @@
+# WHICH_Q is kept for backward compatibility
 WHICH_Q := 2>/dev/null which
+COMMAND_Q := 2>/dev/null command -v
 
 define global-which
-	$(shell export RESULT="$$(for CMD in $(2); do $(WHICH_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
+	$(shell export RESULT="$$(for CMD in $(2); do $(COMMAND_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
 endef
 
 define which
-	$(shell export PATH="$(PATH)"; export RESULT="$$(for CMD in $(2); do $(WHICH_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
+	$(shell export PATH="$(PATH)"; export RESULT="$$(for CMD in $(2); do $(COMMAND_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
 endef
 
 include $(CORE_INC_MK_DIR)/exe.gnu.inc.mk
