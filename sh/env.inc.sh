7872f3ef6a929f28702f54473722e8339ab5fadd
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-04 17:54:13 +0200

    add sf_path_prepend_after and sf_path_append_before

--- sh/env.inc.sh
+++ sh/env.inc.sh
@@ -14,10 +14,30 @@
 
 function sf_path_prepend() {
     echo ":${PATH}:" | grep -q ":$1:" || export PATH=$1:${PATH}
+    export PATH=$(echo "${PATH}" | sed "s|^:||" | sed "s|:$||")
+}
+
+function sf_path_prepend_after() {
+    if echo ":${PATH}:" | grep -q ":$2:"; then
+        export PATH=$(echo "${PATH}" | sed "s/:$2:/:$2:$1:/")
+    else
+        sf_path_prepend "$1"
+    fi
+    export PATH=$(echo "${PATH}" | sed "s|^:||" | sed "s|:$||")
 }
 
 function sf_path_append() {
     echo ":${PATH}:" | grep -q ":$1:" || export PATH=${PATH}:$1
+    export PATH=$(echo "${PATH}" | sed "s|^:||" | sed "s|:$||")
+}
+
+function sf_path_append_before() {
+    if echo ":${PATH}:" | grep -q ":$2:"; then
+        export PATH=$(echo "${PATH}" | sed "s/:$2:/:$1:$2:/")
+    else
+        sf_path_append "$1"
+    fi
+    export PATH=$(echo "${PATH}" | sed "s|^:||" | sed "s|:$||")
 }
 
 [[ "${SF_DEV_INC_SH:-}" = "true" ]] || {
