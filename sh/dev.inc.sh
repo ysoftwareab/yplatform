2ad68e97e78c9237f74f1f26b1149952fa84b49e
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-04 18:28:19 +0200

    source dev.inc.sh only once

--- sh/dev.inc.sh
+++ sh/dev.inc.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-if [ -n "${BASH_VERSION:-}" ]; then
-    GLOBAL_SUPPORT_FIRECLOUD_DIR="${GLOBAL_SUPPORT_FIRECLOUD_DIR:-$(dirname ${BASH_SOURCE[0]})/..}"
-    GLOBAL_SUPPORT_FIRECLOUD_DIR="$(cd "${GLOBAL_SUPPORT_FIRECLOUD_DIR}" && pwd)"
-elif [ -n "${ZSH_VERSION:-}" ]; then
-    GLOBAL_SUPPORT_FIRECLOUD_DIR="${GLOBAL_SUPPORT_FIRECLOUD_DIR:-$(dirname ${(%):-%x})/..}"
-    GLOBAL_SUPPORT_FIRECLOUD_DIR="$(cd "${GLOBAL_SUPPORT_FIRECLOUD_DIR}" && pwd)"
-    autoload -U compaudit compinit bashcompinit
-    bashcompinit || {
-        echo >&2 "Initialization of zsh completion features has failed in"
-        echo >&2 "${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/dev.inc.sh."
-    }
-else
-    echo >&2 "Unsupported shell or \$BASH_VERSION and \$ZSH_VERSION are undefined."
-fi
+[[ "${SF_DEV_INC_SH:-}" = "true" ]] || {
+    if [ -n "${BASH_VERSION:-}" ]; then
+        GLOBAL_SUPPORT_FIRECLOUD_DIR="${GLOBAL_SUPPORT_FIRECLOUD_DIR:-$(dirname ${BASH_SOURCE[0]})/..}"
+        GLOBAL_SUPPORT_FIRECLOUD_DIR="$(cd "${GLOBAL_SUPPORT_FIRECLOUD_DIR}" && pwd)"
+    elif [ -n "${ZSH_VERSION:-}" ]; then
+        GLOBAL_SUPPORT_FIRECLOUD_DIR="${GLOBAL_SUPPORT_FIRECLOUD_DIR:-$(dirname ${(%):-%x})/..}"
+        GLOBAL_SUPPORT_FIRECLOUD_DIR="$(cd "${GLOBAL_SUPPORT_FIRECLOUD_DIR}" && pwd)"
+        autoload -U compaudit compinit bashcompinit
+        bashcompinit || {
+            echo >&2 "Initialization of zsh completion features has failed in"
+            echo >&2 "${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/dev.inc.sh."
+        }
+    else
+        echo >&2 "Unsupported shell or \$BASH_VERSION and \$ZSH_VERSION are undefined."
+    fi
 
-source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/env.inc.sh
-source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/dev-aws-iam-login.inc.sh
+    source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/env.inc.sh
+    source ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/sh/dev-aws-iam-login.inc.sh
 
-sf_path_prepend ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/dev/bin
-sf_path_append ./node_modules/.bin
+    sf_path_prepend ${GLOBAL_SUPPORT_FIRECLOUD_DIR}/dev/bin
+    sf_path_append ./node_modules/.bin
 
-export SF_DEV_INC_SH=true
+    export SF_DEV_INC_SH=true
+}
