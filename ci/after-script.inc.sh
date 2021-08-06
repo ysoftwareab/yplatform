97a1a77358228ebea87b9b69cd9da65087ba0a45
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-06 11:54:57 +0200

    fixup! restore files added to the job git ref

--- ci/after-script.inc.sh
+++ ci/after-script.inc.sh
@@ -75,7 +75,8 @@ EOF
     git checkout -f - || git checkout -f ${GIT_HASH}
     # restore files added above 'git ls-files -X .artifacts --other --ignored | ...'
     git diff --name-only --diff-filter=A ..${JOB_GIT_HASH} | \
-        while read -r NO_XARGS_R; do [[ -n "${NO_XARGS_R}" ]] || continue; git checkout -- "${NO_XARGS_R}"; done
+        while read -r NO_XARGS_R; do [[ -n "${NO_XARGS_R}" ]] || continue; \
+        git checkout ${JOB_GIT_HASH} -- "${NO_XARGS_R}"; done
     git reset
 
     echo_done
