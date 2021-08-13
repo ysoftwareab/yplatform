60f4b25a7b1035268a9246183701bfe99e0c6e33
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-13 16:42:55 +0200

    verbose clean functions

--- dockerfiles/util/clean.inc.sh
+++ dockerfiles/util/clean.inc.sh
@@ -2,23 +2,30 @@
 set -euo pipefail
 
 function dir_clean() {
+    echo_do "${FUNCNAME[0]}: $1..."
+    set -x
     du -hcs $1
     rm -rf $1
+    set +x
+    echo_done
 }
 
 function git_dir_clean() {
+    echo_do "${FUNCNAME[0]}: $1..."
+    set -x
     du -hcs $1
-    (
-        cd $1
-        git clean -xdf .
-        git submodule foreach --recursive git clean -xdf .
-        git reset --hard HEAD
-        git submodule foreach --recursive git reset --hard HEAD
-        git reflog expire --expire=now --all
-    )
+    cd $1
+    git clean -xdf .
+    git submodule foreach --recursive git clean -xdf .
+    git reset --hard HEAD
+    git submodule foreach --recursive git reset --hard HEAD
+    git reflog expire --expire=now --all
     git gc --prune=now
+    cd -
     ${SUPPORT_FIRECLOUD_DIR}/bin/git-shallow $1
     du -hcs $1
+    set +x
+    echo_done
 }
 
 (
