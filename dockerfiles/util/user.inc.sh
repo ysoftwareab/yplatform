873aa4bcf305a10c55aba16edc0b0e8ebcbd490c
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-15 10:17:30 +0200

    dedupe

--- dockerfiles/util/user.inc.sh
+++ dockerfiles/util/user.inc.sh
@@ -22,13 +22,7 @@ echo "Defaults:${UNAME} !secure_path" >> /etc/sudoers
 
 # POST-BOOTSTRAP
 cat <<EOF >> /home/${UNAME}/.bash_aliases
-export HOMEBREW_FAIL_LOG_LINES=100
-export HOMEBREW_FORCE_BREWED_CURL=1
-export HOMEBREW_FORCE_BREWED_GIT=1
-export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-export HOMEBREW_NO_INSTALL_CLEANUP=1
-
+source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-util/env.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/sh/dev.inc.sh
 EOF
 chown ${UID_INDEX}:${GID_INDEX} /home/${UNAME}/.bash_aliases
