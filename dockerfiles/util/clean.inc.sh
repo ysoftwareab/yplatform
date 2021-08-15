e1bab5b43a584c4bdb008c150fe50d845662ec9f
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-15 11:58:02 +0200

    split to git_dir_shallow function

--- dockerfiles/util/clean.inc.sh
+++ dockerfiles/util/clean.inc.sh
@@ -25,6 +25,17 @@ function git_dir_clean() {
         git reflog expire --expire=now --all
         git gc --prune=now
         cd -
+        du -hcs $1
+    } || true
+    set +x
+    echo_done
+}
+
+function git_dir_shallow() {
+    echo_do "${FUNCNAME[0]}: $1..."
+    set -x
+    {
+        du -hcs $1
         ${SUPPORT_FIRECLOUD_DIR}/bin/git-shallow $1
         du -hcs $1
     } || true
@@ -36,6 +47,7 @@ function git_dir_clean() {
     cd ${SUPPORT_FIRECLOUD_DIR}
     git add -f -- BUILD VERSION
     git_dir_clean ${SUPPORT_FIRECLOUD_DIR}
+    git_dir_shallow ${SUPPORT_FIRECLOUD_DIR}
     git reset -- BUILD VERSION
 )
 
@@ -46,5 +58,6 @@ dir_clean /home/${UNAME}/.cache/* # misc cache for sf user
 
 for DIR in /home/linuxbrew/.linuxbrew/Homebrew /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/*/*; do
     git_dir_clean ${DIR}
+    git_dir_shallow ${DIR}
     chown -R ${UNAME}:${GNAME} ${DIR}
 done
