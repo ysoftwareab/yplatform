024504d6b1f05ac7353de30dd559a07c0714a642
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-18 00:02:06 +0200

    bootstrap: add nq to minimal. fix #225

--- bootstrap/brew-install-minimal.inc.sh
+++ bootstrap/brew-install-minimal.inc.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 echo_do "brew: Installing minimal packages..."
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-core.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-asdf.inc.sh
+brew_install_one_if nq "cd $(mktemp -d -t firecloud.XXXXXXXXXX) && nq echo 123 | head -1" "^,"
 echo_done
