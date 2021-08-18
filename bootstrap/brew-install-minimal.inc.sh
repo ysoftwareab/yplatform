f021877b833df8afd0bf5ec80428c97d9c564898
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-18 22:33:01 +0200

    fix nq for alpine

--- bootstrap/brew-install-minimal.inc.sh
+++ bootstrap/brew-install-minimal.inc.sh
@@ -6,8 +6,17 @@ source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-core.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-asdf.inc.sh
 {
     TMP_NQ=$(mktemp -t firecloud.XXXXXXXXXX)
-    brew_install_one_if nq "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
+    case ${OS_RELEASE_ID} in
+        alpine)
+            # "brew install nq" will trigger segmentation fault when calling 'nq'
+            magic_install_one_if nq "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
+            ;;
+        *)
+            brew_install_one_if nq "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
+            ;;
+    esac
     rm -f $(cat ${TMP_NQ})
     rm -f ${TMP_NQ}
+    unset TMP_NQ
 }
 echo_done
