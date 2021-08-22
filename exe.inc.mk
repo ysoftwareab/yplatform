5a664208ec7f79af98fbc8516951f4a09282515d
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-22 15:53:01 +0200

    split into exe.which.inc.mk

--- exe.inc.mk
+++ exe.inc.mk
@@ -33,18 +33,7 @@ export $(1)
 endif
 endef
 
-# WHICH_Q is kept for backward compatibility
-WHICH_Q := 2>/dev/null which
-COMMAND_Q := 2>/dev/null command -v
-
-define global-which
-	$(shell export RESULT="$$(for CMD in $(2); do $(COMMAND_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
-endef
-
-define which
-	$(shell export PATH="$(PATH)"; export RESULT="$$(for CMD in $(2); do $(COMMAND_Q) $${CMD} && break || continue; done)"; echo "$${RESULT:-$(1)_NOT_FOUND}")
-endef
-
+include $(CORE_INC_MK_DIR)/exe.which.inc.mk
 include $(CORE_INC_MK_DIR)/exe.gnu.inc.mk
 include $(CORE_INC_MK_DIR)/exe.echo.inc.mk
 include $(CORE_INC_MK_DIR)/exe.misc.inc.mk
