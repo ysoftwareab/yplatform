ed7c754060a08e787b710fe57031396d18b31db1
Andrei Neculau <andrei.neculau@gmail.com>
2021-08-29 12:31:17 +0200

    alpine: use apk's docker

--- bootstrap/brew-install-docker.inc.sh
+++ bootstrap/brew-install-docker.inc.sh
@@ -4,18 +4,43 @@ set -euo pipefail
 echo_do "brew: Installing Docker packages..."
 
 case ${OS_SHORT}-${OS_RELEASE_ID} in
-    darwin-*|linux-alpine|linux-arch|linux-centos)
+    darwin-*|linux-arch|linux-centos)
         brew_install_one_if docker "docker --version | head -1" "^Docker version \(19\|20\)\."
         brew_install_one_if docker-compose "docker-compose --version | head -1" "^docker-compose version 1\."
         ;;
-    # linux-alpine)
-    #     # https://wiki.alpinelinux.org/wiki/Docker#Installation
-    #     apk_install_one docker
-    #     apk_install_one docker-compose
-    #     addgroup $(whoami) docker
-    #     rc-update add docker boot
-    #     service docker start
-    #     ;;
+    linux-alpine)
+        # docker via linuxbrew will throw
+        # /bin/ps: unrecognized option: p
+        # 5051
+        # BusyBox v1.31.1 () multi-call binary.
+        # Usage: ps [-o COL1,COL2=HEADER]
+        # Show list of processes
+        #     -o COL1,COL2=HEADER	Select columns for display
+        # [signal SIGSEGV: segmentation violation code=0x1 addr=0x28 pc=0x7efe9b00b6e8]
+        # runtime stack:
+        # runtime.throw(0x1e45c1c, 0x2a)
+        #     runtime/panic.go:1117 +0x72
+        # runtime.sigpanic()
+        #     runtime/signal_unix.go:718 +0x2e5
+        # goroutine 1 [running]:
+        # runtime.systemstack_switch()
+        #     runtime/asm_amd64.s:339 fp=0xc000072788 sp=0xc000072780 pc=0x46d940
+        # runtime.main()
+        #     runtime/proc.go:144 +0x89 fp=0xc0000727e0 sp=0xc000072788 pc=0x43b3e9
+        # runtime.goexit()
+        #     runtime/asm_amd64.s:1371 +0x1 fp=0xc0000727e8 sp=0xc0000727e0 pc=0x46f781
+
+        # BEGIN https://wiki.alpinelinux.org/wiki/Docker#Installation
+        apk_install_one docker
+        apk_install_one docker-compose
+        addgroup $(whoami) docker
+        rc-update add docker boot
+        service docker start
+        # END https://wiki.alpinelinux.org/wiki/Docker#Installation
+
+        exe_and_grep_q "docker --version | head -1" "^Docker version \(19\|20\)\."
+        exe_and_grep_q "docker-compose --version | head -1" "^docker-compose version 1\."
+        ;;
     linux-debian|linux-ubuntu)
         # docker-compose via linuxbrew will throw 'Illegal instruction' for e.g. 'docker-compose --version'
         # brew_install_one docker
