e08091d2aa04c2eef2a94010e2c55168aa26eb17
Andrei Neculau <andrei.neculau@gmail.com>
2021-09-02 14:10:17 +0200

    better error message when make is not installed

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 # This script is a dummy script, just to test the functionality of sh/env.inc.sh:make.
 
 [[ -n "${SF_MAKE_COMMAND:-}" ]] || {
-    echo >&2 "[ERR ] Expected SF_MAKE_COMMAND to be defined."
+    echo >&2 "[ERR ] Expected SF_MAKE_COMMAND to be defined. Maybe GNU Make is not available."
     exit 1;
 }
 
