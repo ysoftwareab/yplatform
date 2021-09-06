378a36233c1fe8384b888a38363850668e3b3d45
Andrei Neculau <andrei.neculau@gmail.com>
2021-09-06 15:11:01 +0200

    fix pipeline.script.sh: errexit doesn't work for commands executed in a && or || list
    http://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html

--- ci/pipeline.script.sh
+++ ci/pipeline.script.sh
@@ -8,19 +8,11 @@ after_script() {
 }
 trap after_script EXIT
 
-{
-    ./.ci.sh before_install
-    ./.ci.sh install
-    ./.ci.sh before_script
-    ./.ci.sh script
-} && SF_CI_STATUS=success || SF_CI_STATUS=failure
-
-if [[ "${SF_CI_STATUS}" = "success" ]]; then
-    ./.ci.sh before_cache || true;
-    ./.ci.sh after_success || true;
-fi
-
-if [[ "${SF_CI_STATUS}" = "failure" ]]; then
-    ./.ci.sh after_failure || true;
-    exit 1;
-fi
+SF_CI_STATUS=success
+[[ "${SF_CI_STATUS}" = "success" ]] || ./.ci.sh before_install || SF_CI_STATUS=failure
+[[ "${SF_CI_STATUS}" = "success" ]] || ./.ci.sh install || SF_CI_STATUS=failure
+[[ "${SF_CI_STATUS}" = "success" ]] || ./.ci.sh before_script || SF_CI_STATUS=failure
+[[ "${SF_CI_STATUS}" = "success" ]] || ./.ci.sh script || SF_CI_STATUS=failure
+[[ "${SF_CI_STATUS}" != "success" ]] || ./.ci.sh before_cache || true
+[[ "${SF_CI_STATUS}" != "success" ]] || ./.ci.sh after_success || true
+[[ "${SF_CI_STATUS}" != "failure" ]] || ./.ci.sh after_failure || true
