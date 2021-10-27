a4a21480549ba1071c81d28b80fe6a59fc35c8fc
Andrei Neculau <andrei.neculau@gmail.com>
2021-10-27 09:30:57 +0200

    brew: try to build nq from source on linux. no bottle available

--- bootstrap/brew-install-minimal.inc.sh
+++ bootstrap/brew-install-minimal.inc.sh
@@ -19,13 +19,20 @@ source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-asdf.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
 {
     TMP_NQ=$(mktemp -t firecloud.XXXXXXXXXX)
-    case ${OS_RELEASE_ID} in
-        alpine)
-            # "brew install nq" will trigger segmentation fault when calling 'nq'
+    case ${OS_SHORT} in
+        linux-alpine)
+            # NOTE "brew install nq" will trigger segmentation fault when calling 'nq'
             magic_install_one_unless nq "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
             ;;
-        *)
+        linux-*)
+            # NOTE no bottle available for linuxbrew
+            brew_install_one_unless "nq --build-from-source" "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
+            ;;
+        darwin-*)
             brew_install_one_unless nq "nq echo 123 | tee ${TMP_NQ} | head -1" "^,"
+            ;;
+        *)
+            echo_err "Cannot handle OS_SHORT=${OS_SHORT} OS_RELEASE_ID=${OS_RELEASE_ID}."
             ;;
     esac
     rm -f $(cat ${TMP_NQ})
