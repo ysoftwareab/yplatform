f4d33d8d56aa4730c20d857491e8573047e9116f
Andrei Neculau <andrei.neculau@gmail.com>
2021-11-23 10:57:48 +0100

    enable HOMEBREW_DISPLAY_INSTALL_TIMES

--- bootstrap/brew-util/env.inc.sh
+++ bootstrap/brew-util/env.inc.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+export HOMEBREW_DISPLAY_INSTALL_TIMES=1
 export HOMEBREW_FAIL_LOG_LINES=100
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
