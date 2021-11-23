462ee0811a9acdf358c67109fe3adf7d77d6ae89
Andrei Neculau <andrei.neculau@gmail.com>
2021-11-23 21:59:39 +0100

    order

--- sh/os.inc.sh
+++ sh/os.inc.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 ARCH=$(uname -m)
-ARCH_BIT=$(echo "${ARCH}" | grep -q "64" && echo "64" || echo "32")
 ARCH_SHORT=$(echo "${ARCH}" | grep -q "64" && echo "x64" || echo "x86")
+ARCH_BIT=$(echo "${ARCH}" | grep -q "64" && echo "64" || echo "32")
 
 OS=$(uname | tr "[:upper:]" "[:lower:]")
 OS_SHORT=$(echo "${OS}" | sed "s/^\([[:alpha:]]\{1,\}\).*/\1/g")
