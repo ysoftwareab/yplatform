59313cbe204dfe7b0d08eb5737827d4be915a94c
Andrei Neculau <andrei.neculau@gmail.com>
2021-12-26 12:29:33 +0100

    whitespace

--- .github/workflows.src/main-common.js
+++ .github/workflows.src/main-common.js
@@ -70,32 +70,39 @@ ciShSteps.push({
   shell: 'bash',
   run: './.ci.sh before_install'
 });
+
 ciShSteps.push({
   shell: 'bash',
   run: './.ci.sh install'
 });
+
 ciShSteps.push({
   shell: 'bash',
   run: './.ci.sh before_script'
 });
+
 ciShSteps.push({
   shell: 'bash',
   run: './.ci.sh script'
 });
+
 ciShSteps.push({
   if: 'failure()',
   shell: 'bash',
   run: './.ci.sh after_failure || true'
 });
+
 ciShSteps.push({
   shell: 'bash',
   run: './.ci.sh after_success || true'
 });
+
 ciShSteps.push({
   if: 'always()',
   shell: 'bash',
   run: './.ci.sh after_script || true'
 });
+
 ciShSteps.push({
   if: 'always()',
   shell: 'bash',
@@ -115,10 +122,12 @@ ciShStepsDeploy.push({
   shell: 'bash',
   run: './.ci.sh before_deploy'
 });
+
 ciShStepsDeploy.push({
   shell: 'bash',
   run: './.ci.sh deploy'
 });
+
 ciShStepsDeploy.push({
   shell: 'bash',
   run: './.ci.sh after_deploy || true'
@@ -132,6 +141,7 @@ dockerBuildxSteps.push({
   name: 'Set up QEMU',
   uses: 'docker/setup-qemu-action@v1'
 });
+
 dockerBuildxSteps.push({
   name: 'Set up Docker Buildx',
   id: 'buildx',
