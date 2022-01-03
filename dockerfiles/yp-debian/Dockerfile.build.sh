d1d07b589ae36b8632216389cd0debda3ac4d014
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-03 22:07:22 +0100

    portable-ruby for arm64

--- dockerfiles/yp-debian/Dockerfile.build.sh
+++ dockerfiles/yp-debian/Dockerfile.build.sh
@@ -21,31 +21,6 @@ source ${YP_DIR}/dockerfiles/util/common.inc.sh
     apt_install_one ssh-client
     apt_install_one sudo
 
-    [[ "${ARCH_NORMALIZED}" != "arm64" ]] || {
-        echo_do "Installing Ruby@2.6 for Hombrew on Linux (arm64)..."
-        # https://docs.brew.sh/Homebrew-on-Linux#arm
-        # https://github.com/Homebrew/brew/issues/11320
-        # https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-        apt_install_one autoconf
-        apt_install_one bison
-        apt_install_one build-essential
-        apt_install_one libssl-dev
-        apt_install_one libyaml-dev
-        apt_install_one libreadline6-dev
-        apt_install_one zlib1g-dev
-        apt_install_one libncurses5-dev
-        apt_install_one libffi-dev
-        apt_install_one libgdbm6
-        apt_install_one libgdbm-dev
-        apt_install_one libdb-dev
-
-        apt_install_one curl
-        git clone https://github.com/rbenv/ruby-build.git /opt/ruby-build
-        PREFIX=/usr/local /opt/ruby-build/install.sh
-        ruby-build $(ruby-build --list | grep "^2\.6\." | head -n1) /usr
-        echo_done
-    }
-
     [[ "${YP_SUDO}" = "yp_nosudo_fallback" ]] || export YP_SUDO=sudo
 
     source ${YP_DIR}/dockerfiles/util/gitconfig.inc.sh
