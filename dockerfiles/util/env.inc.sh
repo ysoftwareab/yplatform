69f26a5842c3c6363eef340c5c02cba852acb616
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-23 23:24:27 +0100

    fix git user/email in docker

--- dockerfiles/util/env.inc.sh
+++ dockerfiles/util/env.inc.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 
 export CI=true
 # export YP_DOCKER_CI_IMAGE_NAME= # --build-arg
-# export YP_DOCKER_CI_IMAGE_TAG= # --build-arg
 # export YP_CI_BREW_INSTALL= # --build-arg
 GID_INDEX=999
 UID_INDEX=999
 GNAME=yp
 UNAME=yp
-GIT_USER_EMAIL="${YP_DOCKER_CI_IMAGE_NAME}.${YP_DOCKER_CI_IMAGE_TAG}@docker"
-GIT_USER_NAME="${YP_DOCKER_CI_IMAGE_NAME} ${YP_DOCKER_CI_IMAGE_TAG}"
+GIT_USER_EMAIL="${YP_DOCKER_CI_IMAGE_NAME}@docker"
+GIT_USER_NAME="${YP_DOCKER_CI_IMAGE_NAME}"
 
 [[ ! -e ${YP_DIR}/yp-ci-echo-benchmark ]] || \
     export YP_CI_ECHO_BENCHMARK=${YP_DIR}/yp-ci-echo-benchmark
