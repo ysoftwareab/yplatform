b94e4f3a6dca874b31e34950ec1ff5e0f5e837c2
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-24 11:14:52 +0100

    better log grouping for Dockerfile.test.sh

--- dockerfiles/util/Dockerfile.test.sh
+++ dockerfiles/util/Dockerfile.test.sh
@@ -3,31 +3,47 @@
 set -euo pipefail
 
 YP_DIR=/yplatform
+source ${YP_DIR}/dockerfiles/util/common.inc.sh
 
 [[ $# -gt 0 ]] || {
     # shellcheck disable=SC1091
     UNAME=$(source ${YP_DIR}/dockerfiles/util/env.inc.sh && echo "${UNAME}")
     BASH=$(command -v bash)
 
-    # test root user
+    echo_do "Test root user config..."
     sudo ${BASH} -l -i -c "${BASH_SOURCE[0]} root"
-    # test default user
+    echo_done
+
+    echo_do "Test ${UNAME} user config..."
     sudo --user ${UNAME} ${BASH} -l -i -c "${BASH_SOURCE[0]} ${UNAME}"
+    echo_done
+
     exit 0
 }
 
-set -x
-
 # BASH
+
+echo_do "Test bash env..."
+XTRACE_STATE_DOCKERFILE_TEST_SH="$(set +o | grep xtrace)"
+set -x
 
 ls -la ${HOME}/.bash_aliases
 cat ${HOME}/.bash_aliases | grep -q -Fx "source ${YP_DIR}/bootstrap/brew-util/env.inc.sh"
 cat ${HOME}/.bash_aliases | grep -q -Fx "source ${YP_DIR}/sh/dev.inc.sh"
+
 # shellcheck disable=SC2016
 env -i HOME=${HOME} bash -l -i -c '[[ "${YP_DEV_INC_SH}" = true ]]'
 env -i HOME=${HOME} bash -l -i -c 'command -v brew'
 
+eval "${XTRACE_STATE_DOCKERFILE_TEST_SH}"
+unset XTRACE_STATE_DOCKERFILE_TEST_SH
+echo_done
+
 # GIT
+
+echo_do "Test git config..."
+XTRACE_STATE_DOCKERFILE_TEST_SH="$(set +o | grep xtrace)"
+set -x
 
 ls -la ${HOME}/.gitattributes_global
 [[ "$(readlink -f ${HOME}/.gitattributes_global)" = "${YP_DIR}/gitconfig/dot.gitattributes_global" ]]
@@ -43,7 +59,15 @@ git config -f ${HOME}/.gitconfig --get "user.email" | ${YP_DIR}/bin/ifne -f -p
 git config -f ${HOME}/.gitconfig --get-all "include.path" || true
 git config -f ${HOME}/.gitconfig --get-all "include.path" | grep -q -Fx "${YP_DIR}/gitconfig/dot.gitconfig"
 
+eval "${XTRACE_STATE_DOCKERFILE_TEST_SH}"
+unset XTRACE_STATE_DOCKERFILE_TEST_SH
+echo_done
+
 # SSH
+
+echo_do "Test ssh config..."
+XTRACE_STATE_DOCKERFILE_TEST_SH="$(set +o | grep xtrace)"
+set -x
 
 ls -la ${HOME}/.ssh/config
 cat ${HOME}/.ssh/config | grep -q -Fx "Include ~/.ssh/yplatform/config"
@@ -51,6 +75,18 @@ cat ${HOME}/.ssh/config | grep -q -Fx "Include ~/.ssh/yplatform/config"
 ls -la ${HOME}/.ssh/yplatform
 [[ "$(readlink -f ${HOME}/.ssh/yplatform)" = "${YP_DIR}/sshconfig" ]]
 
+eval "${XTRACE_STATE_DOCKERFILE_TEST_SH}"
+unset XTRACE_STATE_DOCKERFILE_TEST_SH
+echo_done
+
 # MISC
 
+echo_do "Test misc config..."
+XTRACE_STATE_DOCKERFILE_TEST_SH="$(set +o | grep xtrace)"
+set -x
+
 [[ "${USER}" = "root" ]] || test -f ${HOME}/.sudo_as_admin_successful
+
+eval "${XTRACE_STATE_DOCKERFILE_TEST_SH}"
+unset XTRACE_STATE_DOCKERFILE_TEST_SH
+echo_done
