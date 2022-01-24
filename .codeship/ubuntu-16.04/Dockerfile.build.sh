78b3aa2fa29e792d878d450904ce30c095ef2f0b
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-24 19:41:53 +0100

    fix and update codeship docker config

--- .codeship/ubuntu-16.04/Dockerfile.build.sh
+++ .codeship/ubuntu-16.04/Dockerfile.build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# -*- mode: sh -*-
 set -euo pipefail
 
+# keep in sync with dockerfiles/yp-ubuntu-*/Dockerfile.build.sh
+
 YP_DIR=/yplatform
-source ${YP_DIR}/dockerfiles/util/env.inc.sh
+source ${YP_DIR}/dockerfiles/util/common.inc.sh
 # shellcheck disable=SC2034
 UNAME=codeship
 # shellcheck disable=SC2034
@@ -13,9 +16,10 @@ GIT_USER_EMAIL="bot@codeship.com"
 GIT_USER_NAME="Codeship"
 
 # DEPS
-source ${YP_DIR}/sh/exe.inc.sh
-source ${YP_DIR}/sh/package-managers/apt.inc.sh
+# keep in sync with bootstrap/linux/bootstrap-sudo-debian
 [[ -f /yplatform.bootstrapped ]] || {
+    XTRACE_STATE_DOCKERFILE_BUILD_SH="$(set +o | grep xtrace)"
+    set -x
     apt_update
 
     apt_install_one apt-transport-https
@@ -28,9 +32,15 @@ source ${YP_DIR}/sh/package-managers/apt.inc.sh
     apt_install_one ssh-client
     apt_install_one sudo
 
-    source ${YP_DIR}/dockerfiles/util/gitconfig.inc.sh
+    [[ "${YP_SUDO}" = "yp_nosudo_fallback" ]] || export YP_SUDO=sudo
+
+    source ${YP_DIR}/dockerfiles/util/root.inc.sh
     source ${YP_DIR}/dockerfiles/util/user.inc.sh
+    eval "${XTRACE_STATE_DOCKERFILE_BUILD_SH}"
+    unset XTRACE_STATE_DOCKERFILE_BUILD_SH
 }
 
-# source ${YP_DIR}/dockerfiles/util/bootstrap.inc.sh
-# source ${YP_DIR}/dockerfiles/util/clean.inc.sh
+source ${YP_DIR}/dockerfiles/util/bootstrap.inc.sh
+source ${YP_DIR}/dockerfiles/util/clean.inc.sh
+
+/Dockerfile.test.sh
