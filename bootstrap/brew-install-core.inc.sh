74b4f8a01fbc794c940f4008cc06cdf9c6463d63
Andrei Neculau <andrei.neculau@gmail.com>
2022-01-24 09:22:22 +0100

    install libplist as a plutil equivalent on linux

--- bootstrap/brew-install-core.inc.sh
+++ bootstrap/brew-install-core.inc.sh
@@ -28,6 +28,31 @@ brew_install_one_unless unzip "unzip --version 2>&1 | head -2 | tail -1" ", by D
 brew_install_one_unless zip "zip --version 2>&1 | head -2 | tail -1" "Zip 3\.0"
 brew_install_one_unless zip "zip --version 2>&1 | head -2 | tail -1" ", by Info-ZIP\."
 
+case ${OS_SHORT}-${OS_RELEASE_ID} in
+    darwin-*)
+        ;;
+    linux-alpine)
+        apk_install_one libplist
+        ${YP_SUDO:-} ln -s $(command -v plistutil) /usr/bin/plutil
+        ;;
+    linux-arch)
+        pacman_install_one libplist
+        ${YP_SUDO:-} ln -s $(command -v plistutil) /usr/bin/plutil
+        ;;
+    linux-centos)
+        yum_install_one libplist
+        ${YP_SUDO:-} ln -s $(command -v plistutil) /usr/bin/plutil
+        ;;
+    linux-debian|linux-ubuntu)
+        apt_install_one libplist-utils
+        ${YP_SUDO:-} ln -s $(command -v plistutil) /usr/bin/plutil
+        ;;
+    *)
+        echo_err "${OS_SHORT}-${OS_RELEASE_ID} is an unsupported OS for installing plistutil."
+        return 1
+        ;;
+esac
+
 source ${YP_DIR}/bootstrap/brew-install-gnu.inc.sh
 
 echo_done
