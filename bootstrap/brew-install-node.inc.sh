1fc3d7e202aa96b6959152573c0cb83c1756f251
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-06 12:18:00 +0100

    bootstrap: add vscode devcontainer

--- bootstrap/brew-install-node.inc.sh
+++ bootstrap/brew-install-node.inc.sh
@@ -18,6 +18,9 @@ brew_install_one_unless yarn "yarn --version | head -1" "^1\."
 unless_exe_and_grep_q "npm --version | head -1" "^6\." \
     npm install --global --force npm@6 || ${YP_DIR}/bin/is-wsl
 
+unless_exe_and_grep_q "devcontainer --help | head -1" "^devcontainer " \
+    npm install --global @vscode/dev-container-cli^0
+
 unless_exe_and_grep_q "json --version | head -1" "^json 9\." \
     npm install --global json@9
 
