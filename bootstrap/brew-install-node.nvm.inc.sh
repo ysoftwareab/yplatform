ce5c37db669ff3816f9bb822aa101b5bbb3ba330
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-07 09:15:07 +0100

    verbose about nvm bootstrap

--- bootstrap/brew-install-node.nvm.inc.sh
+++ bootstrap/brew-install-node.nvm.inc.sh
@@ -21,21 +21,31 @@ echo_done
 
 exe_and_grep_q "nvm --version | head -1" "^0\."
 
+NODE_VSN=
 [[ ! -f .nvmrc ]] || {
-    echo_do "Activating node $(cat .nvmrc) via NVM (as per .nvmrc)..."
-    nvm install # and use
+    NODE_VSN="$(cat .nvmrc)"
+    echo_do "Activating node ${NODE_VSN} via NVM (as per .nvmrc)..."
+}
+[[ -z "${NODE_VSN:-}" ]] || {
+    exe nvm install ${NODE_VSN} # and use
+    exe node --version
+    exe npm --version
 
     if nvm list --no-colors | grep -q system; then
-        nvm reinstall-packages system
+        exe nvm reinstall-packages system
 
         # the command above skips 'npm'
         SYSTEM_NPM_VSN=$(nvm use system >/dev/null; npm --version)
-        cd ${NVM_DIR}/versions/node/$(node --version)/lib
-        npm install --global-style npm@${SYSTEM_NPM_VSN}
-        cd -
-    fi
+        (
+            cd ${NVM_DIR}/versions/node/$(node --version)/lib
+            npm install --global-style npm@${SYSTEM_NPM_VSN}
+        )
+        unset SYSTEM_NPM_VSN
 
+        exe npm --version
+    fi
     echo_done
 }
+unset NODE_VSN
 
 echo_done
