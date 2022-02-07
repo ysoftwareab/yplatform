d609c2b66c75bcf002d8d4b01019a34334617a1a
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-07 09:14:49 +0100

    document bootstrap flags in ci.sh template

--- repo/dot.ci.sh
+++ repo/dot.ci.sh
@@ -7,6 +7,12 @@ YP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/yplatform" >/dev/null && pwd)"
     git submodule update --init --recursive ${YP_DIR}
 source ${YP_DIR}/sh/common.inc.sh
 
+## see bootstrap/README.md
+# export YP_LOG_BOOTSTRAP=true
+# export YP_PRINTENV_BOOTSTRAP=true
+# export YP_SKIP_SUDO_BOOTSTRAP=true
+# export YP_SKIP_BREW_BOOTSTRAP=true
+
 ## to override an existing phase implementation
 # function ci_run_<phase>() {
 # }
