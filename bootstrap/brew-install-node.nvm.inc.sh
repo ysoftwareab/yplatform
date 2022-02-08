ad1d11f9ffe3f9f56aaf48667da42058c9d09227
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-08 13:45:32 +0100

    add yp::nvm_install

--- bootstrap/brew-install-node.nvm.inc.sh
+++ bootstrap/brew-install-node.nvm.inc.sh
@@ -21,31 +21,13 @@ echo_done
 
 exe_and_grep_q "nvm --version | head -1" "^0\."
 
-NODE_VSN=
 [[ ! -f .nvmrc ]] || {
     NODE_VSN="$(cat .nvmrc)"
-    echo_do "Activating node ${NODE_VSN} via NVM (as per .nvmrc)..."
-}
-[[ -z "${NODE_VSN:-}" ]] || {
-    exe nvm install ${NODE_VSN} # and use
-    exe node --version
-    exe npm --version
-
-    if nvm list --no-colors | grep -q system; then
-        exe nvm reinstall-packages system
-
-        # the command above skips 'npm'
-        SYSTEM_NPM_VSN=$(nvm use system >/dev/null; npm --version)
-        (
-            cd ${NVM_DIR}/versions/node/$(node --version)/lib
-            npm install --global-style npm@${SYSTEM_NPM_VSN}
-        )
-        unset SYSTEM_NPM_VSN
-
-        exe npm --version
-    fi
+    echo_do "Installing node@${NODE_VSN} via NVM (as per .nvmrc)..."
+    yp::nvm_install ${NODE_VSN}
+    nvm use system # revert to homebrew's version
+    unset NODE_VSN
     echo_done
 }
-unset NODE_VSN
 
 echo_done
