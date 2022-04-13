5bb76de8105e4f520873836c8da00ba00f1730c0
Andrei Neculau <andrei.neculau@gmail.com>
2022-04-13 09:06:41 +0200

    refactor pipeline script and add pipeline action

--- ci/pipeline.script.sh
+++ ci/pipeline.script.sh
@@ -3,17 +3,30 @@ set -euo pipefail
 
 after_script() {
     EXIT_STATUS=$?
+    [[ "${EXIT_STATUS}" = "0" ]] || export YP_CI_STATUS=failure
     ./.ci.sh after_script
     exit ${EXIT_STATUS}
 }
 trap after_script EXIT
 
-YP_CI_STATUS=success
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh before_install || YP_CI_STATUS=failure
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh install || YP_CI_STATUS=failure
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh before_script || YP_CI_STATUS=failure
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh script || YP_CI_STATUS=failure
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh before_cache || true
-[[ "${YP_CI_STATUS}" = "failure" ]] || ./.ci.sh after_success || true
-[[ "${YP_CI_STATUS}" = "success" ]] || ./.ci.sh after_failure || true
-[[ "${YP_CI_STATUS}" = "success" ]] || exit 1
+function success_pipeline() {
+    ./.ci.sh before_install
+    ./.ci.sh install
+    ./.ci.sh before_script
+    ./.ci.sh script
+    ./.ci.sh before_cache || true
+    ./.ci.sh after_success || true
+}
+
+function failure_pipeline() {
+    ./.ci.sh after_failure || true
+}
+
+export YP_CI_STATUS=success
+if success_pipeline; then
+    exit 0
+else
+    export YP_CI_STATUS=failure
+    failure_pipeline
+    exit 1
+fi
