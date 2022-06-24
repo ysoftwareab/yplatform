47dcb88ded35fe46bc47cbcd1223ef2b415e74c1
Andrei Neculau <andrei.neculau@gmail.com>
2022-06-24 17:34:36 +0200

    don't make YP_CI_ECHO static, since it could be changed dynamically and it's only a string interpolation

--- mk/exe.echo.inc.mk
+++ mk/exe.echo.inc.mk
@@ -8,9 +8,7 @@ ECHO_INDENT = $(YP_CI_ECHO) -- "      "
 ECHO_NEXT = $(YP_CI_ECHO) -- "[NEXT]"
 ECHO_Q = $(YP_CI_ECHO) -- "[Q   ]"
 ECHO_SKIP = $(YP_CI_ECHO) -- "[SKIP]"
-$(foreach VAR,YP_CI_ECHO ECHO_DO ECHO_DONE ECHO_INDENT ECHO_NEXT ECHO_Q ECHO_SKIP,$(call make-lazy,$(VAR)))
 
 ECHO_ERR = $(YP_CI_ECHO) -- "[ERR ]"
 ECHO_INFO = $(YP_CI_ECHO) -- "[INFO]"
 ECHO_WARN = $(YP_CI_ECHO) -- "[WARN]"
-$(foreach VAR,ECHO_ERR ECHO_INFO ECHO_WARN,$(call make-lazy,$(VAR)))
