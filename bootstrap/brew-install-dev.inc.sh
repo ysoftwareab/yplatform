a1ff25f9709c46863b776dcdcd12906987e5a6fb
Andrei Neculau <andrei.neculau@gmail.com>
2022-07-10 21:33:19 +0200

    fix ttyrec

--- bootstrap/brew-install-dev.inc.sh
+++ bootstrap/brew-install-dev.inc.sh
@@ -11,5 +11,7 @@ brew_install_one_unless jid "jid --version | head -1" "^jid version v0\."
 brew_install_one_unless tmate "tmate -V | head -1" "^tmate 2\."
 # NOTE allow ttyrec to fail. May fail with
 # ttyrec.c:74:18: fatal error: util.h: No such file or directory
-brew_install_one_unless ttyrec "ttyrec -e 'echo 123' -e $(mktemp -t yplatform.XXXXXXXXXX) | head -1" "123" -Fx || true
+# brew_install_one_unless ttyrec "ttyrec -e 'echo 123' $(mktemp -t yplatform.XXXXXXXXXX) | head -1" "123" -Fx || true
+# NOTE ttyrec might even send sigterm 143 with "Out of pty's". Install on best effort
+brew_install_one ttyrec
 echo_done
