85e39ca2daa265d0f642aaf6e7f0dc00fc9b42eb
Andrei Neculau <andrei.neculau@gmail.com>
2022-07-17 09:45:20 +0200

    github: retire macos-10.15, document other envs

--- .github/workflows.src/common-matrix-os.js
+++ .github/workflows.src/common-matrix-os.js
@@ -2,18 +2,25 @@
 
 let _ = require('lodash-firecloud');
 
+// https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
+
 let matrixOs = {
   ubuntu: [
+    // obsolete 2020-09 https://github.com/actions/virtual-environments/issues/3287
+    // 'ubuntu-16.04',
     'ubuntu-18.04',
     'ubuntu-20.04',
     'ubuntu-22.04'
   ],
   macos: [
-    'macos-10.15',
+    // obsolete 2022-08 https://github.com/actions/virtual-environments/issues/5583
+    // 'macos-10.15',
     'macos-11',
     'macos-12'
   ],
   windows: [
+    // obsolete 2022-03 https://github.com/actions/virtual-environments/issues/5238
+    // 'windows-2016',
     'windows-2019',
     'windows-2022'
   ]
