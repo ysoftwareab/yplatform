1c9fc5187c3d672df04935aedc00fb227039af9f
Andrei Neculau <andrei.neculau@gmail.com>
2022-07-18 08:54:55 +0200

    bootstrap: install python without workarounds

--- bootstrap/brew-install-minimal.inc.sh
+++ bootstrap/brew-install-minimal.inc.sh
@@ -3,20 +3,10 @@ set -euo pipefail
 
 echo_do "brew: Installing minimal packages..."
 source ${YP_DIR}/bootstrap/brew-install-core.inc.sh
-# NOTE installing formulas that depend on python might exibit
-# - reinstalling 1 broken dependent from source
-# - bad interpreter
-# This is due to Cellar/opt/python@3.9 not having any content but a 3.9.5-reinstall folder.
-# see https://github.com/Homebrew/homebrew-core/issues/70497
-# We experience this only on centos-8 now when installing docker-compose,
-# but historically it has been a problem for aws-cli as well on ubuntu.
-# NOTE we install by force via homebrew, without checking system's python
-# because formulas like docker-compose, dependent on python, will no use the system's python
-# source ${YP_DIR}/bootstrap/brew-install-py.inc.sh
-brew_install_one python
-
 source ${YP_DIR}/bootstrap/brew-install-asdf.inc.sh
+source ${YP_DIR}/bootstrap/brew-install-py.inc.sh
 source ${YP_DIR}/bootstrap/brew-install-node.inc.sh
+
 {
     TMP_NQ=$(mktemp -t yplatform.XXXXXXXXXX)
     case ${OS_SHORT} in
