1fe5deb32a445a70e43ad7574f54bb8c652a2c81
Andrei Neculau <andrei.neculau@gmail.com>
2022-07-21 10:00:32 +0200

    pipeline wouldn't fail

--- ci/pipeline.deploy.sh
+++ ci/pipeline.deploy.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 # don't deploy from PRs ever
 [[ "${YP_CI_IS_PR}" != "true" ]] || exit 0
 
-./.ci.sh before_deploy
-./.ci.sh deploy
-./.ci.sh after_deploy
+function fail_pipeline() {
+    export YP_CI_STATUS=failure
+    exit 1
+}
+
+export YP_CI_STATUS=success
+./.ci.sh before_deploy || fail_pipeline
+./.ci.sh deploy || fail_pipeline
+./.ci.sh after_deploy || fail_pipeline
