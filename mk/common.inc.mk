9e32214b4a4171693346ebc2e8b55ab6ece4b97c
Andrei Neculau <andrei.neculau@gmail.com>
2023-02-11 18:38:51 +0100

    Revert "add make target for time sentinels"
    This reverts commit 1d06d582025aac64b371a96ff56ad44796bf07a1.

--- mk/common.inc.mk
+++ mk/common.inc.mk
@@ -22,7 +22,6 @@ include $(CORE_INC_MK_DIR)/target.env.inc.mk
 include $(CORE_INC_MK_DIR)/target.help.inc.mk
 include $(CORE_INC_MK_DIR)/target.noop.inc.mk
 include $(CORE_INC_MK_DIR)/target.printvar.inc.mk
-include $(CORE_INC_MK_DIR)/target.sentinel.inc.mk
 include $(CORE_INC_MK_DIR)/target.verbose.inc.mk
 include $(CORE_INC_MK_DIR)/target.lazy.inc.mk
 
