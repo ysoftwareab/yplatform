ac54392db1b43e04cc37ef8fb34152e2c30ada83
Andrei Neculau <andrei.neculau@gmail.com>
2023-06-22 15:59:38 +0300

    add deps/upgrade target. fix #274

--- build.mk/core.deps.mk
+++ build.mk/core.deps.mk
@@ -5,6 +5,18 @@
 #	deps-for-something-else \
 #
 # ------------------------------------------------------------------------------
+#
+# Adds a 'deps/upgrade' target that will call all the targets in YP_DEPS_UPGRADE_TARGETS.
+#
+# To add another deps/upgrade target do
+# YP_DEPS_UPGRADE_TARGETS += \
+#	deps-upgrade-for-something-else \
+#
+# ------------------------------------------------------------------------------
+
+YP_DEPS_TARGETS += \
+
+YP_DEPS_UPGRADE_TARGETS += \
 
 # ------------------------------------------------------------------------------
 
@@ -13,5 +25,13 @@ deps: ## Fetch dependencies.
 	[[ "$(words $(YP_DEPS_TARGETS))" = "0" ]] || { \
 		$(ECHO_DO) "Fetching dependencies..."; \
 		$(MAKE) --no-print-directory $(YP_DEPS_TARGETS); \
+		$(ECHO_DONE); \
+	}
+
+.PHONY: deps/upgrade
+deps/upgrade: ## Upgrade dependencies.
+	[[ "$(words $(YP_DEPS_UPGRADE_TARGETS))" = "0" ]] || { \
+		$(ECHO_DO) "Upgrade dependencies..."; \
+		$(MAKE) --no-print-directory $(YP_DEPS_UPGRADE_TARGETS); \
 		$(ECHO_DONE); \
 	}
