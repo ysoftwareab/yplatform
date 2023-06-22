1207342efe8041291d65ecb2ff8ddbf6a268b7b5
Andrei Neculau <andrei.neculau@gmail.com>
2023-06-22 09:27:05 +0300

    Revert "install binutils and gcc separately, from the get go"
    This reverts commit f7b80a205b67a373c3120849da0580fe12527e27.

--- bootstrap/brew-install-ci.inc.sh
+++ bootstrap/brew-install-ci.inc.sh
@@ -21,12 +21,6 @@ set -euo pipefail
 }
 
 echo_do "brew: Installing CI packages..."
-echo_do "Installing binutils and gcc..."
-brew install --verbose binutils
-# 'brew postinstall gcc' is unstable
-brew install --verbose gcc || brew postinstall --debug gcc
-echo_done
-
 # NOTE 'findutils' provides 'find' with '-min/maxdepth' and '-printf'
 # NOTE 'findutils' provides 'xargs', because the MacOS version has no 'xargs -r'
 brew_install_one_unless findutils "find --version | head -1" "^find (GNU findutils) 4\."
