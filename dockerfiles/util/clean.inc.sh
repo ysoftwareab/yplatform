f4bcb3b258f62ed30e5504679cfeac75bdf1a3ab
Andrei Neculau <andrei.neculau@gmail.com>
2025-03-22 17:23:52 +0100

    updated dockerfiles/util/clean.inc.sh

--- dockerfiles/util/clean.inc.sh
+++ dockerfiles/util/clean.inc.sh
@@ -36,7 +36,7 @@ function git_dir_shallow() {
     set -x
     {
         du -hcs $1
-        ${YP_DIR}/bin/git-shallow $1
+        ${PYTTI_DIR}/bin/git-shallow $1
         du -hcs $1
     } || true
     set +x
@@ -44,21 +44,19 @@ function git_dir_shallow() {
 }
 
 (
-    cd ${YP_DIR}
-    git add -f -- BUILD VERSION
-    git_dir_clean ${YP_DIR}
-    git_dir_shallow ${YP_DIR}
-    git reset -- BUILD VERSION
+    cd ${PYTTI_DIR}
+    git_dir_clean ${PYTTI_DIR}
+    git_dir_shallow ${PYTTI_DIR}
 )
 
-brew_cache_prune
-magic_cache_prune
-dir_clean ${HOME}/.cache/* # misc cache for root
-dir_clean /home/${UNAME}/.cache/* # misc cache for yp user
+sudo rm -rf "$(brew --cache)"
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+
+dir_clean ${HOME}/.cache/*
 dir_clean /tmp/*
 dir_clean /var/log/*
 
 for DIR in $(brew --repository) $(brew --repository)/Library/Taps/*/*; do
     git_dir_shallow ${DIR}
-    chown -R ${UNAME}:${GNAME} ${DIR}
 done
